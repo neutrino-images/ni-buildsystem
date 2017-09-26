b4a22ab9a999743f22dfcd015c43a8ddda5f8d3e
gixxpunk <thomas.harfmann@gmail.com>
2017-09-26 12:21:35 +0200

    - switch to ni/ffmpeg/2.8
    reduces image size and simply has less bugs. the only feature we are
    missing is DASH streams.

--- make/environment.mk
+++ make/environment.mk
@@ -254,8 +254,8 @@ NI_DRIVERS-BIN		= ni-drivers-bin
 # ffmpeg/master is currently not mature enough for daily use
 # if you want to help testing you can enable it here
 NI_FFMPEG		= ni-ffmpeg
-#NI_FFMPEG_BRANCH	= ni/ffmpeg/2.8
-NI_FFMPEG_BRANCH	= ni/ffmpeg/master
+NI_FFMPEG_BRANCH	= ni/ffmpeg/2.8
+#NI_FFMPEG_BRANCH	= ni/ffmpeg/master
 
 NI_OPENTHREADS		= ni-openthreads
 
