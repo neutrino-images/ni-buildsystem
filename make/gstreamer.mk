2678ce6a6ad522093b3a549fc03d40dc67cad830
gixxpunk <thomas.harfmann@gmail.com>
2017-10-08 15:22:23 +0200

    - gst_plugins_dvbmediasink: align with TangoCash buildsystem-ddt

--- make/gstreamer.mk
+++ make/gstreamer.mk
@@ -398,17 +398,16 @@ $(D)/gst_plugins_dvbmediasink: $(D)/gstreamer $(D)/gst_plugins_base $(D)/gst_plu
 		$(CONFIGURE) \
 			--prefix= \
 			--enable-silent-rules \
-			--with-spark \
-			--with-h265 \
-			--with-vb8 \
-			--with-vb9 \
-			--with-dts \
 			--with-wma \
 			--with-wmv \
 			--with-pcm \
+			--with-dts \
 			--with-eac3 \
-			--with-dtsdownmix \
-			--with-mpeg4v2 \
+			--with-h265 \
+			--with-vb6 \
+			--with-vb8 \
+			--with-vb9 \
+			--with-spark \
 			--with-gstversion=1.0 \
 		; \
 		$(MAKE) all; \
