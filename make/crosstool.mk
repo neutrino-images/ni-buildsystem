886200f3860fcf85913a8aacda65efe2d09a2705
gixxpunk <gandharva@gmx.de>
2017-10-30 15:21:21 +0100

    hd51: update gcc (linaro-6.3-2017.02) and use custom kernel for crosstool build

--- make/crosstool.mk
+++ make/crosstool.mk
@@ -95,11 +95,18 @@ crosstool-arm-hd51: CROSS_DIR-check
 	git checkout 1dbb06f2 && \
 	unset CONFIG_SITE LIBRARY_PATH CPATH C_INCLUDE_PATH PKG_CONFIG_PATH CPLUS_INCLUDE_PATH INCLUDE && \
 	cd $(BUILD_TMP)/crosstool-ng && \
+		mkdir -p $(BUILD_TMP)/crosstool-ng/targets/src/ && \
+			pushd $(SOURCE_DIR)/$(NI_LINUX-KERNEL) && \
+				git checkout $(KERNEL_BRANCH) && \
+			popd && \
+		tar cf linux-$(KERNEL_VERSION).tar --exclude-vcs -C $(SOURCE_DIR)/$(NI_LINUX-KERNEL) . && \
+		mv linux-$(KERNEL_VERSION).tar $(BUILD_TMP)/crosstool-ng/targets/src/ && \
 		cp -a $(CONFIGS)/ct-ng-armbox_hd51.config .config && \
 		sed -i "s@^CT_PARALLEL_JOBS=.*@CT_PARALLEL_JOBS=$(NUM_CPUS)@" .config && \
 		export NI_BASE_DIR=$(BASE_DIR) && \
-		export CT_BASE_DIR=$(CROSS_BASE); \
-		export LD_LIBRARY_PATH=; \
+		export NI_CUSTOM_KERNEL=$(BUILD_TMP)/crosstool-ng/targets/src/linux-$(KERNEL_VERSION).tar && \
+		export NI_CUSTOM_KERNEL_VERSION=$(KERNEL_VERSION) && \
+		export LD_LIBRARY_PATH= && \
 		test -f ./configure || ./bootstrap && \
 		./configure --enable-local; MAKELEVEL=0 make; chmod 0755 ct-ng && \
 		./ct-ng oldconfig && \
