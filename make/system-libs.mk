cf628b6cd31419fa1911c45c58217188a6ce6003
vanhofen <vanhofen@gmx.de>
2018-08-18 23:25:47 +0200

    - libtirpc: bump version to 1.0.3

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -715,7 +715,6 @@ $(D)/libtirpc: $(ARCHIVE)/libtirpc-$(LIBTIRPC_VER).tar.bz2 | $(TARGET_DIR)
 	$(PATCH)/libtirpc-0004-Automatically-generate-XDR-header-files-from-.x-sour.patch && \
 	$(PATCH)/libtirpc-0005-Add-more-XDR-files-needed-to-build-rpcbind-on-top-of.patch && \
 	$(PATCH)/libtirpc-0006-Disable-DES-authentification-support.patch && \
-	$(PATCH)/libtirpc-0007-include-stdint.h-for-uintptr_t.patch && \
 		autoreconf -fi && \
 		$(CONFIGURE) \
 			--target=$(TARGET) \
