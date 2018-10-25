9b55ea80a777f16c51416c0e70c9b42bf163f599
vanhofen <vanhofen@gmx.de>
2018-10-25 23:22:54 +0200

    - add some more clean targets

--- make/clean.mk
+++ make/clean.mk
@@ -11,11 +11,17 @@ staging-clean:
 static-clean:
 	-rm -rf $(STATIC_DIR)
 
+cross-clean:
+	-rm -rf $(CROSS_DIR)
+
+host-clean:
+	-rm -rf $(HOST_DIR)
+
 all-clean: rebuild-clean staging-clean static-clean
 	@echo -e "\n$(TERM_RED_BOLD)Any other key then CTRL-C will now remove CROSS_DIR and HOST_DIR$(TERM_NORMAL)"
 	@read
-	-rm -rf $(CROSS_DIR)
-	-rm -rf $(HOST_DIR)
+	make cross-clean
+	make host-clean
 
 %-clean:
 	cd $(D) && find . -name $(subst -clean,,$@) -delete
