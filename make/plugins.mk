6313feee99cce38c65a212a43a049def41905799
vanhofen <vanhofen@gmx.de>
2018-11-15 21:25:58 +0100

    - links: add patch to fix input device for cst hardware; thx to max

--- make/plugins.mk
+++ make/plugins.mk
@@ -561,9 +561,7 @@ $(LIBPLUGINS)/shellexec.so: $(D)/freetype $(LIBPLUGINS) $(SHAREFLEX) $(VARCONFIG
 
 LINKS_PATCH  = links-$(LINKS_VER).patch
 LINKS_PATCH += links-$(LINKS_VER)-ac-prog-cxx.patch
-ifeq ($(BOXSERIES), hd51)
-LINKS_PATCH += links-$(LINKS_VER)-hd51-input.patch
-endif
+LINKS_PATCH += links-$(LINKS_VER)-input-$(BOXTYPE_SC).patch
 
 $(D)/links: $(D)/libpng $(D)/libjpeg $(D)/openssl $(ARCHIVE)/links-$(LINKS_VER).tar.bz2 $(LIBPLUGINS) | $(TARGET_DIR)
 	$(REMOVE)/links-$(LINKS_VER)
