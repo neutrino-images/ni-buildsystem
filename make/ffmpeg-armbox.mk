452381523b51db846d061332b7e4ba7b4870726d
vanhofen <vanhofen@gmx.de>
2018-11-24 22:33:16 +0100

    - ffmpeg-armbox: bump version to 4.1

--- make/ffmpeg-armbox.mk
+++ make/ffmpeg-armbox.mk
@@ -3,7 +3,7 @@
 #
 # -----------------------------------------------------------------------------
 
-FFMPEG_VER = 4.0.3
+FFMPEG_VER = 4.1
 FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VER).tar.xz
 
 $(ARCHIVE)/$(FFMPEG_SOURCE):
@@ -19,7 +19,6 @@ FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-optimize_aac.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix_edit_list_parsing.patch
 # ffmpeg exteplayer3 patches
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix_mpegts.patch
-FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-allow_to_choose_rtmp_impl_at_runtime.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-hls_replace_key_uri.patch
 
 # -----------------------------------------------------------------------------
