e881c42c5a0cc402538cb6530944b0b7812b1262
vanhofen <vanhofen@gmx.de>
2019-03-31 18:44:13 +0200

    Revert "- plugins: remove WWEATHER defie from tuxwetter"
    This reverts commit 2c0f4c69e88a246abd4eee17a8bd1b54ea5bc294.

--- make/plugins.mk
+++ make/plugins.mk
@@ -494,6 +494,7 @@ $(LIBPLUGINS)/tuxwetter.so: $(D)/freetype $(D)/libcurl $(D)/giflib $(D)/libjpeg 
 		\
 		-lfreetype -lz -lcrypto -lssl -lcurl -ljpeg -lpng -lgif -lm \
 		\
+		-DWWEATHER \
 		-DHAVE_CONFIG_H \
 		\
 		fb_display.c \
