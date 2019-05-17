a16efca58cbc6708fa086e809d871867d71987da
vanhofen <vanhofen@gmx.de>
2019-05-17 23:32:51 +0200

    - helpers: add script to get or update git repositories

---
+++ helpers/get-git-source.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# clone or pull an existing git repository
+#
+# (C) 2019 vanhofen
+# License: WTFPLv2
+#
+# parameters:
+# * git URL
+# * destination directory
+#
+GIT_URL="$1"
+DEST="$2"
+
+if [ -d $DEST ]; then
+	cd $DEST
+		git pull || true
+else
+	git clone $GIT_URL $DEST
+fi
