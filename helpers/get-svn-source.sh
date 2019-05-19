81678fd5f6c4c37e762c180f99cdd0d113dbb581
vanhofen <vanhofen@gmx.de>
2019-05-19 22:50:44 +0200

    - helpers: add script to get or update svn repositories

---
+++ helpers/get-svn-source.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# checkout or update an existing svn repository
+#
+# (C) 2019 vanhofen
+# License: WTFPLv2
+#
+# parameters:
+# * svn URL
+# * destination directory
+#
+SVN_URL="$1"
+DEST="$2"
+
+if [ -d $DEST ]; then
+	cd $DEST
+		svn update || true
+else
+	svn checkout $SVN_URL $DEST
+fi
