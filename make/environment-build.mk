aaf0a52e02efdb1262f5b0adba20a3d46be5274b
vanhofen <vanhofen@gmx.de>
2019-06-01 16:36:03 +0200

    - environment-build.mk: formatting REWRITE_LIBTOOL_RULES

--- make/environment-build.mk
+++ make/environment-build.mk
@@ -264,9 +264,12 @@ PKG_CONFIG_LIBDIR = $(TARGET_LIB_DIR)
 PKG_CONFIG_PATH = $(PKG_CONFIG_LIBDIR)/pkgconfig
 
 # helper-"functions":
-REWRITE_RULES          = sed -i -e "s,^libdir=.*,libdir='$(TARGET_LIB_DIR)'," -e "s,\(^dependency_libs='\| \|-L\|^dependency_libs='\)/lib,\ $(TARGET_LIB_DIR),g"
-REWRITE_LIBTOOL        = $(REWRITE_RULES) $(TARGET_LIB_DIR)
-REWRITE_LIBTOOL_STATIC = $(REWRITE_RULES) $(STATIC_LIB_DIR)
+REWRITE_LIBTOOL_RULES  = sed -i \
+				-e "s,^libdir=.*,libdir='$(TARGET_LIB_DIR)'," \
+				-e "s,\(^dependency_libs='\| \|-L\|^dependency_libs='\)/lib,\ $(TARGET_LIB_DIR),g"
+
+REWRITE_LIBTOOL        = $(REWRITE_LIBTOOL_RULES) $(TARGET_LIB_DIR)
+REWRITE_LIBTOOL_STATIC = $(REWRITE_LIBTOOL_RULES) $(STATIC_LIB_DIR)
 
 REWRITE_CONFIG_RULES   = sed -i \
 				-e "s,^prefix=.*,prefix='$(TARGET_DIR)'," \
