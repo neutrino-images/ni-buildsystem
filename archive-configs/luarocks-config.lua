1adc0b6bc1cbfbeb39ee4eade73410ff4e93b1aa
vanhofen <vanhofen@gmx.de>
2019-06-18 05:18:00 +0200

    - host-luarocks: fix luarocks-config.lua

--- archive-configs/luarocks-config.lua
+++ archive-configs/luarocks-config.lua
@@ -7,8 +7,8 @@ variables.LUA_INCDIR = getenv('TARGET_INCLUDE_DIR')
 variables.LUA_LIBDIR = getenv('TARGET_LIB_DIR')
 variables.CC = getenv('TARGET_CC')
 variables.LD = getenv('TARGET_LD')
-variables.CFLAGS = getenv('(TARGET_CFLAGS')
-variables.LDFLAGS = getenv('(TARGET_LDFLAGS')
+variables.CFLAGS = getenv('TARGET_CFLAGS')
+variables.LDFLAGS = getenv('TARGET_LDFLAGS')
 variables.LIBFLAG = [[-shared ]] .. getenv('TARGET_LDFLAGS')
 
 external_deps_dirs = { getenv('TARGET_DIR') }
