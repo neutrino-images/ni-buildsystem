acab4df1bcc0df9ff52e5eb399a59aced7a937db
vanhofen <vanhofen@gmx.de>
2019-12-21 23:43:21 +0100

    - luaexpat: bump version to 1.3.3; align url; update build

--- make/target-lua.mk
+++ make/target-lua.mk
@@ -40,15 +40,13 @@ lua: $(LUA_DEPS) $(ARCHIVE)/$(LUA_SOURCE) | $(TARGET_DIR)
 
 # -----------------------------------------------------------------------------
 
-LUAEXPAT_VER    = 1.3.0
+LUAEXPAT_VER    = 1.3.3
 LUAEXPAT_TMP    = luaexpat-$(LUAEXPAT_VER)
 LUAEXPAT_SOURCE = luaexpat-$(LUAEXPAT_VER).tar.gz
-LUAEXPAT_URL    = https://matthewwild.co.uk/projects/luaexpat
+LUAEXPAT_URL    = https://github.com/tomasguisasola/luaexpat/archive
 
 $(ARCHIVE)/$(LUAEXPAT_SOURCE):
-	$(DOWNLOAD) $(LUAEXPAT_URL)/$(LUAEXPAT_SOURCE)
-
-LUAEXPAT_PATCH  = luaexpat-makefile.patch
+	$(DOWNLOAD) $(LUAEXPAT_URL)/v$(LUAEXPAT_VER).tar.gz -O $(@)
 
 LUAEXPAT_DEPS   = expat lua
 
@@ -56,11 +54,16 @@ luaexpat: $(LUAEXPAT_DEPS) $(ARCHIVE)/$(LUAEXPAT_SOURCE) | $(TARGET_DIR)
 	$(REMOVE)/$(LUAEXPAT_TMP)
 	$(UNTAR)/$(LUAEXPAT_SOURCE)
 	$(CHDIR)/$(LUAEXPAT_TMP); \
-		$(call apply_patches, $(LUAEXPAT_PATCH)); \
+		sed -i 's|^EXPAT_INC=.*|EXPAT_INC= $(TARGET_INCLUDE_DIR)|' makefile; \
+		sed -i 's|^CFLAGS =.*|& -L$(TARGET_LIB_DIR)|' makefile; \
+		sed -i 's|^CC =.*|CC = $(TARGET_CC)|' makefile; \
 		$(BUILD_ENV) \
 		$(MAKE) \
-			PREFIX=$(TARGET_DIR); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
+			PREFIX=$(TARGET_DIR) \
+			LUA_SYS_VER=$(LUA_ABIVER); \
+		$(MAKE) install \
+			PREFIX=$(TARGET_DIR) \
+			LUA_SYS_VER=$(LUA_ABIVER)
 	$(REMOVE)/$(LUAEXPAT_TMP)
 	$(TOUCH)
 
