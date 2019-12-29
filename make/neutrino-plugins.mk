e9a29fb5aa148cc7a0540859b23a4b8a06722f93
vanhofen <vanhofen@gmx.de>
2019-12-29 01:51:12 +0100

    - neutrino-plugins: install rcu_switcher plugin to vuplus models

--- make/neutrino-plugins.mk
+++ make/neutrino-plugins.mk
@@ -52,11 +52,6 @@ ifeq ($(BOXSERIES), $(filter $(BOXSERIES), hd1 hd2))
 		--disable-rcu_switcher
 endif
 
-ifeq ($(BOXSERIES), $(filter $(BOXSERIES), vusolo4k vuduo4k vuultimo4k vuzero4k vuuno4k vuuno4kse vuduo))
-  NP_CONFIGURE_ADDITIONS += \
-		--disable-rcu_switcher
-endif
-
 # -----------------------------------------------------------------------------
 
 ifeq ($(BOXSERIES), $(filter $(BOXSERIES), hd1 hd2))
