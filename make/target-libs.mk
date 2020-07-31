b0e286ae243155f49f7c0ee8ca0f4b8288e365e3
vanhofen <vanhofen@gmx.de>
2020-07-31 23:52:26 +0200

    - libcurl: bump version to 7.71.0

--- make/target-libs.mk
+++ make/target-libs.mk
@@ -142,7 +142,7 @@ giflib: $(ARCHIVE)/$(GIFLIB_SOURCE) | $(TARGET_DIR)
 
 # -----------------------------------------------------------------------------
 
-LIBCURL_VER    = 7.70.0
+LIBCURL_VER    = 7.71.0
 LIBCURL_TMP    = curl-$(LIBCURL_VER)
 LIBCURL_SOURCE = curl-$(LIBCURL_VER).tar.bz2
 LIBCURL_SITE   = https://curl.haxx.se/download
