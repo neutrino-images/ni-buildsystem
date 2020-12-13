3f3d07b8bf38827577f00825b67f04317bea6a29
vanhofen <vanhofen@gmx.de>
2020-12-13 23:36:58 +0100

    - helpers: make console output visible again

--- make/helpers.mk
+++ make/helpers.mk
@@ -55,7 +55,7 @@ REWRITE_LIBTOOL        = $(REWRITE_LIBTOOL_RULES) $(TARGET_libdir)
 REWRITE_LIBTOOL_TAG    = rewritten=1
 
 define rewrite_libtool
-	@cd $(TARGET_libdir); \
+	cd $(TARGET_libdir); \
 	LA=$$(find . -name "*.la" -type f); \
 	for la in $${LA}; do \
 		if ! grep -q "$(REWRITE_LIBTOOL_TAG)" $${la}; then \
@@ -83,7 +83,7 @@ REWRITE_PKGCONF        = $(REWRITE_CONFIG_RULES) $(PKG_CONFIG_PATH)
 REWRITE_CONFIG_TAG     = rewritten=1
 
 define rewrite_pkgconf
-	@cd $(PKG_CONFIG_PATH); \
+	cd $(PKG_CONFIG_PATH); \
 	PC=$$(find . -name "*.pc" -type f); \
 	for pc in $${PC}; do \
 		if ! grep -q "$(REWRITE_CONFIG_TAG)" $${pc}; then \
