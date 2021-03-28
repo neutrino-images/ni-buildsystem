57d0f34020a8167373e6f3782127ee1cc09b31fd
vanhofen <vanhofen@gmx.de>
2021-03-28 21:57:31 +0200

    - remove ifenslave

--- make/ni.mk
+++ make/ni.mk
@@ -63,7 +63,6 @@ ifeq ($(BOXSERIES),$(filter $(BOXSERIES),hd2 hd5x hd6x vusolo4k vuduo4k vuduo4ks
 	$(MAKE) openvpn
 	$(MAKE) openssh
 	$(MAKE) ethtool
-	$(MAKE) ifenslave
 	$(MAKE) f2fs-tools
   ifneq ($(BOXMODEL),kronos_v2)
 	$(MAKE) bash
