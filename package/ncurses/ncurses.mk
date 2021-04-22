28f0ba13048ca8fdc9f4f8cf4449ab0549a02641
vanhofen <vanhofen@gmx.de>
2021-04-22 23:54:15 +0200

    - ncurses: move to package dir

---
+++ package/ncurses/ncurses.mk
@@ -0,0 +1,40 @@
+################################################################################
+#
+# ncurses
+#
+################################################################################
+
+NCURSES_VERSION = 6.1
+NCURSES_DIR = ncurses-$(NCURSES_VERSION)
+NCURSES_SOURCE = ncurses-$(NCURSES_VERSION).tar.gz
+NCURSES_SITE = $(GNU_MIRROR)/ncurses
+
+NCURSES_CONFIG_SCRIPTS = ncurses6-config
+
+NCURSES_CONF_OPTS = \
+	--enable-pc-files \
+	--with-pkg-config \
+	--with-pkg-config-libdir=$(libdir)/pkgconfig \
+	--with-shared \
+	--with-fallbacks='linux vt100 xterm' \
+	--disable-big-core \
+	--disable-db-install \
+	--with-progs \
+	--without-termlib \
+	--without-ticlib \
+	--without-manpages \
+	--without-tests \
+	--without-debug \
+	--without-ada \
+	--without-profile \
+	--without-cxx-binding
+
+define NCURSES_TARGET_CLEANUP
+	$(TARGET_RM) $(addprefix $(TARGET_bindir)/,captoinfo clear infocmp infotocap reset tabs tic toe)
+	$(TARGET_RM) $(addprefix $(TARGET_libdir)/,libform* libmenu* libpanel*)
+	$(TARGET_RM) $(addprefix $(TARGET_libdir)/pkgconfig/,form.pc menu.pc panel.pc)
+endef
+NCURSES_TARGET_FINALIZE_HOOKS += NCURSES_TARGET_CLEANUP
+
+ncurses: | $(TARGET_DIR)
+	$(call autotools-package)
