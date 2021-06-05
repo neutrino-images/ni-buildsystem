688fbae26a47cd4c7254768a3eb5f08307d06198
vanhofen <vanhofen@gmx.de>
2021-06-06 00:26:10 +0200

    - gnuconfig-update: fix typo

--- support/gnuconfig/gnuconfig-update.mk
+++ support/gnuconfig/gnuconfig-update.mk
@@ -12,8 +12,8 @@ GNUCONFIG_SITE = https://git.savannah.gnu.org/git
 update-gnuconfig:
 ifeq ($(NI_ADMIN),true)
 	$(REMOVE)/$(GNUCONFIG_DIR)
-	$(GET_GIT_SOURCE) $(GNUCONFIG_SITE)/$(GNUCONFIG_SOURCE) $(BUILD_DIR)/$(GNUCONFIG_SOURCE)
-	$(INSTALL_EXEC) $(BUILD_DIR)/$(GNUCONFIG_SOURCE)/config.* support/gnuconfig
+	$(GET_GIT_SOURCE) $(GNUCONFIG_SITE)/$(GNUCONFIG_SOURCE) $(BUILD_DIR)/$(GNUCONFIG_DIR)
+	$(INSTALL_EXEC) $(BUILD_DIR)/$(GNUCONFIG_DIR)/config.* support/gnuconfig
 	$(REMOVE)/$(GNUCONFIG_DIR)
 	@$(call MESSAGE,"Commit your changes in support/gnuconfig")
 endif
