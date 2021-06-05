3441ead85d278cfcc9d026d7c3b7d92e72f12344
vanhofen <vanhofen@gmx.de>
2021-06-06 00:06:09 +0200

    - gnuconfig: bump version

--- support/gnuconfig/gnuconfig-update.mk
+++ support/gnuconfig/gnuconfig-update.mk
@@ -10,8 +10,10 @@ GNUCONFIG_SOURCE = config.$(GNUCONFIG_VERSION)
 GNUCONFIG_SITE = https://git.savannah.gnu.org/git
 
 update-gnuconfig:
+ifeq ($(NI_ADMIN),true)
 	$(REMOVE)/$(GNUCONFIG_DIR)
 	$(GET_GIT_SOURCE) $(GNUCONFIG_SITE)/$(GNUCONFIG_SOURCE) $(BUILD_DIR)/$(GNUCONFIG_SOURCE)
 	$(INSTALL_EXEC) $(BUILD_DIR)/$(GNUCONFIG_SOURCE)/config.* support/gnuconfig
 	$(REMOVE)/$(GNUCONFIG_DIR)
 	@$(call MESSAGE,"Commit your changes in support/gnuconfig")
+endif
