ace254884bc493625285eeeb339751bbaa163c83
vanhofen <vanhofen@gmx.de>
2021-08-29 15:06:49 +0200

    - libtirpc: remove patches

--- package/libtirpc/libtirpc.mk
+++ package/libtirpc/libtirpc.mk
@@ -9,8 +9,6 @@ LIBTIRPC_DIR = libtirpc-$(LIBTIRPC_VERSION)
 LIBTIRPC_SOURCE = libtirpc-$(LIBTIRPC_VERSION).tar.bz2
 LIBTIRPC_SITE = https://sourceforge.net/projects/libtirpc/files/libtirpc/$(LIBTIRPC_VERSION)
 
-LIBTIRPC_AUTORECONF = YES
-
 LIBTIRPC_CONF_OPTS = \
 	--disable-gssapi
 
