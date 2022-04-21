7663a2c3f454d4ed7f1e378e8766a8aed71f3fed
vanhofen <vanhofen@gmx.de>
2022-04-21 22:10:49 +0200

    - valgrind: bump version to 3.18.1; update build

--- package/valgrind/valgrind.mk
+++ package/valgrind/valgrind.mk
@@ -4,15 +4,10 @@
 #
 ################################################################################
 
-VALGRIND_VERSION = 3.13.0
+VALGRIND_VERSION = 3.18.1
 VALGRIND_DIR = valgrind-$(VALGRIND_VERSION)
 VALGRIND_SOURCE = valgrind-$(VALGRIND_VERSION).tar.bz2
 VALGRIND_SITE = ftp://sourceware.org/pub/valgrind
-
-$(DL_DIR)/$(VALGRIND_SOURCE):
-	$(download) $(VALGRIND_SITE)/$(VALGRIND_SOURCE)
-
-VALGRIND_PATCH  = valgrind-fix-build-$(TARGET_CPU).patch
 
 VALGRIND_AUTORECONF = YES
 
@@ -20,15 +15,12 @@ VALGRIND_CONF_OPTS = \
 	--enable-only32bit \
 	--datadir=$(REMOVE_datadir)
 
-valgrind: $(DL_DIR)/$(VALGRIND_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCH))
-	$(CHDIR)/$(PKG_DIR); \
-		$(CONFIGURE); \
-		$(MAKE); \
-		make install DESTDIR=$(TARGET_DIR)
-	$(TARGET_RM) $(addprefix $(TARGET_libdir)/valgrind/,*.a *.xml)
-	$(TARGET_RM) $(addprefix $(TARGET_bindir)/,cg_* callgrind_* ms_print)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+define VALGRIND_TARGET_CLEANUP
+	$(TARGET_RM) $(addprefix $(TARGET_libdir)/valgrind/,*.a)
+	$(TARGET_RM) $(addprefix $(TARGET_libexecdir)/valgrind/,*)
+	$(TARGET_RM) $(addprefix $(TARGET_bindir)/,cg_* callgrind_* ms_print valgrind-* vgdb)
+endef
+VALGRIND_TARGET_FINALIZE_HOOKS += VALGRIND_TARGET_CLEANUP
+
+valgrind: | $(TARGET_DIR)
+	$(call autotools-package)
