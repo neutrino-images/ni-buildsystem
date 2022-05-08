a14486699d645d9956f2b0fc942817243cc56db5
vanhofen <vanhofen@gmx.de>
2022-05-08 22:29:41 +0200

    - libfuse: add closefrom patch from buildroot

--- package/libfuse/libfuse.mk
+++ package/libfuse/libfuse.mk
@@ -9,6 +9,9 @@ LIBFUSE_DIR = fuse-$(LIBFUSE_VERSION)
 LIBFUSE_SOURCE = fuse-$(LIBFUSE_VERSION).tar.gz
 LIBFUSE_SITE = https://github.com/libfuse/libfuse/releases/download/fuse-$(LIBFUSE_VERSION)
 
+# We're patching configure.ac
+LIBFUSE_AUTORECONF = YES
+
 LIBFUSE_CONF_OPTS = \
 	--datarootdir=$(REMOVE_datarootdir) \
 	--disable-static \
