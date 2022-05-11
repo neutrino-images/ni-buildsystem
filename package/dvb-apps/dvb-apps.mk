95bbca469743855365ff92fc41bd7f4962b44bd0
vanhofen <vanhofen@gmx.de>
2022-05-11 16:11:58 +0200

    - dvb-apps: align build to buildroot

--- package/dvb-apps/dvb-apps.mk
+++ package/dvb-apps/dvb-apps.mk
@@ -4,10 +4,12 @@
 #
 ################################################################################
 
-DVB_APPS_VERSION = git
-DVB_APPS_DIR = dvb-apps.$(DVB_APPS_VERSION)
-DVB_APPS_SOURCE = dvb-apps.$(DVB_APPS_VERSION)
-DVB_APPS_SITE = https://github.com/openpli-arm
+DVB_APPS_VERSION = hg
+DVB_APPS_DIR = dvb-apps
+DVB_APPS_SOURCE = dvb-apps
+DVB_APPS_SITE = https://linuxtv.org/hg
+
+DVB_APPS_CHECKOUT = 3d43b280298c39a67d1d889e01e173f52c12da35
 
 DVB_APPS_DEPENDENCIES = kernel-headers libiconv
 
@@ -16,14 +18,14 @@ DVB_APPS_MAKE_OPTS = \
 	enable_shared=no \
 	PERL5LIB=$(PKG_BUILD_DIR)/util/scan \
 
-dvb-apps: $(DVB_APPS_DEPENDENCIES) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(GET_GIT_SOURCE) $(PKG_SITE)/$(PKG_SOURCE) $(DL_DIR)/$(PKG_SOURCE)
-	$(CPDIR)/$(PKG_SOURCE)
+dvb-apps: | $(TARGET_DIR)
+	$(call DEPENDENCIES)
+	$(call DOWNLOAD,$($(PKG)_SOURCE))
+	$(call STARTUP)
+	$(call EXTRACT,$(BUILD_DIR))
 	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
+	$(CHDIR)/$($(PKG)_DIR); \
 		$(TARGET_CONFIGURE_ENV) LDLIBS="-liconv" \
 		$(MAKE) $($(PKG)_MAKE_OPTS); \
 		$(MAKE) $($(PKG)_MAKE_OPTS) install DESTDIR=$(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+	$(call TARGET_FOLLOWUP)
