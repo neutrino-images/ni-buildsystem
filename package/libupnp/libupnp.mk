e9cf9ecb0061ce5be6fb6d5c793ad97836c619bd
vanhofen <vanhofen@gmx.de>
2022-05-12 22:13:09 +0200

    - libupnp: bump version to 1.14.12

--- package/libupnp/libupnp.mk
+++ package/libupnp/libupnp.mk
@@ -4,14 +4,21 @@
 #
 ################################################################################
 
-LIBUPNP_VERSION = 1.6.25
+LIBUPNP_VERSION = 1.14.12
 LIBUPNP_DIR = libupnp-$(LIBUPNP_VERSION)
 LIBUPNP_SOURCE = libupnp-$(LIBUPNP_VERSION).tar.bz2
-LIBUPNP_SITE = http://sourceforge.net/projects/pupnp/files/pupnp/libUPnP%20$(LIBUPNP_VERSION)
+LIBUPNP_SITE = http://downloads.sourceforge.net/project/pupnp/release-$(LIBUPNP_VERSION)
 
+LIBUPNP_DEPENDENCIES += openssl
+
+LIBUPNP_CONF_ENV = \
+	ac_cv_lib_compat_ftime=no
+
+# Bind the internal miniserver socket with reuseaddr to allow clean restarts.
 LIBUPNP_CONF_OPTS = \
-	--enable-shared \
-	--disable-static
+	--disable-samples \
+	--enable-open-ssl \
+	--enable-reuseaddr
 
 libupnp: | $(TARGET_DIR)
 	$(call autotools-package)
