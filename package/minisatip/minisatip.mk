d865692dd028e0a8d2412b7528568afba855f659
vanhofen <vanhofen@gmx.de>
2022-05-13 20:25:34 +0200

    - minisatip: rework build

--- package/minisatip/minisatip.mk
+++ package/minisatip/minisatip.mk
@@ -18,20 +18,33 @@ MINISATIP_CONF_OPTS = \
 	--enable-static \
 	--disable-netcv
 
-minisatip: $(MINISATIP_DEPENDENCIES) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(GET_GIT_SOURCE) $(PKG_SITE)/$(PKG_SOURCE) $(DL_DIR)/$(PKG_SOURCE)
-	$(CPDIR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR); \
-		$(CONFIGURE); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE)
+define MINISATIP_INSTALL
 	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/minisatip $(TARGET_bindir)/minisatip
 	$(INSTALL) -d $(TARGET_datadir)/minisatip
 	$(INSTALL_COPY) $(PKG_BUILD_DIR)/html $(TARGET_datadir)/minisatip
+endef
+MINISATIP_PRE_FOLLOWUP_HOOKS += MINISATIP_INSTALL
+
+define MINISATIP_INSTALL_DEFAULTS
 	$(INSTALL) -d $(TARGET_sysconfdir)/default
 	echo 'MINISATIP_OPTS="-x 9090 -t -o /tmp/camd.socket"' > $(TARGET_sysconfdir)/default/minisatip
+endef
+MINISATIP_TARGET_FINALIZE_HOOKS += MINISATIP_INSTALL_DEFAULTS
+
+define MINISATIP_INSTALL_INIT_SCRIPT
 	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/minisatip.init $(TARGET_sysconfdir)/init.d/minisatip
 	$(UPDATE-RC.D) minisatip defaults 75 25
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+endef
+MINISATIP_TARGET_FINALIZE_HOOKS += MINISATIP_INSTALL_INIT_SCRIPT
+
+minisatip: | $(TARGET_DIR)
+	$(call DEPENDENCIES)
+	$(call DOWNLOAD,$($(PKG)_SOURCE))
+	$(call STARTUP)
+	$(call EXTRACT,$(BUILD_DIR))
+	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
+	$(CHDIR)/$($(PKG)_DIR); \
+		$(CONFIGURE); \
+		$(TARGET_CONFIGURE_ENV) \
+		$(MAKE)
+	$(call TARGET_FOLLOWUP)
