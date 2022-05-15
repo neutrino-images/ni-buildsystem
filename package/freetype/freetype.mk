56805703af58dd564bf3b95d37a24613336ae572
vanhofen <vanhofen@gmx.de>
2022-05-15 23:19:58 +0200

    - freetype: rework target

--- package/freetype/freetype.mk
+++ package/freetype/freetype.mk
@@ -9,9 +9,6 @@ FREETYPE_DIR = freetype-$(FREETYPE_VERSION)
 FREETYPE_SOURCE = freetype-$(FREETYPE_VERSION).tar.xz
 FREETYPE_SITE = https://sourceforge.net/projects/freetype/files/freetype2/$(FREETYPE_VERSION)
 
-$(DL_DIR)/$(FREETYPE_SOURCE):
-	$(download) $(FREETYPE_SITE)/$(FREETYPE_SOURCE)
-
 FREETYPE_DEPENDENCIES = zlib libpng
 
 FREETYPE_CONFIG_SCRIPTS = freetype-config
@@ -25,23 +22,28 @@ FREETYPE_CONF_OPTS = \
 	--without-harfbuzz \
 	--without-bzip2
 
-freetype: $(FREETYPE_DEPENDENCIES) $(DL_DIR)/$(FREETYPE_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		$(SED) '/^FONT_MODULES += \(type1\|cid\|pfr\|type42\|pcf\|bdf\|winfonts\|cff\)/d' modules.cfg
-	$(CHDIR)/$(PKG_DIR)/builds/unix; \
+define FREETYPE_PATCH_MODULES_CFG
+	$(SED) '/^FONT_MODULES += \(type1\|cid\|pfr\|type42\|pcf\|bdf\|winfonts\|cff\)/d' $(PKG_BUILD_DIR)/modules.cfg
+endef
+FREETYPE_POST_PATCH_HOOKS += FREETYPE_PATCH_MODULES_CFG
+
+define FREETYPE_EXECUTE_AUTOTOOLS
+	$(CHDIR)/$($(PKG)_DIR)/builds/unix; \
 		libtoolize --force --copy; \
 		aclocal -I .; \
 		autoconf
-	$(CHDIR)/$(PKG_DIR); \
-		$(CONFIGURE); \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
+endef
+FREETYPE_POST_PATCH_HOOKS += FREETYPE_EXECUTE_AUTOTOOLS
+
+define FREETYPE_LINK_FREETYPE
 	ln -sf freetype2 $(TARGET_includedir)/freetype
-	$(REWRITE_CONFIG_SCRIPTS)
-	$(REWRITE_LIBTOOL)
-	$(REMOVE)/$(PKG_DIR) \
-		$(TARGET_datadir)/aclocal
-	$(TOUCH)
+endef
+FREETYPE_TARGET_FINALIZE_HOOKS += FREETYPE_LINK_FREETYPE
+
+define FREETYPE_TARGET_CLEANUP
+	$(TARGET_RM) $(TARGET_datadir)/aclocal
+endef
+FREETYPE_TARGET_FINALIZE_HOOKS += FREETYPE_TARGET_CLEANUP
+
+freetype: | $(TARGET_DIR)
+	$(call autotools-package)
