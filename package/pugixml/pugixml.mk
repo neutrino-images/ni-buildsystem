ae6351f680c519676953a38bdec2f42a8ba59a70
vanhofen <vanhofen@gmx.de>
2022-05-23 23:34:13 +0200

    - pugixml: update build

--- package/pugixml/pugixml.mk
+++ package/pugixml/pugixml.mk
@@ -9,8 +9,5 @@ PUGIXML_DIR = pugixml-$(PUGIXML_VERSION)
 PUGIXML_SOURCE = pugixml-$(PUGIXML_VERSION).tar.gz
 PUGIXML_SITE = https://github.com/zeux/pugixml/releases/download/v$(PUGIXML_VERSION)
 
-$(DL_DIR)/$(PUGIXML_SOURCE):
-	$(download) $(PUGIXML_SITE)/$(PUGIXML_SOURCE)
-
-pugixml: $(DL_DIR)/$(PUGIXML_SOURCE) | $(TARGET_DIR)
+pugixml: | $(TARGET_DIR)
 	$(call cmake-package)
