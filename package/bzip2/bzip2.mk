5bac3d06bf16e34b4055e8a1217194ffc4fd8228
vanhofen <vanhofen@gmx.de>
2022-05-27 01:17:22 +0200

    - bzup2: rework build

--- package/bzip2/bzip2.mk
+++ package/bzip2/bzip2.mk
@@ -9,18 +9,20 @@ BZIP2_DIR = bzip2-$(BZIP2_VERSION)
 BZIP2_SOURCE = bzip2-$(BZIP2_VERSION).tar.gz
 BZIP2_SITE = https://sourceware.org/pub/bzip2
 
-$(DL_DIR)/$(BZIP2_SOURCE):
-	$(download) $(BZIP2_SITE)/$(BZIP2_SOURCE)
+define BZIP2_MAKEFILE_LIBBZ2_SO
+	mv $(PKG_BUILD_DIR)/Makefile-libbz2_so $(PKG_BUILD_DIR)/Makefile
+endef
+BZIP2_POST_PATCH_HOOKS += BZIP2_MAKEFILE_LIBBZ2_SO
 
-bzip2: $(DL_DIR)/$(BZIP2_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		mv Makefile-libbz2_so Makefile; \
+define BZIP2_TARGET_CLEANUP
+	$(TARGET_RM) $(TARGET_bindir)/bzip2
+endef
+BZIP2_TARGET_FINALIZE_HOOKS += BZIP2_TARGET_CLEANUP
+
+bzip2: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
 		$(TARGET_CONFIGURE_ENV) \
 		$(MAKE); \
 		$(MAKE) install PREFIX=$(TARGET_prefix)
-	$(TARGET_RM) $(TARGET_bindir)/bzip2
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+	$(call TARGET_FOLLOWUP)
