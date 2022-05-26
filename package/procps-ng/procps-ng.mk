a5a014f0db2aa38cc5db43dc854153d1b648c559
vanhofen <vanhofen@gmx.de>
2022-05-27 01:17:22 +0200

    - procps-ng: bump version to 4.0.0

--- package/procps-ng/procps-ng.mk
+++ package/procps-ng/procps-ng.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PROCPS_NG_VERSION = 3.3.17
-PROCPS_NG_DIR = procps-$(PROCPS_NG_VERSION)
+PROCPS_NG_VERSION = 4.0.0
+PROCPS_NG_DIR = procps-ng-$(PROCPS_NG_VERSION)
 PROCPS_NG_SOURCE = procps-ng-$(PROCPS_NG_VERSION).tar.xz
 PROCPS_NG_SITE = http://sourceforge.net/projects/procps-ng/files/Production
 
@@ -28,18 +28,22 @@ PROCPS_NG_CONF_OPTS = \
 
 # Allows unicode characters to show in 'watch'
 ifeq ($(BS_PACKAGE_NCURSES_WCHAR),y)
-  PROCPS_NG_CONF_OPTS += \
+PROCPS_NG_CONF_OPTS += \
 	--enable-watch8bit
+
+PROCPS_NG_CONF_OPTS += \
+	CPPFLAGS=-DOFF_XTRAWIDE
 endif
 
 PROCPS_NG_BINARIES = ps top
 
 define PROCPS_NG_INSTALL_BINARIES
-	for bin in $(PROCPS_NG_BINARIES); do \
-		rm -f $(TARGET_base_bindir)/$$bin; \
-		$(INSTALL_EXEC) -D $(TARGET_base_bindir).$(@F)/$$bin $(TARGET_base_bindir)/$$bin; \
-	done
-	rm -r $(TARGET_base_bindir).$(@F)
+	$(foreach binary,$($(PKG)_BINARIES),\
+		rm -f $(TARGET_base_bindir)/$(binary); \
+		$(INSTALL_EXEC) -D $(TARGET_base_bindir).$(@F)/$(binary) $(TARGET_base_bindir)/$(binary); \
+		rm -f $(TARGET_base_bindir).$(@F)/$(binary)$(sep) \
+	)
+	$(TARGET_RM) $(TARGET_base_bindir).$(@F)
 endef
 PROCPS_NG_TARGET_FINALIZE_HOOKS += PROCPS_NG_INSTALL_BINARIES
 
