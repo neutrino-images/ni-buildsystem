12bee011544bd260591a79d1e926b7aa512f5dde
vanhofen <vanhofen@gmx.de>
2022-05-27 01:17:22 +0200

    - evtest: bump version to 1.35

--- package/evtest/evtest.mk
+++ package/evtest/evtest.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EVTEST_VERSION = 1.34
+EVTEST_VERSION = 1.35
 EVTEST_DIR = evtest-evtest-$(EVTEST_VERSION)
 EVTEST_SOURCE = evtest-evtest-$(EVTEST_VERSION).tar.gz
 EVTEST_SITE = https://gitlab.freedesktop.org/libevdev/evtest/-/archive/evtest-$(EVTEST_VERSION)
