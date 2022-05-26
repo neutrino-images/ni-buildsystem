e1b8e9ea4d9b7d0908c37cacc1b696e5062567da
vanhofen <vanhofen@gmx.de>
2022-05-27 01:17:22 +0200

    - f2fs-tools: bump version to 1.15.0

--- package/f2fs-tools/f2fs-tools.mk
+++ package/f2fs-tools/f2fs-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-F2FS_TOOLS_VERSION = 1.14.0
+F2FS_TOOLS_VERSION = 1.15.0
 F2FS_TOOLS_DIR = f2fs-tools-$(F2FS_TOOLS_VERSION)
 F2FS_TOOLS_SOURCE = f2fs-tools-$(F2FS_TOOLS_VERSION).tar.gz
 F2FS_TOOLS_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs-tools.git/snapshot
