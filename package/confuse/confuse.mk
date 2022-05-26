42132ac0111ddb445fda727fa35214d973b746a2
vanhofen <vanhofen@gmx.de>
2022-05-27 01:17:22 +0200

    - confuse: bump version to 3.3

--- package/confuse/confuse.mk
+++ package/confuse/confuse.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-CONFUSE_VERSION = 3.2.2
+CONFUSE_VERSION = 3.3
 CONFUSE_DIR = confuse-$(CONFUSE_VERSION)
 CONFUSE_SOURCE = confuse-$(CONFUSE_VERSION).tar.xz
 CONFUSE_SITE = https://github.com/martinh/libconfuse/releases/download/v$(CONFUSE_VERSION)
 
 CONFUSE_CONF_OPTS = \
 	--docdir=$(REMOVE_docdir) \
+	--localedir=$(REMOVE_localedir) \
 	--enable-static \
 	--disable-shared
 
