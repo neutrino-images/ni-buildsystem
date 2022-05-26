debdf3d0a95c7aff2d63da3d063506b1376e5400
vanhofen <vanhofen@gmx.de>
2022-05-27 01:17:22 +0200

    - fbshot: rework build

--- package/fbshot/fbshot.mk
+++ package/fbshot/fbshot.mk
@@ -9,19 +9,17 @@ FBSHOT_DIR = fbshot-$(FBSHOT_VERSION)
 FBSHOT_SOURCE = fbshot-$(FBSHOT_VERSION).tar.gz
 FBSHOT_SITE = http://distro.ibiblio.org/amigolinux/download/Utils/fbshot
 
-$(DL_DIR)/$(FBSHOT_SOURCE):
-	$(download) $(FBSHOT_SITE)/$(FBSHOT_SOURCE)
-
 FBSHOT_DEPENDENCIES = libpng
 
-fbshot: $(FBSHOT_DEPENDENCIES) $(DL_DIR)/$(FBSHOT_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		$(SED) 's|	gcc |	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) |' Makefile; \
-		$(SED) '/strip fbshot/d' Makefile; \
+define FBSHOT_PATCH_MAKEFILE
+	$(SED) 's|	gcc |	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) |' $(PKG_BUILD_DIR)/Makefile
+	$(SED) '/strip fbshot/d' $(PKG_BUILD_DIR)/Makefile
+endef
+FBSHOT_POST_PATCH_HOOKS += FBSHOT_PATCH_MAKEFILE
+
+fbshot: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
 		$(MAKE); \
 		$(INSTALL_EXEC) -D fbshot $(TARGET_bindir)/fbshot
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+	$(call TARGET_FOLLOWUP)
