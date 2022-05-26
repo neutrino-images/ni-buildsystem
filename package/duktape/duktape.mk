a1edbbfbb04945adfeb60747e505e41841985c49
vanhofen <vanhofen@gmx.de>
2022-05-27 01:17:22 +0200

    - duktape: bump version to 2.7.0

--- package/duktape/duktape.mk
+++ package/duktape/duktape.mk
@@ -4,19 +4,15 @@
 #
 ################################################################################
 
-DUKTAPE_VERSION = 2.6.0
+DUKTAPE_VERSION = 2.7.0
 DUKTAPE_DIR = duktape-$(DUKTAPE_VERSION)
 DUKTAPE_SOURCE = duktape-$(DUKTAPE_VERSION).tar.xz
 DUKTAPE_SITE = 	https://github.com/svaarala/duktape/releases/download/v$(DUKTAPE_VERSION)
 
 duktape:
-	$(call DEPENDENCIES)
-	$(call DOWNLOAD,$($(PKG)_SOURCE))
-	$(call STARTUP)
-	$(call EXTRACT,$(BUILD_DIR))
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
+	$(call PREPARE)
 	$(CHDIR)/$($(PKG)_DIR); \
 		$(TARGET_CONFIGURE_ENV) \
 		$(MAKE) -f Makefile.sharedlibrary; \
-		$(MAKE) -f Makefile.sharedlibrary INSTALL_PREFIX=$(TARGET_DIR)/usr install
+		$(MAKE) -f Makefile.sharedlibrary INSTALL_PREFIX=$(TARGET_DIR)$(prefix) install
 	$(call TARGET_FOLLOWUP)
