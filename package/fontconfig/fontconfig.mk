f5361044a515c29d7c471ea2d39844a50825d3dc
vanhofen <vanhofen@gmx.de>
2022-05-27 01:17:22 +0200

    - fontconfig: bump version to 2.14.0

--- package/fontconfig/fontconfig.mk
+++ package/fontconfig/fontconfig.mk
@@ -4,14 +4,19 @@
 #
 ################################################################################
 
-FONTCONFIG_VERSION = 2.11.93
+FONTCONFIG_VERSION = 2.14.0
 FONTCONFIG_DIR = fontconfig-$(FONTCONFIG_VERSION)
-FONTCONFIG_SOURCE = fontconfig-$(FONTCONFIG_VERSION).tar.bz2
+FONTCONFIG_SOURCE = fontconfig-$(FONTCONFIG_VERSION).tar.xz
 FONTCONFIG_SITE = https://www.freedesktop.org/software/fontconfig/release
 
 FONTCONFIG_DEPENDENCIES = freetype expat
 
 FONTCONFIG_CONF_OPTS = \
+	--bindir=$(REMOVE_bindir) \
+	--datarootdir=$(REMOVE_datarootdir) \
+	--sysconfdir=$(REMOVE_sysconfdir) \
+	--with-arch=$(GNU_TARGET_NAME) \
+	--with-cache-dir=/var/cache/fontconfig \
 	--with-freetype-config=$(HOST_DIR)/bin/freetype-config \
 	--with-expat-includes=$(TARGET_includedir) \
 	--with-expat-lib=$(TARGET_libdir) \
