081ff470ab1a73825dc5371839b60fb8ddfc9da7
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:53 +0200

    - add libnl

---
+++ package/libnl/libnl.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# libnl
+#
+################################################################################
+
+LIBNL_VERSION = 3.5.0
+LIBNL_DIR = libnl-$(LIBNL_VERSION)
+LIBNL_SOURCE = libnl-$(LIBNL_VERSION).tar.gz
+LIBNL_SITE = https://github.com/thom311/libnl/releases/download/libnl$(subst .,_,$(LIBNL_VERSION))
+
+LIBNL_CONF_OPTS = \
+	--disable-cli \
+	--disable-unit-tests
+
+libnl: | $(TARGET_DIR)
+	$(call autotools-package)
