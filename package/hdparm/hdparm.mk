91b2dc2f4a73b34b9652e98fae650c9a1dfd9f27
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    - hdparm: bump version to 9.63; rework build

--- package/hdparm/hdparm.mk
+++ package/hdparm/hdparm.mk
@@ -4,21 +4,15 @@
 #
 ################################################################################
 
-HDPARM_VERSION = 9.60
+HDPARM_VERSION = 9.63
 HDPARM_DIR = hdparm-$(HDPARM_VERSION)
 HDPARM_SOURCE = hdparm-$(HDPARM_VERSION).tar.gz
 HDPARM_SITE = https://sourceforge.net/projects/hdparm/files/hdparm
 
-$(DL_DIR)/$(HDPARM_SOURCE):
-	$(download) $(HDPARM_SITE)/$(HDPARM_SOURCE)
-
-hdparm: $(DL_DIR)/$(HDPARM_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
+hdparm: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
 		$(TARGET_CONFIGURE_ENV) \
 		$(MAKE); \
 		$(MAKE) install DESTDIR=$(TARGET_DIR) mandir=$(REMOVE_mandir)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+	$(call TARGET_FOLLOWUP)
