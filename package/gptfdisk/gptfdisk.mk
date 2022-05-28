b417b9f763e964a4f7a8bc193b54ba15d99bd711
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    - gptfdisk: bump version to 1.0.9; rework build

--- package/gptfdisk/gptfdisk.mk
+++ package/gptfdisk/gptfdisk.mk
@@ -4,27 +4,26 @@
 #
 ################################################################################
 
-GPTFDISK_VERSION = 1.0.8
+GPTFDISK_VERSION = 1.0.9
 GPTFDISK_DIR = gptfdisk-$(GPTFDISK_VERSION)
 GPTFDISK_SOURCE = gptfdisk-$(GPTFDISK_VERSION).tar.gz
 GPTFDISK_SITE = https://sourceforge.net/projects/gptfdisk/files/gptfdisk/$(GPTFDISK_VERSION)
 
-$(DL_DIR)/$(GPTFDISK_SOURCE):
-	$(download) $(GPTFDISK_SITE)/$(GPTFDISK_SOURCE)
-
 GPTFDISK_DEPENDENCIES = popt e2fsprogs ncurses
 
-GPTFDISK_SBINARIES = sgdisk
+GPTFDISK_BINARIES = sgdisk
 
-gptfdisk: $(GPTFDISK_DEPENDENCIES) $(DL_DIR)/$(GPTFDISK_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
+define GPTFDISK_INSTALL_BINARIES
+	$(foreach binary,$($(PKG)_BINARIES),\
+		rm -f $(TARGET_sbindir)/$(binary); \
+		$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/$(binary) $(TARGET_sbindir)/$(binary)$(sep) \
+	)
+endef
+GPTFDISK_PRE_FOLLOWUP_HOOKS += GPTFDISK_INSTALL_BINARIES
+
+gptfdisk: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
 		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE) $($(PKG)_SBINARIES); \
-		for sbin in $($(PKG)_SBINARIES); do \
-			$(INSTALL_EXEC) -D $$sbin $(TARGET_sbindir)/$$sbin; \
-		done
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+		$(MAKE) $($(PKG)_SBINARIES)
+	$(call TARGET_FOLLOWUP)
