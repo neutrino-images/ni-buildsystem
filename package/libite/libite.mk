220df27fb48bfa04eff9a56261bca57fbe322799
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    - libite: bump version to 2.5.2

--- package/libite/libite.mk
+++ package/libite/libite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBITE_VERSION = 2.0.2
+LIBITE_VERSION = 2.5.2
 LIBITE_DIR = libite-$(LIBITE_VERSION)
 LIBITE_SOURCE = libite-$(LIBITE_VERSION).tar.xz
 LIBITE_SITE = https://github.com/troglobit/libite/releases/download/v$(LIBITE_VERSION)
