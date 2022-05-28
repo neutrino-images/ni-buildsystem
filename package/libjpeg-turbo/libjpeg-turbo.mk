daef63a5d532ae2dd8173870f46bc2f89417ec60
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    - libjpeg-turbo: bump version to 2.1.3; rework build

--- package/libjpeg-turbo/libjpeg-turbo.mk
+++ package/libjpeg-turbo/libjpeg-turbo.mk
@@ -4,22 +4,15 @@
 #
 ################################################################################
 
-LIBJPEG_TURBO_VERSION = 2.1.1
+LIBJPEG_TURBO_VERSION = 2.1.3
 LIBJPEG_TURBO_DIR = libjpeg-turbo-$(LIBJPEG_TURBO_VERSION)
 LIBJPEG_TURBO_SOURCE = libjpeg-turbo-$(LIBJPEG_TURBO_VERSION).tar.gz
 LIBJPEG_TURBO_SITE = https://sourceforge.net/projects/libjpeg-turbo/files/$(LIBJPEG_TURBO_VERSION)
 
-$(DL_DIR)/$(LIBJPEG_TURBO_SOURCE):
-	$(download) $(LIBJPEG_TURBO_SITE)/$(LIBJPEG_TURBO_SOURCE)
-
 LIBJPEG_TURBO_CONF_OPTS = \
+	-DCMAKE_INSTALL_BINDIR="$(REMOVE_bindir)" \
 	-DWITH_SIMD=False \
 	-DWITH_JPEG8=80
 
-define LIBJPEG_TURBO_TARGET_CLEANUP
-	$(TARGET_RM) $(addprefix $(TARGET_bindir)/,cjpeg djpeg jpegtran rdjpgcom tjbench wrjpgcom)
-endef
-LIBJPEG_TURBO_TARGET_FINALIZE_HOOKS += LIBJPEG_TURBO_TARGET_CLEANUP
-
-libjpeg-turbo: $(DL_DIR)/$(LIBJPEG_TURBO_SOURCE) | $(TARGET_DIR)
+libjpeg-turbo: | $(TARGET_DIR)
 	$(call cmake-package)
