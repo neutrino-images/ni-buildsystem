bc48dfb6d88d3f2df58c5cb2d3f93ab5a04aeb6b
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    - libdpf: rework build

--- package/libdpf/libdpf.mk
+++ package/libdpf/libdpf.mk
@@ -14,16 +14,16 @@ LIBDPF_DEPENDENCIES = libusb-compat
 LIBDPF_MAKE_OPTS = \
 	CC=$(TARGET_CC) PREFIX=$(TARGET_prefix)
 
-libdpf: $(LIBDPF_DEPENDENCIES) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(GET_GIT_SOURCE) $(PKG_SITE)/$(PKG_SOURCE) $(DL_DIR)/$(PKG_SOURCE)
-	$(CPDIR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		$(MAKE1) -C dpflib libdpf.a $($(PKG)_MAKE_OPTS)
+define LIBDPF_INSTALL_FILES
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/dpflib/libdpf.a $(TARGET_libdir)/libdpf.a
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/dpflib/dpf.h $(TARGET_includedir)/libdpf/libdpf.h
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/include/spiflash.h $(TARGET_includedir)/libdpf/spiflash.h
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/include/usbuser.h $(TARGET_includedir)/libdpf/usbuser.h
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+endef
+LIBDPF_PRE_FOLLOWUP_HOOKS += LIBDPF_INSTALL_FILES
+
+libdpf: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
+		$(MAKE1) -C dpflib libdpf.a $($(PKG)_MAKE_OPTS)
+	$(call TARGET_FOLLOWUP)
