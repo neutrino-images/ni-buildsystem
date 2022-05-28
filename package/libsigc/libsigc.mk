5197bcea2e359b90eec189bafad59964389b73ff
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    - libsigc++: rework build

--- package/libsigc/libsigc.mk
+++ package/libsigc/libsigc.mk
@@ -9,24 +9,17 @@ LIBSIGC_DIR = libsigc++-$(LIBSIGC_VERSION)
 LIBSIGC_SOURCE = libsigc++-$(LIBSIGC_VERSION).tar.xz
 LIBSIGC_SITE = https://download.gnome.org/sources/libsigc++/$(basename $(LIBSIGC_VERSION))
 
-$(DL_DIR)/$(LIBSIGC_SOURCE):
-	$(download) $(LIBSIGC_SITE)/$(LIBSIGC_SOURCE)
-
 LIBSIGC_CONF_OPTS = \
 	--disable-benchmark \
 	--disable-documentation \
 	--disable-warnings \
 	--without-boost
 
-libsigc: $(DL_DIR)/$(LIBSIGC_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR); \
-		$(CONFIGURE); \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR); \
-		cp sigc++config.h $(TARGET_includedir)
+define LIBSIGC_INSTALL_HEADER
+	cp $(PKG_BUILD_DIR)/sigc++config.h $(TARGET_includedir)
 	ln -sf ./sigc++-2.0/sigc++ $(TARGET_includedir)/sigc++
-	$(REWRITE_LIBTOOL)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+endef
+LIBSIGC_PRE_FOLLOWUP_HOOKS += LIBSIGC_INSTALL_HEADER
+
+libsigc: | $(TARGET_DIR)
+	$(call autotools-package)
