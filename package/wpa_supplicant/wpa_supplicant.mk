7d54051bcde40189d21e33a40d89d3b5c68cf6c2
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:53 +0200

    - wpa_supplicant: bump version to 2.10

--- package/wpa_supplicant/wpa_supplicant.mk
+++ package/wpa_supplicant/wpa_supplicant.mk
@@ -4,28 +4,36 @@
 #
 ################################################################################
 
-WPA_SUPPLICANT_VERSION = 0.7.3
+WPA_SUPPLICANT_VERSION = 2.10
 WPA_SUPPLICANT_DIR = wpa_supplicant-$(WPA_SUPPLICANT_VERSION)
 WPA_SUPPLICANT_SOURCE = wpa_supplicant-$(WPA_SUPPLICANT_VERSION).tar.gz
 WPA_SUPPLICANT_SITE = https://w1.fi/releases
 
-$(DL_DIR)/$(WPA_SUPPLICANT_SOURCE):
-	$(download) $(WPA_SUPPLICANT_SITE)/$(WPA_SUPPLICANT_SOURCE)
+WPA_SUPPLICANT_DEPENDENCIES = openssl libnl
 
-WPA_SUPPLICANT_DEPENDENCIES = openssl
+define WPA_SUPPLICANT_INSTALL_CONFIG
+	$(INSTALL_DATA) $(PKG_FILES_DIR)/wpa_supplicant.config $(PKG_BUILD_DIR)/wpa_supplicant/.config
+endef
+WPA_SUPPLICANT_POST_PATCH_HOOKS += WPA_SUPPLICANT_INSTALL_CONFIG
 
-wpa_supplicant: $(WPA_SUPPLICANT_DEPENDENCIES) $(DL_DIR)/$(WPA_SUPPLICANT_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR)/wpa_supplicant; \
-		$(INSTALL_DATA) $(PKG_FILES_DIR)/wpa_supplicant.config .config; \
+define WPA_SUPPLICANT_INSTALL_FILES
+	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/pre-wlan0.sh $(TARGET_sysconfdir)/network/pre-wlan0.sh
+	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/post-wlan0.sh $(TARGET_sysconfdir)/network/post-wlan0.sh
+	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/wpa_supplicant.conf $(TARGET_sysconfdir)/wpa_supplicant.conf
+endef
+WPA_SUPPLICANT_TARGET_FINALIZE_HOOKS += WPA_SUPPLICANT_INSTALL_FILES
+
+ifeq ($(PERSISTENT_VAR_PARTITION),yes)
+define WPA_SUPPLICANT_INSTALL_LINK
+	ln -sf /var/etc/wpa_supplicant.conf $(TARGET_sysconfdir)/wpa_supplicant.conf
+endef
+WPA_SUPPLICANT_TARGET_FINALIZE_HOOKS += WPA_SUPPLICANT_INSTALL_LINK
+endif
+
+wpa_supplicant: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR)/wpa_supplicant; \
 		$(TARGET_CONFIGURE_ENV) \
 		$(MAKE); \
 		$(MAKE) install DESTDIR=$(TARGET_DIR) BINDIR=$(sbindir)
-	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/pre-wlan0.sh $(TARGET_sysconfdir)/network/pre-wlan0.sh
-	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/post-wlan0.sh $(TARGET_sysconfdir)/network/post-wlan0.sh
-  ifeq ($(PERSISTENT_VAR_PARTITION),yes)
-	ln -sf /var/etc/wpa_supplicant.conf $(TARGET_sysconfdir)/wpa_supplicant.conf
-  endif
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+	$(call TARGET_FOLLOWUP)
