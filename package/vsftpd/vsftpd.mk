0710b8d8a873ab3ae1a6752b2d8592c0f9f6e87e
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:53 +0200

    - vsftp: bump version to 3.0.5; rework build

--- package/vsftpd/vsftpd.mk
+++ package/vsftpd/vsftpd.mk
@@ -4,32 +4,34 @@
 #
 ################################################################################
 
-VSFTPD_VERSION = 3.0.3
+VSFTPD_VERSION = 3.0.5
 VSFTPD_DIR = vsftpd-$(VSFTPD_VERSION)
 VSFTPD_SOURCE = vsftpd-$(VSFTPD_VERSION).tar.gz
 VSFTPD_SITE = https://security.appspot.com/downloads
 
-$(DL_DIR)/$(VSFTPD_SOURCE):
-	$(download) $(VSFTPD_SITE)/$(VSFTPD_SOURCE)
+VSFTPD_DEPENDENCIES = openssl
 
 VSFTPD_LIBS += -lcrypt $$($(PKG_CONFIG) --libs libssl libcrypto)
 
-VSFTPD_DEPENDENCIES = openssl
+define VSFTPD_PATCH_BUILDDEFS_H
+	$(SED) 's/.*VSF_BUILD_PAM/#undef VSF_BUILD_PAM/' $(PKG_BUILD_DIR)/builddefs.h
+	$(SED) 's/.*VSF_BUILD_SSL/#define VSF_BUILD_SSL/' $(PKG_BUILD_DIR)/builddefs.h
+endef
+VSFTPD_POST_PATCH_HOOKS += VSFTPD_PATCH_BUILDDEFS_H
 
-vsftpd: $(VSFTPD_DEPENDENCIES) $(DL_DIR)/$(VSFTPD_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		$(SED) 's/.*VSF_BUILD_PAM/#undef VSF_BUILD_PAM/' builddefs.h; \
-		$(SED) 's/.*VSF_BUILD_SSL/#define VSF_BUILD_SSL/' builddefs.h; \
-		$(MAKE) clean; \
-		$(MAKE) $(TARGET_CONFIGURE_ENV) LIBS="$($(PKG)_LIBS)"; \
-		$(INSTALL_EXEC) -D vsftpd $(TARGET_sbindir)/vsftpd
+define VSFTPD_INSTALL_FILES
 	$(INSTALL) -d $(TARGET_datadir)/empty
 	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/vsftpd.conf $(TARGET_sysconfdir)/vsftpd.conf
 	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/vsftpd.chroot_list $(TARGET_sysconfdir)/vsftpd.chroot_list
 	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/vsftpd.init $(TARGET_sysconfdir)/init.d/vsftpd
 	$(UPDATE-RC.D) vsftpd defaults 75 25
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+endef
+VSFTPD_PRE_FOLLOWUP_HOOKS += VSFTPD_INSTALL_FILES
+
+vsftpd: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
+		$(MAKE) clean; \
+		$(MAKE) $(TARGET_CONFIGURE_ENV) LIBS="$($(PKG)_LIBS)"; \
+		$(INSTALL_EXEC) -D vsftpd $(TARGET_sbindir)/vsftpd
+	$(call TARGET_FOLLOWUP)
