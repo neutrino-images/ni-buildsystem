b1491214db020cc1306749bbbbfce825163a9e22
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:53 +0200

    - luaexpat: rework build

--- package/luaexpat/luaexpat.mk
+++ package/luaexpat/luaexpat.mk
@@ -7,10 +7,7 @@
 LUAEXPAT_VERSION = 1.3.3
 LUAEXPAT_DIR = luaexpat-$(LUAEXPAT_VERSION)
 LUAEXPAT_SOURCE = luaexpat-$(LUAEXPAT_VERSION).tar.gz
-LUAEXPAT_SITE = https://github.com/tomasguisasola/luaexpat/archive
-
-$(DL_DIR)/$(LUAEXPAT_SOURCE):
-	$(download) $(LUAEXPAT_SITE)/v$(LUAEXPAT_VERSION).tar.gz -O $(@)
+LUAEXPAT_SITE = $(call github,tomasguisasola,luaexpat,v$(LUAEXPAT_VERSION))
 
 LUAEXPAT_DEPENDENCIES = expat lua
 
@@ -21,14 +18,16 @@ LUAEXPAT_MAKE_OPTS = \
 	PREFIX=$(TARGET_prefix) \
 	LUA_SYS_VER=$(LUA_ABIVERSION)
 
-luaexpat: $(LUAEXPAT_DEPENDENCIES) $(DL_DIR)/$(LUAEXPAT_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR); \
-		$(SED) 's|^EXPAT_INC=.*|EXPAT_INC= $(TARGET_includedir)|' makefile; \
-		$(SED) 's|^CFLAGS =.*|& -L$(TARGET_libdir)|' makefile; \
-		$(SED) 's|^CC =.*|CC = $(TARGET_CC)|' makefile; \
+define LUAEXPAT_PATCH_MAKEFILE
+	$(SED) 's|^EXPAT_INC=.*|EXPAT_INC= $(TARGET_includedir)|' $(PKG_BUILD_DIR)/makefile
+	$(SED) 's|^CFLAGS =.*|& -L$(TARGET_libdir)|' $(PKG_BUILD_DIR)/makefile
+	$(SED) 's|^CC =.*|CC = $(TARGET_CC)|' $(PKG_BUILD_DIR)/makefile
+endef
+LUAEXPAT_POST_PATCH_HOOKS += LUAEXPAT_PATCH_MAKEFILE
+
+luaexpat: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
 		$($(PKG)_MAKE_ENV) $(MAKE) $($(PKG)_MAKE_OPTS); \
 		$($(PKG)_MAKE_ENV) $(MAKE) $($(PKG)_MAKE_OPTS) install
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+	$(call TARGET_FOLLOWUP)
