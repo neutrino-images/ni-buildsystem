b445fcee30ccc40b7fc4826bc3147225c6a98243
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    -libexif: bump verion to 0.6.24

--- package/libexif/libexif.mk
+++ package/libexif/libexif.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBEXIF_VERSION = 0.6.22
+LIBEXIF_VERSION = 0.6.24
 LIBEXIF_DIR = libexif-$(LIBEXIF_VERSION)
-LIBEXIF_SOURCE = libexif-$(LIBEXIF_VERSION).tar.xz
-LIBEXIF_SITE = https://github.com/libexif/libexif/releases/download/libexif-$(subst .,_,$(LIBEXIF_VERSION))-release
+LIBEXIF_SOURCE = libexif-$(LIBEXIF_VERSION).tar.bz2
+LIBEXIF_SITE = https://github.com/libexif/libexif/releases/download/v$(LIBEXIF_VERSION)
 
 LIBEXIF_CONF_OPTS = \
 	--datarootdir=$(REMOVE_datarootdir) \
