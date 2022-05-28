d0a1fc3fb84a109154d333579025e66cbc5e8e40
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:53 +0200

    - zlib: rework build

--- package/zlib/zlib.mk
+++ package/zlib/zlib.mk
@@ -9,9 +9,6 @@ ZLIB_DIR = zlib-$(ZLIB_VERSION)
 ZLIB_SOURCE = zlib-$(ZLIB_VERSION).tar.xz
 ZLIB_SITE = https://sourceforge.net/projects/libpng/files/zlib/$(ZLIB_VERSION)
 
-$(DL_DIR)/$(ZLIB_SOURCE):
-	$(download) $(ZLIB_SITE)/$(ZLIB_SOURCE)
-
 ZLIB_CONF_ENV = \
 	mandir=$(REMOVE_mandir)
 
@@ -20,17 +17,14 @@ ZLIB_CONF_OPTS = \
 	--shared \
 	--uname=Linux
 
-zlib: $(DL_DIR)/$(ZLIB_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
+zlib: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
 		$(TARGET_CONFIGURE_ENV) \
 		./configure $($(PKG)_CONF_OPTS); \
 		$(MAKE); \
 		$(MAKE) install DESTDIR=$(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+	$(call TARGET_FOLLOWUP)
 
 # -----------------------------------------------------------------------------
 
@@ -38,9 +32,6 @@ HOST_ZLIB_VERSION = $(ZLIB_VERSION)
 HOST_ZLIB_DIR = $(ZLIB_DIR)
 HOST_ZLIB_SOURCE = $(ZLIB_SOURCE)
 HOST_ZLIB_SITE = $(ZLIB_SITE)
-
-#$(DL_DIR)/$(HOST_ZLIB_SOURCE):
-#	$(download) $(HOST_ZLIB_SITE)/$(HOST_ZLIB_SOURCE)
 
 #HOST_ZLIB_CONF_ENV = \
 #	libdir=$(HOST_DIR)/lib \
@@ -51,14 +42,11 @@ HOST_ZLIB_CONF_OPTS = \
 	--shared \
 	--uname=Linux
 
-host-zlib: $(DL_DIR)/$(HOST_ZLIB_SOURCE) | $(HOST_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
+host-zlib: | $(HOST_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
 		$(HOST_CONFIGURE_ENV) \
 		./configure $($(PKG)_CONF_OPTS); \
 		$(MAKE); \
 		$(MAKE) install DESTDIR=$(HOST_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+	$(call HOST_FOLLOWUP)
