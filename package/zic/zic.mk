13a7173b2a0c04de80b4ac216ceaf311c7db6bd9
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:53 +0200

    - zic: bump version to 2022a; rework build

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -4,24 +4,25 @@
 #
 ################################################################################
 
-HOST_ZIC_VERSION = 2020f
+HOST_ZIC_VERSION = 2022a
 HOST_ZIC_DIR = tzcode$(HOST_ZIC_VERSION)
 HOST_ZIC_SOURCE = tzcode$(HOST_ZIC_VERSION).tar.gz
-HOST_ZIC_SITE = ftp://ftp.iana.org/tz/releases
-
-$(DL_DIR)/$(HOST_ZIC_SOURCE):
-	$(download) $(HOST_ZIC_SITE)/$(HOST_ZIC_SOURCE)
+HOST_ZIC_SITE = https://data.iana.org/time-zones/releases
 
 HOST_ZIC = $(HOST_DIR)/sbin/zic
 
-host-zic: $(DL_DIR)/$(HOST_ZIC_SOURCE) | $(HOST_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(MKDIR)/$(PKG_DIR)
-	$(CHDIR)/$(PKG_DIR); \
-		tar -xf $(DL_DIR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		$(MAKE) zic
+define HOST_ZIC_INSTALL_BINARY
 	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/zic $(HOST_ZIC)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+endef
+HOST_ZIC_PRE_FOLLOWUP_HOOKS += HOST_ZIC_INSTALL_BINARY
+
+host-zic: | $(HOST_DIR)
+	$(call STARTUP)
+	$(call DEPENDENCIES)
+	$(call DOWNLOAD,$($(PKG)_SOURCE))
+	$(MKDIR)/$($(PKG)_DIR)
+	$(call EXTRACT,$(PKG_BUILD_DIR))
+	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
+	$(CHDIR)/$($(PKG)_DIR); \
+		$(MAKE) zic
+	$(call HOST_FOLLOWUP)
