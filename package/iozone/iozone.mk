0d324cdc08388185efe8d236bd31d3c6cf683ad3
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    - iozone: bump version to 3_493; rework build

--- package/iozone/iozone.mk
+++ package/iozone/iozone.mk
@@ -4,22 +4,32 @@
 #
 ################################################################################
 
-IOZONE_VERSION = 3_490
+IOZONE_VERSION = 3_493
 IOZONE_DIR = iozone$(IOZONE_VERSION)
-IOZONE_SOURCE = iozone$(IOZONE_VERSION).tar
+IOZONE_SOURCE = iozone$(IOZONE_VERSION).tgz
 IOZONE_SITE = http://www.iozone.org/src/current
 
-$(DL_DIR)/$(IOZONE_SOURCE):
-	$(download) $(IOZONE_SITE)/$(IOZONE_SOURCE)
+# AIO support not available on uClibc, use the linux (non-aio) target.
+ifeq ($(BOXTYPE),coolstream)
+IOZONE_TARGET = linux-noaio
+else
+IOZONE_TARGET = linux-arm
+endif
 
-iozone: $(DL_DIR)/$(IOZONE_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR)/src/current; \
-		$(SED) "s/= gcc/= $(TARGET_CC)/" makefile; \
-		$(SED) "s/= cc/= $(TARGET_CC)/" makefile; \
+define IOZONE_PATCH_MAKEFILE
+	$(SED) "s/= gcc/= $(TARGET_CC)/" $(PKG_BUILD_DIR)/src/current/makefile
+	$(SED) "s/= cc/= $(TARGET_CC)/" $(PKG_BUILD_DIR)/src/current/makefile
+endef
+IOZONE_POST_PATCH_HOOKS += IOZONE_PATCH_MAKEFILE
+
+define IOZONE_INSTALL_BINARY
+	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/src/current/iozone $(TARGET_bindir)/iozone
+endef
+IOZONE_PRE_FOLLOWUP_HOOKS += IOZONE_INSTALL_BINARY
+
+iozone: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR)/src/current; \
 		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE) linux-arm; \
-		$(INSTALL_EXEC) -D iozone $(TARGET_bindir)/iozone
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+		$(MAKE) $($(PKG)_TARGET)
+	$(call TARGET_FOLLOWUP)
