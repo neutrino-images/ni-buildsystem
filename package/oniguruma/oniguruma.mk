68b72f1ca255c8a9f5e2c2fda9e86aabdc2bec97
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:52 +0200

    - oniguruma: bump version to 6.9.8

--- package/oniguruma/oniguruma.mk
+++ package/oniguruma/oniguruma.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-ONIGURUMA_VERSION = 6.9.7.1
-ONIGURUMA_DIR = onig-$(basename $(ONIGURUMA_VERSION))
+ONIGURUMA_VERSION = 6.9.8
+ONIGURUMA_DIR = onig-$(ONIGURUMA_VERSION)
 ONIGURUMA_SOURCE = onig-$(ONIGURUMA_VERSION).tar.gz
 ONIGURUMA_SITE = https://github.com/kkos/oniguruma/releases/download/v$(ONIGURUMA_VERSION)
 
