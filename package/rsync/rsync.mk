f8e8a1e04cef9165bf2b70cb7a8d7a1ae0ffee89
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:53 +0200

    - rsync: bump version to 3.2.4

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,19 +4,22 @@
 #
 ################################################################################
 
-RSYNC_VERSION = 3.1.3
+RSYNC_VERSION = 3.2.4
 RSYNC_DIR = rsync-$(RSYNC_VERSION)
 RSYNC_SOURCE = rsync-$(RSYNC_VERSION).tar.gz
-RSYNC_SITE = https://download.samba.org/pub/rsync/src/
+RSYNC_SITE = https://download.samba.org/pub/rsync/src
 
 RSYNC_DEPENDENCIES = zlib popt
 
 RSYNC_CONF_OPTS = \
-	--disable-debug \
-	--disable-locale \
-	--disable-acl-support \
 	--with-included-zlib=no \
-	--with-included-popt=no
+	--with-included-popt=no \
+	--disable-simd \
+	--disable-openssl \
+	--disable-xxhash \
+	--disable-zstd \
+	--disable-lz4 \
+	--disable-asm
 
 rsync: | $(TARGET_DIR)
 	$(call autotools-package)
