c96e23deb74607ca3fd117a0d38358246828e898
vanhofen <vanhofen@gmx.de>
2022-05-28 15:30:53 +0200

    - popt: bump version to 1.18

--- package/popt/popt.mk
+++ package/popt/popt.mk
@@ -4,13 +4,20 @@
 #
 ################################################################################
 
-POPT_VERSION = 1.16
+POPT_VERSION = 1.18
 POPT_DIR = popt-$(POPT_VERSION)
 POPT_SOURCE = popt-$(POPT_VERSION).tar.gz
-POPT_SITE = ftp://anduin.linuxfromscratch.org/BLFS/popt
+POPT_SITE = http://ftp.rpm.org/popt/releases/popt-1.x
+
+POPT_DEPENDENCIES = libiconv
+
+POPT_CONF_ENV = \
+	ac_cv_va_copy=yes \
+	am_cv_lib_iconv=yes
 
 POPT_CONF_OPTS = \
-	--datarootdir=$(REMOVE_datarootdir)
+	--datarootdir=$(REMOVE_datarootdir) \
+	--with-libiconv-prefix=$(TARGET_prefix)
 
 popt: | $(TARGET_DIR)
 	$(call autotools-package)
