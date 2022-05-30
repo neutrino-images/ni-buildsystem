d082bd487ca8ae3f64e15429fc4357e2fe3d8c91
vanhofen <vanhofen@gmx.de>
2022-05-30 23:34:24 +0200

    - add libudev-zero

---
+++ package/libudev-zero/libudev-zero.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# libudev-tero
+#
+################################################################################
+
+LIBUDEV_ZERO_VERSION = 1.0.1
+LIBUDEV_ZERO_DIR = libudev-zero-$(LIBUDEV_ZERO_VERSION)
+LIBUDEV_ZERO_SOURCE = libudev-zero-$(LIBUDEV_ZERO_VERSION).tar.gz
+LIBUDEV_ZERO_SITE = $(call github,illiliti,libudev-zero,$(LIBUDEV_ZERO_VERSION))
+
+LIBUDEV_ZERO_MAKE_OPTS = \
+	PREFIX=$(prefix)
+
+libudev-zero: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
+		$(MAKE) $($(PKG)_MAKE_OPTS); \
+		$(MAKE) $($(PKG)_MAKE_OPTS) install DESTDIR=$(TARGET_DIR)
+	$(call TARGET_FOLLOWUP)
