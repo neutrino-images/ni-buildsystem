85bb9177c5daff6753afe2487d1a79f5e2f9a2ce
vanhofen <vanhofen@gmx.de>
2022-05-30 23:01:54 +0200

    - e2fsprogs: rework build

--- package/e2fsprogs/e2fsprogs.mk
+++ package/e2fsprogs/e2fsprogs.mk
@@ -10,9 +10,6 @@ E2FSPROGS_DIR = e2fsprogs-$(E2FSPROGS_VERSION)
 E2FSPROGS_SOURCE = e2fsprogs-$(E2FSPROGS_VERSION).tar.gz
 E2FSPROGS_SITE = https://sourceforge.net/projects/e2fsprogs/files/e2fsprogs/v$(E2FSPROGS_VERSION)
 
-$(DL_DIR)/$(E2FSPROGS_SOURCE):
-	$(download) $(E2FSPROGS_SITE)/$(E2FSPROGS_SOURCE)
-
 # Use libblkid and libuuid from util-linux
 E2FSPROGS_DEPENDENCIES = util-linux
 
@@ -53,18 +50,20 @@ E2FSPROGS_CONF_OPTS = \
 	--with-gnu-ld \
 	--with-crond-dir=no
 
-e2fsprogs: $(E2FSPROGS_DEPENDENCIES) $(DL_DIR)/$(E2FSPROGS_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR); \
-		$(CONFIGURE); \
-		$(MAKE); \
-		$(MAKE1) install install-libs DESTDIR=$(TARGET_DIR)
+define E2FSPROGS_TARGET_CLEANUP
 	$(TARGET_RM) $(addprefix $(TARGET_base_sbindir)/,dumpe2fs e2mmpstatus e2undo logsave)
 	$(TARGET_RM) $(addprefix $(TARGET_bindir)/,chattr compile_et lsattr mk_cmds uuidgen)
 	$(TARGET_RM) $(addprefix $(TARGET_sbindir)/,e2freefrag e4crypt filefrag)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+endef
+E2FSPROGS_TARGET_FINALIZE_HOOKS += E2FSPROGS_TARGET_CLEANUP
+
+e2fsprogs: | $(TARGET_DIR)
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
+		$(CONFIGURE); \
+		$(MAKE); \
+		$(MAKE1) install install-libs DESTDIR=$(TARGET_DIR)
+	$(call TARGET_FOLLOWUP)
 
 # -----------------------------------------------------------------------------
 
@@ -73,19 +72,9 @@ HOST_E2FSPROGS_DIR = $(E2FSPROGS_DIR)
 HOST_E2FSPROGS_SOURCE = $(E2FSPROGS_SOURCE)
 HOST_E2FSPROGS_SITE = $(E2FSPROGS_SITE)
 
-#$(DL_DIR)/$(HOST_E2FSPROGS_SOURCE):
-#	$(download) $(HOST_E2FSPROGS_SITE)/$(HOST_E2FSPROGS_SOURCE)
-
 HOST_E2FSPROGS_CONF_OPTS = \
 	--enable-symlink-install \
 	--with-crond-dir=no
 
-host-e2fsprogs: $(DL_DIR)/$(HOST_E2FSPROGS_SOURCE) | $(HOST_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR); \
-		$(HOST_CONFIGURE);\
-		$(MAKE); \
-		$(MAKE) install
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+host-e2fsprogs: | $(HOST_DIR)
+	$(call host-autotools-package)
