f6ab3f5523691e15964516f99e5684fda9322b20
vanhofen <vanhofen@gmx.de>
2022-05-30 21:33:42 +0200

    - luarocks: move to package dir; fix build

--- make/host-tools.mk
+++ make/host-tools.mk
@@ -147,48 +147,6 @@ host-python3-setuptools: $(HOST_PYTHON3_SETUPTOOLS_DEPENDENCIES) $(DL_DIR)/$(HOS
 
 # -----------------------------------------------------------------------------
 
-HOST_LUAROCKS_VERSION = 3.1.3
-HOST_LUAROCKS_DIR = luarocks-$(HOST_LUAROCKS_VERSION)
-HOST_LUAROCKS_SOURCE = luarocks-$(HOST_LUAROCKS_VERSION).tar.gz
-HOST_LUAROCKS_SITE = https://luarocks.github.io/luarocks/releases
-
-$(DL_DIR)/$(HOST_LUAROCKS_SOURCE):
-	$(download) $(HOST_LUAROCKS_SITE)/$(HOST_LUAROCKS_SOURCE)
-
-HOST_LUAROCKS_DEPENDENCIES = host-lua
-
-HOST_LUAROCKS_CONFIG = $(HOST_DIR)/etc/luarocks/config-$(LUA_ABIVERSION).lua
-
-HOST_LUAROCKS_MAKE_ENV = \
-	LUA_PATH="$(HOST_DIR)/share/lua/$(LUA_ABIVERSION)/?.lua" \
-	TARGET_CC="$(TARGET_CC)" \
-	TARGET_LD="$(TARGET_LD)" \
-	TARGET_CFLAGS="$(TARGET_CFLAGS) -fPIC" \
-	TARGET_LDFLAGS="-L$(TARGET_libdir)" \
-	TARGET_DIR="$(TARGET_DIR)" \
-	TARGET_includedir="$(TARGET_includedir)" \
-	TARGET_libdir="$(TARGET_libdir)"
-
-HOST_LUAROCKS_CONF_OPTS = \
-	--with-lua=$(HOST_DIR) \
-	--rocks-tree=$(TARGET_DIR)
-
-HOST_LUAROCKS = $(HOST_DIR)/bin/luarocks
-
-host-luarocks: $(HOST_LUAROCKS_DEPENDENCIES) $(DL_DIR)/$(HOST_LUAROCKS_SOURCE) | $(HOST_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		$(HOST_CONFIGURE);\
-		$(MAKE); \
-		$(MAKE) install
-	cat $(PKG_FILES_DIR)/luarocks-config.lua >> $(HOST_LUAROCKS_CONFIG)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
-
-# -----------------------------------------------------------------------------
-
 # helper target to create ccache links
 
 ifndef CCACHE
---
+++ package/luarocks/luarocks.mk
@@ -0,0 +1,43 @@
+################################################################################
+#
+# luarocks
+#
+################################################################################
+
+HOST_LUAROCKS_VERSION = 3.1.3
+HOST_LUAROCKS_DIR = luarocks-$(HOST_LUAROCKS_VERSION)
+HOST_LUAROCKS_SOURCE = luarocks-$(HOST_LUAROCKS_VERSION).tar.gz
+HOST_LUAROCKS_SITE = https://luarocks.github.io/luarocks/releases
+
+HOST_LUAROCKS_DEPENDENCIES = host-lua
+
+HOST_LUAROCKS_CONFIG = $(HOST_DIR)/etc/luarocks/config-$(LUA_ABIVERSION).lua
+
+HOST_LUAROCKS_MAKE_ENV = \
+	LUA_PATH="$(HOST_DIR)/share/lua/$(LUA_ABIVERSION)/?.lua" \
+	TARGET_CC="$(TARGET_CC)" \
+	TARGET_LD="$(TARGET_LD)" \
+	TARGET_CFLAGS="$(TARGET_CFLAGS) -fPIC" \
+	TARGET_LDFLAGS="-L$(TARGET_libdir)" \
+	TARGET_DIR="$(TARGET_DIR)" \
+	TARGET_includedir="$(TARGET_includedir)" \
+	TARGET_libdir="$(TARGET_libdir)"
+
+HOST_LUAROCKS_CONF_OPTS = \
+	--with-lua=$(HOST_DIR) \
+	--rocks-tree=$(TARGET_DIR)
+
+define HOST_LUAROCKS_REMOVE_CONFIG
+	rm -f $(HOST_LUAROCKS_CONFIG)
+endef
+HOST_LUAROCKS_POST_PATCH_HOOKS += HOST_LUAROCKS_REMOVE_CONFIG
+
+define HOST_LUAROCKS_CREATE_CONFIG
+	cat $(PKG_FILES_DIR)/luarocks-config.lua >> $(HOST_LUAROCKS_CONFIG)
+endef
+HOST_LUAROCKS_HOST_FINALIZE_HOOKS += HOST_LUAROCKS_CREATE_CONFIG
+
+HOST_LUAROCKS = $(HOST_DIR)/bin/luarocks
+
+host-luarocks: | $(HOST_DIR)
+	$(call host-autotools-package)
