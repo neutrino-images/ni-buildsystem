37cdc89ca94b0df41ee9da08c2e287a065c1f492
vanhofen <vanhofen@gmx.de>
2022-05-30 23:43:43 +0200

    - usbutils: bump version to 014; rework build

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -4,29 +4,17 @@
 #
 ################################################################################
 
-# usbutils-008 needs udev
-USBUTILS_VERSION = 007
+USBUTILS_VERSION = 014
 USBUTILS_DIR = usbutils-$(USBUTILS_VERSION)
 USBUTILS_SOURCE = usbutils-$(USBUTILS_VERSION).tar.xz
 USBUTILS_SITE = $(KERNEL_MIRROR)/linux/utils/usb/usbutils
 
-$(DL_DIR)/$(USBUTILS_SOURCE):
-	$(download) $(USBUTILS_SITE)/$(USBUTILS_SOURCE)
+USBUTILS_DEPENDENCIES = libusb-compat libudev-zero
 
-USBUTILS_DEPENDENCIES = libusb-compat
+define USBUTILS_TARGET_CLEANUP
+	$(TARGET_RM) $(addprefix $(TARGET_bindir)/,lsusb.py usbhid-dump)
+endef
+USBUTILS_TARGET_FINALIZE_HOOKS += USBUTILS_TARGET_CLEANUP
 
-usbutils: $(USBUTILS_DEPENDENCIES) $(DL_DIR)/$(USBUTILS_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		$(CONFIGURE); \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
-	$(TARGET_RM) $(TARGET_bindir)/lsusb.py
-	$(TARGET_RM) $(TARGET_bindir)/usbhid-dump
-	$(TARGET_RM) $(TARGET_sbindir)/update-usbids.sh
-	$(TARGET_RM) $(TARGET_datadir)/pkgconfig
-	$(TARGET_RM) $(TARGET_datadir)/usb.ids.gz
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+usbutils: | $(TARGET_DIR)
+	$(call autotools-package)
