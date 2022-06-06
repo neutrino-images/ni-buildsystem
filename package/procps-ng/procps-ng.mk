536c1a8fc7153242d7248003682717fea374c805
vanhofen <vanhofen@gmx.de>
2022-06-06 20:59:32 +0200

    - procps-ns: back to 3.3.17 for coolstream because of missing xlocale

--- package/procps-ng/procps-ng.mk
+++ package/procps-ng/procps-ng.mk
@@ -4,8 +4,12 @@
 #
 ################################################################################
 
-PROCPS_NG_VERSION = 4.0.0
-PROCPS_NG_DIR = procps-ng-$(PROCPS_NG_VERSION)
+PROCPS_NG_VERSION = $(if $(filter $(BOXTYPE),coolstream),3.3.17,4.0.0)
+ifeq ($(BOXTYPE),coolstream)
+PROCPS_NG_DIR = procps-$(PROCPS_NG_VERSION)
+else
+PROCPS_NG_DIR = procps-ng-$(PROCPS_NG_VERSION))
+endif
 PROCPS_NG_SOURCE = procps-ng-$(PROCPS_NG_VERSION).tar.xz
 PROCPS_NG_SITE = http://sourceforge.net/projects/procps-ng/files/Production
 
@@ -24,6 +28,7 @@ PROCPS_NG_CONF_OPTS = \
 	--libdir=$(libdir) \
 	--bindir=$(base_bindir).$(@F) \
 	--docdir=$(REMOVE_docdir) \
+	--disable-numa \
 	--without-systemd
 
 # Allows unicode characters to show in 'watch'
