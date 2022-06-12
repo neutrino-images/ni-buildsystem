6552196d07847e9c3edbd257fe0396f6bd2d15d5
vanhofen <vanhofen@gmx.de>
2022-06-12 22:49:58 +0200

    - libusb: back to v1.0.23 for coolstream

--- package/libusb/libusb.mk
+++ package/libusb/libusb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUSB_VERSION = 1.0.26
+LIBUSB_VERSION = $(if $(filter $(BOXTYPE),coolstream),1.0.23,1.0.26)
 LIBUSB_DIR = libusb-$(LIBUSB_VERSION)
 LIBUSB_SOURCE = libusb-$(LIBUSB_VERSION).tar.bz2
 LIBUSB_SITE = https://github.com/libusb/libusb/releases/download/v$(LIBUSB_VERSION)
