bba32e62141763226f938c18bc9c6faeb040e8c0
vanhofen <vanhofen@gmx.de>
2022-06-19 21:48:30 +0200

    - logo-addon: switch to individual package

--- package/channellogos/logo-addon.mk
+++ package/channellogos/logo-addon.mk
@@ -4,8 +4,18 @@
 #
 ################################################################################
 
-logo-addon: $(SOURCE_DIR)/$(NI_LOGO_STUFF) $(SHARE_PLUGINS)
-	$(INSTALL_EXEC) $(SOURCE_DIR)/$(NI_LOGO_STUFF)/logo-addon/*.sh $(SHARE_PLUGINS)/
-	$(INSTALL_DATA) $(SOURCE_DIR)/$(NI_LOGO_STUFF)/logo-addon/*.cfg $(SHARE_PLUGINS)/
-	$(INSTALL_DATA) $(SOURCE_DIR)/$(NI_LOGO_STUFF)/logo-addon/*.png $(SHARE_PLUGINS)/
-	$(TOUCH)
+LOGO_ADDON_VERSION = ni-git
+LOGO_ADDON_DIR = $(NI_LOGO_STUFF)
+LOGO_ADDON_SOURCE = $(NI_LOGO_STUFF)
+LOGO_ADDON_SITE = https://github.com/neutrino-images
+
+define LOGO_ADDON_INSTALL
+	$(INSTALL) -d $(SHARE_PLUGINS)
+	$(INSTALL_EXEC) $(PKG_BUILD_DIR)/logo-addon/logo-addon.sh $(SHARE_PLUGINS)/
+	$(INSTALL_DATA) $(PKG_BUILD_DIR)/logo-addon/logo-addon.cfg $(SHARE_PLUGINS)/
+	$(INSTALL_DATA) $(PKG_BUILD_DIR)/logo-addon/logo-addon_hint.png $(SHARE_PLUGINS)/
+endef
+LOGO_ADDON_INDIVIDUAL_HOOKS += LOGO_ADDON_INSTALL
+
+logo-addon: | $(TARGET_DIR)
+	$(call individual-package)
