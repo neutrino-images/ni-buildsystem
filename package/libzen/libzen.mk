11d4665563c597cf80b5afc2b176502a66d92560
vanhofen <vanhofen@gmx.de>
2022-06-21 19:46:51 +0200

    - lbzen: switch to package handling

--- package/libzen/libzen.mk
+++ package/libzen/libzen.mk
@@ -9,20 +9,11 @@ LIBZEN_DIR = ZenLib
 LIBZEN_SOURCE = libzen_$(LIBZEN_VERSION).tar.bz2
 LIBZEN_SITE = https://mediaarea.net/download/source/libzen/$(LIBZEN_VERSION)
 
-$(DL_DIR)/$(LIBZEN_SOURCE):
-	$(download) $(LIBZEN_SITE)/$(LIBZEN_SOURCE)
+LIBZEN_SUBDIR = Project/GNU/Library
 
 LIBZEN_DEPENDENCIES = zlib
 
 LIBZEN_AUTORECONF = YES
 
-libzen: $(LIBZEN_DEPENDENCIES) $(DL_DIR)/$(LIBZEN_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR)/Project/GNU/Library; \
-		$(TARGET_CONFIGURE); \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
-	$(REWRITE_LIBTOOL)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+libzen: | $(TARGET_DIR)
+	$(call autotools-package)
