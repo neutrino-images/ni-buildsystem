a8e49fc4e7b4841d19e0b14aacfeb05c197f9e18
vanhofen <vanhofen@gmx.de>
2022-06-21 20:37:04 +0200

    - libmediainfoq: switch to package handling

--- package/libmediainfo/libmediainfo.mk
+++ package/libmediainfo/libmediainfo.mk
@@ -9,20 +9,11 @@ LIBMEDIAINFO_DIR = MediaInfoLib
 LIBMEDIAINFO_SOURCE = libmediainfo_$(LIBMEDIAINFO_VERSION).tar.bz2
 LIBMEDIAINFO_SITE = https://mediaarea.net/download/source/libmediainfo/$(LIBMEDIAINFO_VERSION)
 
-$(DL_DIR)/$(LIBMEDIAINFO_SOURCE):
-	$(download) $(LIBMEDIAINFO_SITE)/$(LIBMEDIAINFO_SOURCE)
+LIBMEDIAINFO_SUBDIR = Project/GNU/Library
 
 LIBMEDIAINFO_DEPENDENCIES = libzen
 
 LIBMEDIAINFO_AUTORECONF = YES
 
-libmediainfo: $(LIBMEDIAINFO_DEPENDENCIES) $(DL_DIR)/$(LIBMEDIAINFO_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(CHDIR)/$(PKG_DIR)/Project/GNU/Library; \
-		$(TARGET_CONFIGURE); \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
-	$(REWRITE_LIBTOOL)
-	$(REMOVE)/$(PKG_DIR)
-	$(TOUCH)
+libmediainfo: | $(TARGET_DIR)
+	$(call autotools-package)
