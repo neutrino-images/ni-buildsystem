a15a057ffbb7484d760885bc4760334bd9b3902a
vanhofen <vanhofen@gmx.de>
2022-06-24 21:29:10 +0200

    - add libfuse3; first meson package for testing

---
+++ package/libfuse3/libfuse3.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# libfuse3
+#
+################################################################################
+
+LIBFUSE3_VERSION = 3.11.0
+LIBFUSE3_DIR     = fuse-$(LIBFUSE3_VERSION)
+LIBFUSE3_SOURCE  = fuse-$(LIBFUSE3_VERSION).tar.xz
+LIBFUSE3_SITE    = https://github.com/libfuse/libfuse/releases/download/fuse-$(LIBFUSE3_VERSION)
+
+LIBFUSE3_CONF_OPTS = \
+	-Ddisable-mtab=true \
+	-Dudevrulesdir=/dev/null \
+	-Dutils=false \
+	-Dexamples=false \
+	-Duseroot=false
+
+libfuse3: | $(TARGET_DIR)
+	$(call meson-package)
