a5b53203e617a21f17ad3e3afbc8e87b34a951e7
vanhofen <vanhofen@gmx.de>
2022-06-26 00:01:59 +0200

    - mtools: bump to version 4.0.40

--- package/mtools/mtools.mk
+++ package/mtools/mtools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MTOOLS_VERSION = 4.0.39
+MTOOLS_VERSION = 4.0.40
 MTOOLS_DIR = mtools-$(MTOOLS_VERSION)
 MTOOLS_SOURCE = mtools-$(MTOOLS_VERSION).tar.gz
 MTOOLS_SITE = $(GNU_MIRROR)/mtools
@@ -17,9 +17,4 @@ HOST_MTOOLS_SOURCE = $(MTOOLS_SOURCE)
 HOST_MTOOLS_SITE = $(MTOOLS_SITE)
 
 host-mtools: | $(HOST_DIR)
-	$(call PREPARE)
-	$(call HOST_CONFIGURE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE1); \
-		$(MAKE) install
-	$(call HOST_FOLLOWUP)
+	$(call host-autotools-package)
