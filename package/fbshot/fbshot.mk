f1232ee3a978e674996bd8fa5f67a0ae861342f7
vanhofen <vanhofen@gmx.de>
2022-06-26 00:10:59 +0200

    - fbshot: reowrk build

--- package/fbshot/fbshot.mk
+++ package/fbshot/fbshot.mk
@@ -14,12 +14,10 @@ FBSHOT_DEPENDENCIES = libpng
 define FBSHOT_PATCH_MAKEFILE
 	$(SED) 's|	gcc |	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) |' $(PKG_BUILD_DIR)/Makefile
 	$(SED) '/strip fbshot/d' $(PKG_BUILD_DIR)/Makefile
+	$(SED) 's|/usr/bin/fbshot|$$(DESTDIR)/usr/bin/fbshot|' $(PKG_BUILD_DIR)/Makefile
+	$(SED) '/install fbshot.1.man/d' $(PKG_BUILD_DIR)/Makefile
 endef
 FBSHOT_POST_PATCH_HOOKS += FBSHOT_PATCH_MAKEFILE
 
 fbshot: | $(TARGET_DIR)
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE); \
-		$(INSTALL_EXEC) -D fbshot $(TARGET_bindir)/fbshot
-	$(call TARGET_FOLLOWUP)
+	$(call generic-package)
