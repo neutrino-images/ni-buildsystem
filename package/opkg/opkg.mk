af6509728b4665b5fc69d4c42bd6d3d651f23070
vanhofen <vanhofen@gmx.de>
2022-06-27 22:04:17 +0200

    - add opkg

---
+++ package/opkg/opkg.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# opkg
+#
+################################################################################
+
+OPKG_VERSION = 0.4.5
+OPKG_DIR = opkg-$(OPKG_VERSION)
+OPKG_SOURCE = opkg-$(OPKG_VERSION).tar.gz
+OPKG_SITE = https://downloads.yoctoproject.org/releases/opkg
+
+OPKG_DEPENDENCIES = libarchive
+
+OPKG_CONF_OPTS = \
+	--datarootdir=$(REMOVE_datarootdir) \
+	--disable-curl \
+	--disable-gpg
+
+define OPKG_INSTALL_DIRECTORIES
+	$(INSTALL) -d $(TARGET_libdir)/opkg
+	$(INSTALL) -d $(TARGET_sysconfdir)/opkg
+endef
+OPKG_TARGET_FINALIZE_HOOKS += OPKG_INSTALL_DIRECTORIES
+
+opkg: | $(TARGET_DIR)
+	$(call autotools-package)
