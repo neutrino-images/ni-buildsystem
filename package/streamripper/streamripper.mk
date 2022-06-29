161876ca2ea1ca1bcca92e6f878a4b8a6442cdcd
vanhofen <vanhofen@gmx.de>
2022-06-29 23:27:19 +0200

    - streamripper: update build

--- package/streamripper/streamripper.mk
+++ package/streamripper/streamripper.mk
@@ -15,7 +15,6 @@ STREAMRIPPER_AUTORECONF = YES
 
 STREAMRIPPER_CONF_OPTS = \
 	--includedir=$(TARGET_includedir) \
-	--datarootdir=$(REMOVE_datarootdir) \
 	--with-ogg-includes=$(TARGET_includedir) \
 	--with-ogg-libraries=$(TARGET_libdir) \
 	--with-vorbis-includes=$(TARGET_includedir) \
@@ -23,19 +22,10 @@ STREAMRIPPER_CONF_OPTS = \
 	--with-included-argv=yes \
 	--with-included-libmad=no
 
-define STREAMRIPPER_INSTALL_BINARY
-	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/streamripper $(TARGET_bindir)/streamripper
-endef
-STREAMRIPPER_PRE_FOLLOWUP_HOOKS += STREAMRIPPER_INSTALL_BINARY
-
 define STREAMRIPPER_INSTALL_SCRIPT
 	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/streamripper.sh $(TARGET_bindir)/streamripper.sh
 endef
 STREAMRIPPER_TARGET_FINALIZE_HOOKS += STREAMRIPPER_INSTALL_SCRIPT
 
 streamripper: | $(TARGET_DIR)
-	$(call PREPARE)
-	$(call TARGET_CONFIGURE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE)
-	$(call TARGET_FOLLOWUP)
+	$(call autotools-package)
