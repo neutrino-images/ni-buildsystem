bba670c4b78160491cf2123d60855032c05efd01
vanhofen <vanhofen@gmx.de>
2022-06-29 23:15:56 +0200

    - hdparm: update build

--- package/hdparm/hdparm.mk
+++ package/hdparm/hdparm.mk
@@ -9,10 +9,11 @@ HDPARM_DIR = hdparm-$(HDPARM_VERSION)
 HDPARM_SOURCE = hdparm-$(HDPARM_VERSION).tar.gz
 HDPARM_SITE = https://sourceforge.net/projects/hdparm/files/hdparm
 
+HDPARM_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
+HDPARM_MAKE_OPTS = \
+	mandir=$(REMOVE_mandir)
+
 hdparm: | $(TARGET_DIR)
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR) mandir=$(REMOVE_mandir)
-	$(call TARGET_FOLLOWUP)
+	$(call generic-package)
