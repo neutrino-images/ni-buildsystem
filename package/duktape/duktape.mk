75655c7a8202918804bd963e325177fbcdbfed49
vanhofen <vanhofen@gmx.de>
2022-06-30 23:02:33 +0200

    - duktape: update build

--- package/duktape/duktape.mk
+++ package/duktape/duktape.mk
@@ -7,12 +7,13 @@
 DUKTAPE_VERSION = 2.7.0
 DUKTAPE_DIR = duktape-$(DUKTAPE_VERSION)
 DUKTAPE_SOURCE = duktape-$(DUKTAPE_VERSION).tar.xz
-DUKTAPE_SITE = 	https://github.com/svaarala/duktape/releases/download/v$(DUKTAPE_VERSION)
+DUKTAPE_SITE = https://github.com/svaarala/duktape/releases/download/v$(DUKTAPE_VERSION)
+
+DUKTAPE_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
+DUKTAPE_MAKE_OPTS = \
+	-f Makefile.sharedlibrary INSTALL_PREFIX=$(prefix)
 
 duktape:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE) -f Makefile.sharedlibrary; \
-		$(MAKE) -f Makefile.sharedlibrary INSTALL_PREFIX=$(TARGET_DIR)$(prefix) install
-	$(call TARGET_FOLLOWUP)
+	$(call generic-package)
