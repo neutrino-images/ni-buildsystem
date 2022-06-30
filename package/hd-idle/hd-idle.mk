85daafbaf1eafa95a8d7cdb6c3c216818b85ac30
vanhofen <vanhofen@gmx.de>
2022-06-30 22:58:33 +0200

    - hd-idle: upate build

--- package/hd-idle/hd-idle.mk
+++ package/hd-idle/hd-idle.mk
@@ -9,14 +9,13 @@ HD_IDLE_DIR = hd-idle
 HD_IDLE_SOURCE = hd-idle-$(HD_IDLE_VERSION).tgz
 HD_IDLE_SITE = https://sourceforge.net/projects/hd-idle/files
 
+HD_IDLE_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
 define HD_IDLE_INSTALL_BINARY
 	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/hd-idle $(TARGET_sbindir)/hd-idle
 endef
 HD_IDLE_PRE_FOLLOWUP_HOOKS += HD_IDLE_INSTALL_BINARY
 
 hd-idle: | $(TARGET_DIR)
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE)
-	$(call TARGET_FOLLOWUP)
+	$(call generic-package,$(PKG_NO_INSTALL))
