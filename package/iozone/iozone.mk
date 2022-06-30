30a97d82e36eb18995ad37a76e1cbb3bfc8a1484
vanhofen <vanhofen@gmx.de>
2022-06-30 22:44:50 +0200

    - iozone: update build

--- package/iozone/iozone.mk
+++ package/iozone/iozone.mk
@@ -9,6 +9,8 @@ IOZONE_DIR = iozone$(IOZONE_VERSION)
 IOZONE_SOURCE = iozone$(IOZONE_VERSION).tgz
 IOZONE_SITE = http://www.iozone.org/src/current
 
+IOZONE_SUBDIR = src/current
+
 # AIO support not available on uClibc, use the linux (non-aio) target.
 ifeq ($(BOXTYPE),coolstream)
 IOZONE_TARGET = linux-noaio
@@ -16,20 +18,22 @@ else
 IOZONE_TARGET = linux-arm
 endif
 
+IOZONE_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
+IOZONE_MAKE_OPTS = \
+	$(IOZONE_TARGET)
+
 define IOZONE_PATCH_MAKEFILE
-	$(SED) "s/= gcc/= $(TARGET_CC)/" $(PKG_BUILD_DIR)/src/current/makefile
-	$(SED) "s/= cc/= $(TARGET_CC)/" $(PKG_BUILD_DIR)/src/current/makefile
+	$(SED) "s/= gcc/= $(TARGET_CC)/" $(PKG_BUILD_DIR)/makefile
+	$(SED) "s/= cc/= $(TARGET_CC)/" $(PKG_BUILD_DIR)/makefile
 endef
 IOZONE_POST_PATCH_HOOKS += IOZONE_PATCH_MAKEFILE
 
 define IOZONE_INSTALL_BINARY
-	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/src/current/iozone $(TARGET_bindir)/iozone
+	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/iozone $(TARGET_bindir)/iozone
 endef
 IOZONE_PRE_FOLLOWUP_HOOKS += IOZONE_INSTALL_BINARY
 
 iozone: | $(TARGET_DIR)
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR)/src/current; \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE) $($(PKG)_TARGET)
-	$(call TARGET_FOLLOWUP)
+	$(call generic-package,$(PKG_NO_INSTALL))
