afdebd57bd8964595dece94ac02b24186033ab9e
vanhofen <vanhofen@gmx.de>
2022-07-04 23:17:52 +0200

    - djmount: rework and fix build

--- package/djmount/djmount.mk
+++ package/djmount/djmount.mk
@@ -9,9 +9,6 @@ DJMOUNT_DIR = djmount-$(DJMOUNT_VERSION)
 DJMOUNT_SOURCE = djmount-$(DJMOUNT_VERSION).tar.gz
 DJMOUNT_SITE = https://sourceforge.net/projects/djmount/files/djmount/$(DJMOUNT_VERSION)
 
-$(DL_DIR)/$(DJMOUNT_SOURCE):
-	$(download) $(DJMOUNT_SITE)/$(DJMOUNT_SOURCE)
-
 DJMOUNT_DEPENDENCIES = libfuse
 
 DJMOUNT_AUTORECONF = YES
@@ -20,16 +17,19 @@ DJMOUNT_CONF_OPTS = \
 	--with-fuse-prefix=$(TARGET_prefix) \
 	--disable-debug
 
-djmount: $(DJMOUNT_DEPENDENCIES) $(DL_DIR)/$(DJMOUNT_SOURCE) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(UNTAR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$(PKG_DIR); \
-		touch libupnp/config.aux/config.rpath; \
-		$(TARGET_CONFIGURE); \
-		$(MAKE1); \
-		$(MAKE1) install DESTDIR=$(TARGET_DIR)
+DJMOUNT_MAKE = \
+	$(MAKE1)
+
+define DJMOUNT_TOUCH_CONFIG_RPATH
+	touch $(PKG_BUILD_DIR)/libupnp/config.aux/config.rpath
+endef
+DJMOUNT_PRE_CONFIGURE_HOOKS += DJMOUNT_TOUCH_CONFIG_RPATH
+
+define DJMOUNT_INSTALL_INIT_SCRIPT
 	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/djmount.init $(TARGET_sysconfdir)/init.d/djmount
 	$(UPDATE-RC.D) djmount defaults 75 25
-	$(REMOVE)/$(PKG_DIR)
-	$(call TOUCH)
+endef
+DJMOUNT_TARGET_FINALIZE_HOOKS += DJMOUNT_INSTALL_INIT_SCRIPT
+
+djmount: | $(TARGET_DIR)
+	$(call autotools-package)
