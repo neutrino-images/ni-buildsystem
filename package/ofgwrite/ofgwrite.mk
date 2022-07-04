2c4533a87121304d2c6c6b518ba890bcafc219fc
vanhofen <vanhofen@gmx.de>
2022-07-04 21:44:54 +0200

    - ofgwrite: rework build

--- package/ofgwrite/ofgwrite.mk
+++ package/ofgwrite/ofgwrite.mk
@@ -9,6 +9,9 @@ OFGWRITE_DIR = $(NI_OFGWRITE)
 OFGWRITE_SOURCE = $(NI_OFGWRITE)
 OFGWRITE_SITE = https://github.com/neutrino-images
 
+OFGWRITE_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
 define OFGWRITE_INSTALL_BINARIES
 	$(INSTALL_EXEC) $(PKG_BUILD_DIR)/ofgwrite_bin $(TARGET_bindir)
 	$(INSTALL_EXEC) $(PKG_BUILD_DIR)/ofgwrite_caller $(TARGET_bindir)
@@ -18,8 +21,4 @@ endef
 OFGWRITE_PRE_FOLLOWUP_HOOKS += OFGWRITE_INSTALL_BINARIES
 
 ofgwrite: | $(TARGET_DIR)
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE)
-	$(call TARGET_FOLLOWUP)
+	$(call generic-package,$(PKG_NO_INSTALL))
