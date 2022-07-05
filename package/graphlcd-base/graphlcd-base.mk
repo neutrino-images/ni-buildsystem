ffec543c3fc74b155d7cab7016241e74a1d509cc
vanhofen <vanhofen@gmx.de>
2022-07-05 17:07:31 +0200

    - graphlcd-base: rework build

--- package/graphlcd-base/graphlcd-base.mk
+++ package/graphlcd-base/graphlcd-base.mk
@@ -9,24 +9,25 @@ GRAPHLCD_BASE_DIR = graphlcd-base.$(GRAPHLCD_BASE_VERSION)
 GRAPHLCD_BASE_SOURCE = graphlcd-base.$(GRAPHLCD_BASE_VERSION)
 GRAPHLCD_BASE_SITE = https://projects.vdr-developer.org/git
 
-GRAPHLCD_BASE_PATCH  = graphlcd.patch
+GRAPHLCD_BASE_DEPENDENCIES = freetype libiconv libusb
+
+GRAPHLCD_BASE_PATCH  = 0001-graphlcd.patch
 GRAPHLCD_BASE_PATCH += 0003-strip-graphlcd-conf.patch
 GRAPHLCD_BASE_PATCH += 0004-material-colors.patch
 ifeq ($(BOXMODEL),$(filter $(BOXMODEL),vuduo4k vuduo4kse vusolo4k vuultimo4k vuuno4kse))
   GRAPHLCD_BASE_PATCH += 0005-add-vuplus-driver.patch
 endif
 
-GRAPHLCD_BASE_DEPENDENCIES = freetype libiconv libusb
+GRAPHLCD_BASE_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
+GRAPHLCD_BASE_MAKE_INSTALL_OPTS = \
+	PREFIX=$(prefix)
+
+define GRAPHLCD_BASE_TARGET_CLEANUP
+	$(TARGET_RM) $(TARGET_sysconfdir)/udev
+endef
+GRAPHLCD_BASE_TARGET_FINALIZE_HOOKS += GRAPHLCD_BASE_TARGET_CLEANUP
 
 graphlcd-base: $(GRAPHLCD_BASE_DEPENDENCIES) | $(TARGET_DIR)
-	$(REMOVE)/$(PKG_DIR)
-	$(GET_GIT_SOURCE) $(PKG_SITE)/$(PKG_SOURCE) $(DL_DIR)/$(PKG_SOURCE)
-	$(CPDIR)/$(PKG_SOURCE)
-	$(call APPLY_PATCHES,$(PKG_PATCH))
-	$(CHDIR)/$(PKG_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR) PREFIX=$(prefix)
-	$(TARGET_RM) $(TARGET_sysconfdir)/udev
-	$(REMOVE)/$(PKG_DIR)
-	$(call TOUCH)
+	$(call generic-package)
