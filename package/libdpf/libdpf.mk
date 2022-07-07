e5fa797d9e518a82515f2e1d92a35474d3523287
vanhofen <vanhofen@gmx.de>
2022-07-07 20:50:26 +0200

    - libdpf: rework build

--- package/libdpf/libdpf.mk
+++ package/libdpf/libdpf.mk
@@ -18,13 +18,12 @@ LIBDPF_MAKE_OPTS = \
 	-C dpflib libdpf.a \
 	CC=$(TARGET_CC) PREFIX=$(TARGET_prefix)
 
-define LIBDPF_INSTALL_FILES
+define LIBDPF_INSTALL_CMDS
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/dpflib/libdpf.a $(TARGET_libdir)/libdpf.a
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/dpflib/dpf.h $(TARGET_includedir)/libdpf/libdpf.h
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/include/spiflash.h $(TARGET_includedir)/libdpf/spiflash.h
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/include/usbuser.h $(TARGET_includedir)/libdpf/usbuser.h
 endef
-LIBDPF_PRE_FOLLOWUP_HOOKS += LIBDPF_INSTALL_FILES
 
 libdpf: | $(TARGET_DIR)
-	$(call generic-package,$(PKG_NO_INSTALL))
+	$(call generic-package)
