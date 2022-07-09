efc9c64557a7008125a5ccca1d78b88f552924f4
vanhofen <vanhofen@gmx.de>
2022-07-09 14:37:19 +0200

    - dvb-apps: rework build

--- package/dvb-apps/dvb-apps.mk
+++ package/dvb-apps/dvb-apps.mk
@@ -13,19 +13,14 @@ DVB_APPS_CHECKOUT = 3d43b280298c39a67d1d889e01e173f52c12da35
 
 DVB_APPS_DEPENDENCIES = kernel-headers libiconv
 
+DVB_APPS_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV) \
+	LDLIBS="-liconv"
+
 DVB_APPS_MAKE_OPTS = \
-	KERNEL_HEADERS=$(KERNEL_HEADERS_DIR) \
 	enable_shared=no \
+	KERNEL_HEADERS=$(KERNEL_HEADERS_DIR) \
 	PERL5LIB=$(PKG_BUILD_DIR)/util/scan \
 
 dvb-apps: | $(TARGET_DIR)
-	$(call DEPENDENCIES)
-	$(call DOWNLOAD,$($(PKG)_SOURCE))
-	$(call STARTUP)
-	$(call EXTRACT,$(BUILD_DIR))
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) LDLIBS="-liconv" \
-		$(MAKE) $($(PKG)_MAKE_OPTS); \
-		$(MAKE) $($(PKG)_MAKE_OPTS) install DESTDIR=$(TARGET_DIR)
-	$(call TARGET_FOLLOWUP)
+	$(call generic-package)
