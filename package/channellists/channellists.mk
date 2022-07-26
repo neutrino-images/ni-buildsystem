4fe7dad7fca46c01b6ec755a67ecb7180bb0c8fc
vanhofen <vanhofen@gmx.de>
2022-07-26 22:26:42 +0200

    - neutrino-initial-settings: move target to own package dir

--- package/channellists/channellists.mk
+++ package/channellists/channellists.mk
@@ -60,17 +60,4 @@ matze-192-130:
 			UPDATE_MD5FILE=$(CHANNELLISTS_MD5FILE) \
 			UPDATE_NAME=$(@) \
 			UPDATE_DESC="$$desc - " \
-			UPDATE_VERSION="$$date" \
-
-# -----------------------------------------------------------------------------
-
-initial-settings: matze-192
-	$(REMOVE)/initial
-	$(MKDIR)/initial
-	$(CHDIR)/initial; \
-		tar -xf $(STAGING_DIR)/updates/matze-192.bin; \
-		cp temp_inst/inst/var/tuxbox/config/zapit/* $(SOURCE_DIR)/$(NI_NEUTRINO)/data/initial/
-	P192=`grep -m 1 'position=\"192\"' $(SOURCE_DIR)/$(NI_NEUTRINO)/data/config/satellites.xml`; \
-	P192=`echo $$P192`; \
-	$(SED) "/position=\"192\"/c\	$$P192" $(SOURCE_DIR)/$(NI_NEUTRINO)/data/initial/services.xml
-	@$(call MESSAGE,"Commit your changes in $(SOURCE_DIR)/$(NI_NEUTRINO)/data/initial")
+			UPDATE_VERSION="$$date"
