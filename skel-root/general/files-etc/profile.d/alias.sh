3553cb5cbcc255e3d85e9ab06577404cb6431989
vanhofen <vanhofen@gmx.de>
2022-08-26 19:34:38 +0200

    - files-etc: add my neutrino aliases

--- skel-root/general/files-etc/profile.d/alias.sh
+++ skel-root/general/files-etc/profile.d/alias.sh
@@ -1,3 +1,8 @@
 alias mc='mc -a'
 alias sc='setconsole'
 alias sr='setconsole -r'
+
+alias xn='killall neutrino'
+alias bn='/usr/bin/neutrino'
+alias tn='/tmp/neutrino'
+alias sn='/etc/init.d/start_neutrino'
