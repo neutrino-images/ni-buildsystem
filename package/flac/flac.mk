3191edc56797b5b8b7b7c1967a611eca51f036ca
vanhofen <vanhofen@gmx.de>
2022-09-18 20:33:02 +0200

    - flac: bump version to 1.4.0; update build

--- package/flac/flac.mk
+++ package/flac/flac.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-FLAC_VERSION = 1.3.4
+FLAC_VERSION = 1.4.0
 FLAC_DIR = flac-$(FLAC_VERSION)
 FLAC_SOURCE = flac-$(FLAC_VERSION).tar.xz
 FLAC_SITE = https://ftp.osuosl.org/pub/xiph/releases/flac
-
-FLAC_AUTORECONF = YES
 
 FLAC_CONF_OPTS = \
 	--bindir=$(REMOVE_bindir) \
@@ -17,10 +15,21 @@ FLAC_CONF_OPTS = \
 	--enable-shared \
 	--disable-static \
 	--disable-cpplibs \
-	--disable-xmms-plugin \
+	--disable-debug \
+	--disable-asm-optimizations \
+	--disable-sse \
 	--disable-altivec \
+	--disable-vsx \
+	--disable-doxygen-docs \
+	--disable-thorough-tests \
+	--disable-exhaustive-tests \
+	--disable-valgrind-testing \
 	--disable-ogg \
-	--disable-sse
+	--disable-oggtest \
+	--disable-local-xmms-plugin \
+	--disable-xmms-plugin \
+	--disable-examples \
+	--disable-rpath
 
 flac: | $(TARGET_DIR)
 	$(call autotools-package)
