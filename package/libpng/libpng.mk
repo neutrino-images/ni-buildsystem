ba6808cd1aeac053793f07360cff01ef0c9506bc
vanhofen <vanhofen@gmx.de>
2022-10-07 21:54:40 +0200

    - libpng: bump version to 1.6.38

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPNG_VERSION = 1.6.37
+LIBPNG_VERSION = 1.6.38
 LIBPNG_DIR = libpng-$(LIBPNG_VERSION)
 LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
 LIBPNG_SITE = https://sourceforge.net/projects/libpng/files/libpng16/$(LIBPNG_VERSION)
@@ -12,10 +12,6 @@ LIBPNG_SITE = https://sourceforge.net/projects/libpng/files/libpng16/$(LIBPNG_VE
 LIBPNG_DEPENDENCIES = zlib
 
 LIBPNG_CONFIG_SCRIPTS = libpng16-config
-
-LIBPNG_CONF_OPTS = \
-	--disable-static \
-	$(if $(filter $(BOXSERIES),hd5x hd6x vusolo4k vuduo4k vuduo4kse vuultimo4k vuzero4k vuuno4k vuuno4kse),--enable-arm-neon,--disable-arm-neon)
 
 define LIBPNG_TARGET_CLEANUP
 	$(TARGET_RM) $(addprefix $(TARGET_bindir)/,libpng-config)
