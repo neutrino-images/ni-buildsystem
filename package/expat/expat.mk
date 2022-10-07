ea6bcbaf4e793e85507be0f1a5ea4234d6ec6fe1
vanhofen <vanhofen@gmx.de>
2022-10-07 21:54:40 +0200

    - expat: bump version to 2.4.9

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXPAT_VERSION = 2.4.8
+EXPAT_VERSION = 2.4.9
 EXPAT_DIR = expat-$(EXPAT_VERSION)
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.bz2
 EXPAT_SITE = https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$(EXPAT_VERSION))
@@ -13,8 +13,10 @@ EXPAT_AUTORECONF = YES
 
 EXPAT_CONF_OPTS = \
 	--docdir=$(REMOVE_docdir) \
-	--without-xmlwf \
-	--without-docbook
+	--without-docbook \
+	--without-examples \
+	--without-tests \
+	--without-xmlwf
 
 define EXPAT_TARGET_CLEANUP
 	$(TARGET_RM) $(TARGET_libdir)/cmake
