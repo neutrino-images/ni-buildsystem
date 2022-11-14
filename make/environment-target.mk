9c9fb650f14b0392450607ed2010e04367ed115f
vanhofen <vanhofen@gmx.de>
2022-11-14 19:35:10 +0100

    - neutrino: introduce neutrino-dirs.mk

--- make/environment-target.mk
+++ make/environment-target.mk
@@ -97,27 +97,4 @@ $(foreach dir,$(dir_VARIABLES),$(eval REMOVE_$(dir) = $(REMOVE_DIR)$($(dir))))
 
 # -----------------------------------------------------------------------------
 
-SHARE_FLEX	= $(TARGET_datadir)/tuxbox/neutrino/flex
-SHARE_ICONS	= $(TARGET_datadir)/tuxbox/neutrino/icons
-SHARE_LOGOS	= $(TARGET_datadir)/tuxbox/neutrino/icons/logo
-SHARE_PLUGINS	= $(TARGET_datadir)/tuxbox/neutrino/plugins
-SHARE_THEMES	= $(TARGET_datadir)/tuxbox/neutrino/themes
-SHARE_WEBRADIO	= $(TARGET_datadir)/tuxbox/neutrino/webradio
-SHARE_WEBTV	= $(TARGET_datadir)/tuxbox/neutrino/webtv
-VAR_CONFIG	= $(TARGET_localstatedir)/tuxbox/config
-VAR_PLUGINS	= $(TARGET_localstatedir)/tuxbox/plugins
-
-$(SHARE_FLEX) \
-$(SHARE_ICONS) \
-$(SHARE_LOGOS) \
-$(SHARE_PLUGINS) \
-$(SHARE_THEMES) \
-$(SHARE_WEBRADIO) \
-$(SHARE_WEBTV) \
-$(VAR_CONFIG) \
-$(VAR_PLUGINS) : | $(TARGET_DIR)
-	$(INSTALL) -d $(@)
-
-# -----------------------------------------------------------------------------
-
 PERSISTENT_VAR_PARTITION = $(if $(filter $(BOXMODEL),apollo shiner kronos kronos_v2),yes,no)
