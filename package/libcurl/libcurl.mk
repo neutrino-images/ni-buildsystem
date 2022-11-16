6793c64f1eef093f53f7d20f22ae76c3ee30f909
vanhofen <vanhofen@gmx.de>
2022-11-16 21:50:08 +0100

    - libcurl: bump version to 7.86.0

--- package/libcurl/libcurl.mk
+++ package/libcurl/libcurl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCURL_VERSION = 7.85.0
+LIBCURL_VERSION = 7.86.0
 LIBCURL_DIR = curl-$(LIBCURL_VERSION)
 LIBCURL_SOURCE = curl-$(LIBCURL_VERSION).tar.bz2
 LIBCURL_SITE = https://curl.haxx.se/download
