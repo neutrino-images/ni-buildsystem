c2dae856cfd0e8189e56fba21d1f848de7c985b5
vanhofen <vanhofen@gmx.de>
2022-12-11 22:49:36 +0100

    - libxslt: bump version to 1.1.37

--- package/libxslt/libxslt.mk
+++ package/libxslt/libxslt.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBXSLT_VERSION = 1.1.34
+LIBXSLT_VERSION = 1.1.37
 LIBXSLT_DIR = libxslt-$(LIBXSLT_VERSION)
-LIBXSLT_SOURCE = libxslt-$(LIBXSLT_VERSION).tar.gz
-LIBXSLT_SITE = http://xmlsoft.org/sources
+LIBXSLT_SOURCE = libxslt-$(LIBXSLT_VERSION).tar.xz
+LIBXSLT_SITE = https://download.gnome.org/sources/libxslt/1.1
 
 LIBXSLT_DEPENDENCIES = libxml2
 
@@ -15,15 +15,13 @@ LIBXSLT_CONFIG_SCRIPTS = xslt-config
 
 LIBXSLT_CONF_OPTS = \
 	--datarootdir=$(REMOVE_datarootdir) \
-	--enable-shared \
-	--disable-static \
-	--without-python \
-	--without-crypto \
+	--with-gnu-ld \
 	--without-debug \
-	--without-mem-debug
+	--without-python \
+	--without-crypto
 
 define LIBXSLT_TARGET_CLEANUP
-	$(TARGET_RM) $(addprefix $(TARGET_libdir)/,libxslt-plugins xsltConf.sh)
+	$(TARGET_RM) $(addprefix $(TARGET_libdir)/,xsltConf.sh)
 endef
 LIBXSLT_TARGET_FINALIZE_HOOKS += LIBXSLT_TARGET_CLEANUP
 
