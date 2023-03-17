7e23c4709fadc57ce173b8109d3e3e8072eb1a5c
vanhofen <vanhofen@gmx.de>
2023-03-17 23:51:09 +0100

    - libnl: bump version to 3.7.0

--- package/libnl/libnl.mk
+++ package/libnl/libnl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNL_VERSION = 3.5.0
+LIBNL_VERSION = 3.7.0
 LIBNL_DIR = libnl-$(LIBNL_VERSION)
 LIBNL_SOURCE = libnl-$(LIBNL_VERSION).tar.gz
 LIBNL_SITE = https://github.com/thom311/libnl/releases/download/libnl$(subst .,_,$(LIBNL_VERSION))
