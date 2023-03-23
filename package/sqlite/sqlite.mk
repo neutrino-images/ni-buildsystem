17529193370a2eaefe5f607d2133fbb49fdfc6ed
vanhofen <vanhofen@gmx.de>
2023-03-23 22:30:35 +0100

    - sqlite: bump version to 3410200

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3390000
+SQLITE_VERSION = 3410200
 SQLITE_DIR = sqlite-autoconf-$(SQLITE_VERSION)
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
-SQLITE_SITE = http://www.sqlite.org/2022
+SQLITE_SITE = http://www.sqlite.org/2023
 
 SQLITE_CONF_OPTS = \
 	--bindir=$(REMOVE_bindir)
