f95d05c1dbc40c9c1a4811ddd7074d24db70a3b4
vanhofen <vanhofen@gmx.de>
2023-04-05 20:54:13 +0200

    - strace: bump version to 6.2

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-STRACE_VERSION = 5.9
+STRACE_VERSION = 6.2
 STRACE_DIR = strace-$(STRACE_VERSION)
 STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
 STRACE_SITE = https://strace.io/files/$(STRACE_VERSION)
