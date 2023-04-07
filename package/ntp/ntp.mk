02e7bc98cd2c48ef56335eaa767cd13c37016d67
vanhofen <vanhofen@gmx.de>
2023-04-08 00:06:04 +0200

    - ntp: update build

--- package/ntp/ntp.mk
+++ package/ntp/ntp.mk
@@ -11,16 +11,29 @@ NTP_SITE = https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-$(basename $(NTP_V
 
 NTP_DEPENDENCIES = openssl
 
+# 0002-ntp-syscalls-fallback.patch
+NTP_AUTORECONF = YES
+
+NTP_CONF_ENV = \
+	ac_cv_lib_md5_MD5Init=no \
+	POSIX_SHELL=/bin/sh
+
 NTP_CONF_OPTS = \
 	--bindir=$(REMOVE_bindir) \
 	--datadir=$(REMOVE_datadir) \
 	--libexecdir=$(REMOVE_libexecdir) \
 	--docdir=$(REMOVE_docdir) \
+	--disable-ATOM \
+	--disable-SHM \
+	--disable-tickadj \
 	--disable-debugging \
+	--disable-linuxcaps \
 	--with-shared \
-	--with-crypto \
+	--with-crypto --enable-openssl-random \
 	--with-yielding-select=yes \
-	--without-ntpsnmpd
+	--without-sntp \
+	--without-ntpsnmpd \
+	--without-lineeditlibs
 
 define NTP_TARGET_CLEANUP
 	$(TARGET_RM) $(addprefix $(TARGET_sbindir)/,calc_tickadj ntp-keygen ntp-wait ntpd ntptime tickadj update-leap)
