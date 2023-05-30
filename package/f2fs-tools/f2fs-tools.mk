f95253824add9bfb61305bcb60230c065ff0d35c
vanhofen <vanhofen@gmx.de>
2023-05-30 23:30:00 +0200

    - f2fs-tools: bump to version 1.16.0; update build

--- package/f2fs-tools/f2fs-tools.mk
+++ package/f2fs-tools/f2fs-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-F2FS_TOOLS_VERSION = 1.15.0
+F2FS_TOOLS_VERSION = 1.16.0
 F2FS_TOOLS_DIR = f2fs-tools-$(F2FS_TOOLS_VERSION)
 F2FS_TOOLS_SOURCE = f2fs-tools-$(F2FS_TOOLS_VERSION).tar.gz
 F2FS_TOOLS_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs-tools.git/snapshot
@@ -22,11 +22,6 @@ F2FS_TOOLS_CONF_OPTS = \
 	--includedir=$(includedir) \
 	--libdir=$(libdir) \
 	--without-selinux
-
-define F2FS_TOOLS_TARGET_CLEANUP
-	$(TARGET_RM) $(addprefix $(TARGET_base_sbindir)/,sg_write_buffer)
-endef
-F2FS_TOOLS_TARGET_FINALIZE_HOOKS += F2FS_TOOLS_TARGET_CLEANUP
 
 f2fs-tools: | $(TARGET_DIR)
 	$(call autotools-package)
