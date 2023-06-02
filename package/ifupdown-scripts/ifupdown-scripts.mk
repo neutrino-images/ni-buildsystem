3602f88af4497711712d5794fbef7d05a0f44e15
vanhofen <vanhofen@gmx.de>
2023-06-02 22:06:52 +0200

    - ifupdown-scripts: update build

--- package/ifupdown-scripts/ifupdown-scripts.mk
+++ package/ifupdown-scripts/ifupdown-scripts.mk
@@ -4,13 +4,27 @@
 #
 ################################################################################
 
-ifupdown-scripts: | $(TARGET_DIR)
-	$(INSTALL) -d $(TARGET_sysconfdir)/network/if-{up,pre-up,post-up,down,pre-down,post-down}.d
-  ifeq ($(PERSISTENT_VAR_PARTITION),yes)
+define IFUPDOWN_SCRIPTS_INSTALL_IFUPDOWN_DIRS
+	$(INSTALL) -d $(TARGET_sysconfdir)/network/if-{pre-up,up,post-up,pre-down,down,post-down}.d
+endef
+IFUPDOWN_SCRIPTS_INDIVIDUAL_HOOKS += IFUPDOWN_SCRIPTS_INSTALL_IFUPDOWN_DIRS
+
+ifeq ($(PERSISTENT_VAR_PARTITION),yes)
+define IFUPDOWN_SCRIPTS_INSTALL_INTERFACES
 	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/interfaces $(TARGET_localstatedir)/etc/network/interfaces
 	ln -sf /var/etc/network/interfaces $(TARGET_sysconfdir)/network/interfaces
-  else
+endef
+else
+define IFUPDOWN_SCRIPTS_INSTALL_INTERFACES
 	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/interfaces $(TARGET_sysconfdir)/network/interfaces
-  endif
+endef
+endif
+IFUPDOWN_SCRIPTS_INDIVIDUAL_HOOKS += IFUPDOWN_SCRIPTS_INSTALL_INTERFACES
+
+define IFUPDOWN_SCRIPTS_INSTALL_INIT_SYSV
 	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/network.init $(TARGET_sysconfdir)/init.d/network
 	$(UPDATE-RC.D) network stop 98 0 6 .
+endef
+
+ifupdown-scripts: | $(TARGET_DIR)
+	$(call individual-package,$(PKG_NO_DOWNLOAD) $(PKG_NO_EXTRACT) $(PKG_NO_PATCHES))
