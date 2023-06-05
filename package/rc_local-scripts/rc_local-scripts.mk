d85da4abbdce282d5eba67b6aac43371cf155a8c
vanhofen <vanhofen@gmx.de>
2023-06-06 00:37:07 +0200

    - rc_local-scripts: switch to virtual-package structure

--- package/rc_local-scripts/rc_local-scripts.mk
+++ package/rc_local-scripts/rc_local-scripts.mk
@@ -4,10 +4,22 @@
 #
 ################################################################################
 
-rc_local-scripts: | $(TARGET_DIR)
+define RC_LOCAL_SCRIPTS_INSTALL
+	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/rc.local $(TARGET_sysconfdir)/rc.local
+endef
+RC_LOCAL_SCRIPTS_INDIVIDUAL_HOOKS += RC_LOCAL_SCRIPTS_INSTALL
+
+ifeq ($(PERSISTENT_VAR_PARTITION),yes)
+define RC_LOCAL_SCRIPTS_INSTALL_VAR
+	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/rc.local-var $(TARGET_localstatedir)/etc/rc.local
+endef
+RC_LOCAL_SCRIPTS_INDIVIDUAL_HOOKS += RC_LOCAL_SCRIPTS_INSTALL_VAR
+endif
+
+define RC_LOCAL_SCRIPTS_INSTALL_INIT_SYSV
 	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/rc.local.init $(TARGET_sysconfdir)/init.d/rc.local
 	$(UPDATE-RC.D) rc.local start 99 2 3 4 5 .
-	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/rc.local $(TARGET_sysconfdir)/rc.local
-ifeq ($(PERSISTENT_VAR_PARTITION),yes)
-	$(INSTALL_EXEC) -D $(PKG_FILES_DIR)/rc.local-var $(TARGET_localstatedir)/etc/rc.local
-endif
+endef
+
+rc_local-scripts: | $(TARGET_DIR)
+	$(call virtual-package)
