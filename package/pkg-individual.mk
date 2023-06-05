1fdfa6332f64641c4721ab3ef3e715d54e0ee772
vanhofen <vanhofen@gmx.de>
2023-06-05 23:54:06 +0200

    - pkg-individual: add virtual-package infrastructure

--- package/pkg-individual.mk
+++ package/pkg-individual.mk
@@ -14,7 +14,7 @@ endef
 define individual-package
 	$(eval PKG_MODE = $(pkg-mode))
 	$(call PREPARE,$(1))
-	$(call INDIVIDUAL)
+	$(if $($(PKG)_INDIVIDUAL_HOOKS),$(call INDIVIDUAL))
 	$(call TARGET_FOLLOWUP)
 endef
 
@@ -23,6 +23,24 @@ endef
 define host-individual-package
 	$(eval PKG_MODE = $(pkg-mode))
 	$(call PREPARE,$(1))
-	$(call INDIVIDUAL)
+	$(if $($(PKG)_INDIVIDUAL_HOOKS),$(call INDIVIDUAL))
 	$(call HOST_FOLLOWUP)
+endef
+
+################################################################################
+#
+# Virtual package infrastructure
+#
+# - just to resolve dependencies and call $($(PKG)_INDIVIDUAL_HOOKS)
+#
+################################################################################
+
+define virtual-package
+	$(call individual-package,$(PKG_NO_DOWNLOAD) $(PKG_NO_EXTRACT) $(PKG_NO_PATCHES))
+endef
+
+# -----------------------------------------------------------------------------
+
+define host-virtual-package
+	$(call host-individual-package,$(PKG_NO_DOWNLOAD) $(PKG_NO_EXTRACT) $(PKG_NO_PATCHES))
 endef
