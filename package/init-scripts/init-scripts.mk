c840b97208a9db05300d1067bedff43dd007d79d
vanhofen <vanhofen@gmx.de>
2023-06-06 00:37:07 +0200

    - init-scripts: add missing multibox

--- package/init-scripts/init-scripts.mk
+++ package/init-scripts/init-scripts.mk
@@ -62,18 +62,18 @@ $(TARGET_sysconfdir)/init.d/hostname:
 	$(INSTALL_EXEC) -D $(INIT_SCRIPTS_FILES_DIR)/hostname.init $(@)
 
 $(TARGET_sysconfdir)/init.d/partitions-by-name:
-ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd51 bre2ze4k h7 e4hdultra protek4k hd60 hd61 multiboxse))
+ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd51 bre2ze4k h7 e4hdultra protek4k hd60 hd61 multibox multiboxse))
 	$(INSTALL_EXEC) -D $(INIT_SCRIPTS_FILES_DIR)/partitions-by-name.init $(@)
 endif
 
 $(TARGET_sysconfdir)/init.d/proc:
-ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd51 bre2ze4k h7 e4hdultra protek4k hd60 hd61 multiboxse vusolo4k vuduo4k vuduo4kse vuultimo4k vuzero4k vuuno4k vuuno4kse vuduo))
+ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd51 bre2ze4k h7 e4hdultra protek4k hd60 hd61 multibox multiboxse vusolo4k vuduo4k vuduo4kse vuultimo4k vuzero4k vuuno4k vuuno4kse vuduo))
 	$(INSTALL_EXEC) -D $(INIT_SCRIPTS_FILES_DIR)/proc.init $(@)
 	$(UPDATE-RC.D) $(@F) start 90 S .
 endif
 
 $(TARGET_sysconfdir)/init.d/resizerootfs:
-ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd51 bre2ze4k h7 e4hdultra protek4k hd60 hd61 multiboxse))
+ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd51 bre2ze4k h7 e4hdultra protek4k hd60 hd61 multibox multiboxse))
 	$(INSTALL_EXEC) -D $(INIT_SCRIPTS_FILES_DIR)/resizerootfs.init $(@)
 endif
 
@@ -81,19 +81,19 @@ $(TARGET_sysconfdir)/init.d/sys_update.sh:
 	$(INSTALL_EXEC) -D $(INIT_SCRIPTS_FILES_DIR)/sys_update.sh $(@)
 
 $(TARGET_sysconfdir)/init.d/sendsigs:
-ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd60 hd61 multiboxse))
+ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd60 hd61 multibox multiboxse))
 	$(INSTALL_EXEC) -D $(INIT_SCRIPTS_FILES_DIR)/sendsigs.init $(@)
 	$(UPDATE-RC.D) $(@F) start 85 0 .
 endif
 
 $(TARGET_sysconfdir)/init.d/umountfs:
-ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd60 hd61 multiboxse))
+ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd60 hd61 multibox multiboxse))
 	$(INSTALL_EXEC) -D $(INIT_SCRIPTS_FILES_DIR)/umountfs.init $(@)
 	$(UPDATE-RC.D) $(@F) start 86 0 .
 endif
 
 $(TARGET_sysconfdir)/init.d/suspend:
-ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd60 hd61 multiboxse))
+ifeq ($(BOXMODEL),$(filter $(BOXMODEL),hd60 hd61 multibox multiboxse))
 	$(INSTALL_EXEC) -D $(INIT_SCRIPTS_FILES_DIR)/suspend.init $(@)
 	$(UPDATE-RC.D) $(@F) start 89 0 .
 endif
