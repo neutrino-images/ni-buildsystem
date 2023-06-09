eabdf453d88da4bc94e3a7044a2b31505f689f61
vanhofen <vanhofen@gmx.de>
2023-06-09 21:38:01 +0200

    - pkg-individual: update

--- package/pkg-individual.mk
+++ package/pkg-individual.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-define INDIVIDUAL
+define INDIVIDUAL_HOOKS
 	@$(call MESSAGE,"Individual build and/or install $(pkgname)")
 	$(foreach hook,$($(PKG)_INDIVIDUAL_HOOKS),$(call $(hook))$(sep))
 endef
@@ -14,7 +14,7 @@ endef
 define individual-package
 	$(eval PKG_MODE = $(pkg-mode))
 	$(call PREPARE,$(1))
-	$(if $($(PKG)_INDIVIDUAL_HOOKS),$(call INDIVIDUAL))
+	$(if $($(PKG)_INDIVIDUAL_HOOKS),$(call INDIVIDUAL_HOOKS))
 	$(call TARGET_FOLLOWUP)
 endef
 
@@ -23,6 +23,6 @@ endef
 define host-individual-package
 	$(eval PKG_MODE = $(pkg-mode))
 	$(call PREPARE,$(1))
-	$(if $($(PKG)_INDIVIDUAL_HOOKS),$(call INDIVIDUAL))
+	$(if $($(PKG)_INDIVIDUAL_HOOKS),$(call INDIVIDUAL_HOOKS))
 	$(call HOST_FOLLOWUP)
 endef
