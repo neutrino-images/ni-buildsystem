2df7a14cdc43e4fdd855678fde5235b46db5e3cc
vanhofen <vanhofen@gmx.de>
2023-06-17 22:23:58 +0200

    - doscam-webif-skin: switch to virtual package

--- package/doscam-webif-skin/doscam-webif-skin.mk
+++ package/doscam-webif-skin/doscam-webif-skin.mk
@@ -4,7 +4,11 @@
 #
 ################################################################################
 
-doscam-webif-skin:
+define DOSCAM_WEBIF_SKIN_INSTALL
 	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/doscam_ni-dark.css $(TARGET_datadir)/doscam/skin/doscam_ni-dark.css
 	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/IC_doscam_ni.tpl $(TARGET_datadir)/doscam/tpl/IC_doscam_ni.tpl
-	$(call TOUCH)
+endef
+DOSCAM_WEBIF_SKIN_INDIVIDUAL_HOOKS += DOSCAM_WEBIF_SKIN_INSTALL
+
+doscam-webif-skin: | $(TARGET_DIR)
+	$(call virtual-package)
