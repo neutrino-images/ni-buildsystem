eba5ca8fd07600ec78d2f4042b502b3c3055cc01
vanhofen <vanhofen@gmx.de>
2023-07-04 22:21:34 +0200

    - flac: bump to version 1.4.3

--- package/flac/flac.mk
+++ package/flac/flac.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FLAC_VERSION = 1.4.2
+FLAC_VERSION = 1.4.3
 FLAC_DIR = flac-$(FLAC_VERSION)
 FLAC_SOURCE = flac-$(FLAC_VERSION).tar.xz
 FLAC_SITE = https://ftp.osuosl.org/pub/xiph/releases/flac
@@ -18,8 +18,6 @@ FLAC_CONF_OPTS = \
 	--disable-debug \
 	--disable-asm-optimizations \
 	--disable-stack-smash-protection \
-	--disable-altivec \
-	--disable-vsx \
 	--disable-doxygen-docs \
 	--disable-thorough-tests \
 	--disable-exhaustive-tests \
