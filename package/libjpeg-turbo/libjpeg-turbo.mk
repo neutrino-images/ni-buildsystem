07e0fde9b8c6d6effbf9db0ac3ce6b853d49e41f
vanhofen <vanhofen@gmx.de>
2023-07-04 22:21:34 +0200

    - libjpeg-turbo: bump to version 3.0.0

--- package/libjpeg-turbo/libjpeg-turbo.mk
+++ package/libjpeg-turbo/libjpeg-turbo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBJPEG_TURBO_VERSION = 2.1.5.1
+LIBJPEG_TURBO_VERSION = 3.0.0
 LIBJPEG_TURBO_DIR = libjpeg-turbo-$(LIBJPEG_TURBO_VERSION)
 LIBJPEG_TURBO_SOURCE = libjpeg-turbo-$(LIBJPEG_TURBO_VERSION).tar.gz
 LIBJPEG_TURBO_SITE = https://sourceforge.net/projects/libjpeg-turbo/files/$(LIBJPEG_TURBO_VERSION)
