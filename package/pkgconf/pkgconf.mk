4b9639bb22a69900ba0f3a0965d2786c8bd873ff
vanhofen <vanhofen@gmx.de>
2023-09-07 22:38:09 +0200

    - pkgconf: bump to version 2.0.2

--- package/pkgconf/pkgconf.mk
+++ package/pkgconf/pkgconf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PKGCONF_VERSION = 1.7.3
+PKGCONF_VERSION = 2.0.2
 PKGCONF_DIR = pkgconf-$(PKGCONF_VERSION)
 PKGCONF_SOURCE = pkgconf-$(PKGCONF_VERSION).tar.gz
 PKGCONF_SITE = https://distfiles.ariadne.space/pkgconf
