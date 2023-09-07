8f1fc60bbdfbd61d943dd03ebdc09ce9c28ceb94
vanhofen <vanhofen@gmx.de>
2023-09-07 22:38:09 +0200

    - sysvinit: bump to version 3.08

--- package/sysvinit/sysvinit.mk
+++ package/sysvinit/sysvinit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYSVINIT_VERSION = 3.07
+SYSVINIT_VERSION = 3.08
 SYSVINIT_DIR = sysvinit-$(SYSVINIT_VERSION)
 SYSVINIT_SOURCE = sysvinit-$(SYSVINIT_VERSION).tar.xz
 SYSVINIT_SITE = https://github.com/slicer69/sysvinit/releases/download/$(SYSVINIT_VERSION)
