7cdf9704bddc1fc6d2a6a87eb0a4b18354c12a09
vanhofen <vanhofen@gmx.de>
2023-09-23 23:00:09 +0200

    - add python3 target package infrastrucure

--- package/pkg-python.mk
+++ package/pkg-python.mk
@@ -4,6 +4,58 @@
 #
 ################################################################################
 
+TARGET_PYTHON3_ENV = \
+	CC="$(TARGET_CC)" \
+	CFLAGS="$(TARGET_CFLAGS)" \
+	LDFLAGS="$(TARGET_LDFLAGS)" \
+	LDSHARED="$(TARGET_CC) -shared" \
+	PYTHONPATH=$(PYTHON3_SITEPACKAGES_DIR)
+
+TARGET_PYTHON3_OPTS = \
+	$(if $(VERBOSE),,-q)
+
+define TARGET_PYTHON3_BUILD_CMDS_DEFAULT
+	$(CD) $(PKG_BUILD_DIR); \
+		$(TARGET_PYTHON3_ENV) \
+		CPPFLAGS="$(TARGET_CPPFLAGS) -I$(PYTHON3_INCLUDE_DIR)" \
+		$(HOST_PYTHON3_BINARY) ./setup.py build --executable=/usr/bin/python \
+			$(TARGET_PYTHON3_OPTS)
+endef
+
+define TARGET_PYTHON3_BUILD
+	@$(call MESSAGE,"Building $(pkgname)")
+	$(foreach hook,$($(PKG)_PRE_BUILD_HOOKS),$(call $(hook))$(sep))
+	$(Q)$(call $(PKG)_BUILD_CMDS)
+	$(foreach hook,$($(PKG)_POST_BUILD_HOOKS),$(call $(hook))$(sep))
+endef
+
+define TARGET_PYTHON3_INSTALL_CMDS_DEFAULT
+	$(CD) $(PKG_BUILD_DIR); \
+		$(TARGET_PYTHON3_ENV) \
+		CPPFLAGS="$(TARGET_CPPFLAGS) -I$(PYTHON3_INCLUDE_DIR)" \
+		$(HOST_PYTHON3_BINARY) ./setup.py install --root=$(TARGET_DIR) --prefix=/usr \
+			$(TARGET_PYTHON3_OPTS)
+endef
+
+define PYTHON3_INSTALL
+	@$(call MESSAGE,"Installing $(pkgname)")
+	$(foreach hook,$($(PKG)_PRE_INSTALL_HOOKS),$(call $(hook))$(sep))
+	$(Q)$(call $(PKG)_INSTALL_CMDS)
+	$(foreach hook,$($(PKG)_POST_INSTALL_HOOKS),$(call $(hook))$(sep))
+endef
+
+# -----------------------------------------------------------------------------
+
+define python3-package
+	$(eval PKG_MODE = $(pkg-mode))
+	$(call PREPARE,$(1))
+	$(if $(filter $(1),$(PKG_NO_BUILD)),,$(call PYTHON3_BUILD))
+	$(if $(filter $(1),$(PKG_NO_INSTALL)),,$(call PYTHON3_INSTALL))
+	$(call TARGET_FOLLOWUP)
+endef
+
+# -----------------------------------------------------------------------------
+
 HOST_PYTHON3_ENV = \
 	CC="$(HOSTCC)" \
 	CFLAGS="$(HOST_CFLAGS)" \
@@ -17,7 +69,7 @@ HOST_PYTHON3_OPTS = \
 define HOST_PYTHON3_BUILD_CMDS_DEFAULT
 	$(CD) $(PKG_BUILD_DIR); \
 		$(HOST_PYTHON3_ENV) \
-		$(HOST_PYTHON3_BINARY) ./setup.py build --executable=/usr/bin/python \
+		$(HOST_PYTHON3_BINARY) ./setup.py build \
 			$(HOST_PYTHON3_OPTS)
 endef
 
@@ -31,7 +83,7 @@ endef
 define HOST_PYTHON3_INSTALL_CMDS_DEFAULT
 	$(CD) $(PKG_BUILD_DIR); \
 		$(HOST_PYTHON3_ENV) \
-		$(HOST_PYTHON3_BINARY) ./setup.py install --root=$(HOST_DIR) --prefix= \
+		$(HOST_PYTHON3_BINARY) ./setup.py install --prefix=$(HOST_DIR) \
 			$(HOST_PYTHON3_OPTS)
 endef
 
