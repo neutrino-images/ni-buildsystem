8d220bec4fe02f45681b679513469a20992c1300
vanhofen <vanhofen@gmx.de>
2023-09-24 22:03:43 +0200

    - pyton-wheel: add TARGET target

--- package/python-wheel/python-wheel.mk
+++ package/python-wheel/python-wheel.mk
@@ -9,5 +9,10 @@ PYTHON_WHEEL_DIR = wheel-$(PYTHON_WHEEL_VERSION)
 PYTHON_WHEEL_SOURCE = wheel-$(PYTHON_WHEEL_VERSION).tar.gz
 PYTHON_WHEEL_SITE = https://files.pythonhosted.org/packages/fc/ef/0335f7217dd1e8096a9e8383e1d472aa14717878ffe07c4772e68b6e8735
 
+python-wheel: | $(TARGET_DIR)
+	$(call python-package)
+
+# -----------------------------------------------------------------------------
+
 host-python-wheel: | $(HOST_DIR)
 	$(call host-python-package)
