e817e207b9cba16fbfea99f868c9fb2d26a78513
vanhofen <vanhofen@gmx.de>
2023-10-01 23:47:07 +0200

    - ninja: bump to version 1.11.1.g95dee.kitware.jobserver-1

--- package/ninja/ninja.mk
+++ package/ninja/ninja.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-NINJA_VERSION = 1.10.2
+NINJA_VERSION_MAJOR = 1.11.1
+NINJA_VERSION = $(NINJA_VERSION_MAJOR).g95dee.kitware.jobserver-1
 NINJA_DIR = ninja-$(NINJA_VERSION)
 NINJA_SOURCE = ninja-$(NINJA_VERSION).tar.gz
-NINJA_SITE = $(call github,ninja-build,ninja,v$(NINJA_VERSION))
+NINJA_SITE = $(call github,Kitware,ninja,v$(NINJA_VERSION))
 
 # -----------------------------------------------------------------------------
 
