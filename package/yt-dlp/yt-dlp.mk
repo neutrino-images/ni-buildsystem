6dfbf6c81298f472e7e5607e0402bfda130f5da4
vanhofen <vanhofen@gmx.de>
2023-11-05 22:38:56 +0100

    - yt-dlp: add dependencies

--- package/yt-dlp/yt-dlp.mk
+++ package/yt-dlp/yt-dlp.mk
@@ -10,6 +10,9 @@ YT_DLP_SOURCE = yt-dlp
 YT_DLP_SITE = https://github.com/yt-dlp/yt-dlp/releases/$(YT_DLP_VERSION)/download
 YT_DLP_SITE_METHOD = curl
 
+YT_DLP_DEPENDENCIES = python3 brotli python-certifi python-websockets \
+	python-requests python-mutagen python-pycryptodomex
+
 define YT_DLP_INSTALL
 	$(INSTALL_EXEC) -D $(DL_DIR)/yt-dlp $(TARGET_bindir)/yt-dlp
 endef
