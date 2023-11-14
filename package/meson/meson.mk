fbb3a3e71cb2924429c8f8c7361ac2bf5fa48b4f
vanhofen <vanhofen@gmx.de>
2023-11-14 21:00:25 +0100

    - meson: fix build

--- package/meson/meson.mk
+++ package/meson/meson.mk
@@ -9,6 +9,8 @@ MESON_DIR = meson-$(MESON_VERSION)
 MESON_SOURCE = meson-$(MESON_VERSION).tar.gz
 MESON_SITE = https://github.com/mesonbuild/meson/releases/download/$(MESON_VERSION)
 
+MESON_SETUP_TYPE = setuptools
+
 # -----------------------------------------------------------------------------
 
 HOST_MESON_DEPENDENCIES = host-ninja
