84f5e5539b4b5e38a4b1c6b09062b8362597234f
vanhofen <vanhofen@gmx.de>
2023-11-19 22:16:25 +0100

    - channellists: update matze settings

--- package/neutrino-initial-settings/neutrino-initial-settings.mk
+++ package/neutrino-initial-settings/neutrino-initial-settings.mk
@@ -10,7 +10,7 @@ neutrino-initial-settings: matze-192
 	$(CHDIR)/initial; \
 		tar -xf $(STAGING_DIR)/updates/matze-192.bin; \
 		cp temp_inst/inst/var/tuxbox/config/zapit/* $(SOURCE_DIR)/$(NI_NEUTRINO)/data/initial/
-	P192=`grep -m 1 'position=\"192\"' $(SOURCE_DIR)/$(NI_NEUTRINO)/data/config/satellites.xml`; \
-	P192=`echo $$P192`; \
+	P192=$$(grep -m 1 "position=\"192\"" $(SOURCE_DIR)/$(NI_NEUTRINO)/data/config/satellites.xml); \
+	P192=$$(echo $$P192); \
 	$(SED) "/position=\"192\"/c\	$$P192" $(SOURCE_DIR)/$(NI_NEUTRINO)/data/initial/services.xml
 	@$(call MESSAGE,"Commit your changes in $(SOURCE_DIR)/$(NI_NEUTRINO)/data/initial")
