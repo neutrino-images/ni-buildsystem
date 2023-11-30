a19d16d96ab229531075e34b59b4750793a3ab45
vanhofen <vanhofen@gmx.de>
2023-11-30 23:52:26 +0100

    - luaexpat: bump version to 1.5.1-1; switch to luarocks-package

--- package/luaexpat/luaexpat.mk
+++ package/luaexpat/luaexpat.mk
@@ -4,26 +4,20 @@
 #
 ################################################################################
 
-LUAEXPAT_VERSION = 1.3.3
+LUAEXPAT_VERSION = 1.5.1-1
 LUAEXPAT_DIR = luaexpat-$(LUAEXPAT_VERSION)
-LUAEXPAT_SOURCE = luaexpat-$(LUAEXPAT_VERSION).tar.gz
-LUAEXPAT_SITE = $(call github,tomasguisasola,luaexpat,v$(LUAEXPAT_VERSION))
+LUAEXPAT_SOURCE = luaexpat-$(LUAEXPAT_VERSION).src.rock
+LUAEXPAT_SITE = $(LUAROCKS_MIRROR)
 
-LUAEXPAT_DEPENDENCIES = expat lua
+LUAEXPAT_ROCKSPEC = luaexpat-$(LUAEXPAT_VERSION).rockspec
+LUAEXPAT_SUBDIR = luaexpat
 
-LUAEXPAT_MAKE_ENV = \
-	$(TARGET_CONFIGURE_ENV)
+LUAEXPAT_DEPENDENCIES = expat
 
-LUAEXPAT_MAKE_OPTS = \
-	PREFIX=$(TARGET_prefix) \
-	LUA_SYS_VER=$(LUA_ABIVERSION)
-
-define LUAEXPAT_PATCH_MAKEFILE
-	$(SED) 's|^EXPAT_INC=.*|EXPAT_INC= $(TARGET_includedir)|' $(PKG_BUILD_DIR)/makefile
-	$(SED) 's|^CFLAGS =.*|& -L$(TARGET_libdir)|' $(PKG_BUILD_DIR)/makefile
-	$(SED) 's|^CC =.*|CC = $(TARGET_CC)|' $(PKG_BUILD_DIR)/makefile
+define LUAEXPAT_TARGET_CLEANUP
+	$(TARGET_RM) $(TARGET_libdir)/luarocks
 endef
-LUAEXPAT_POST_PATCH_HOOKS += LUAEXPAT_PATCH_MAKEFILE
+LUAEXPAT_TARGET_FINALIZE_HOOKS += LUAEXPAT_TARGET_CLEANUP
 
 luaexpat: | $(TARGET_DIR)
-	$(call generic-package)
+	$(call luarocks-package)
