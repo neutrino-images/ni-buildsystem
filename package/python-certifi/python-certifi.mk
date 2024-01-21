32b11bb7e0510825d42e45aff6c36f79b24414c9
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:51 +0100

    - python-certifi: bump to version 2023.11.17; update url

--- package/python-certifi/python-certifi.mk
+++ package/python-certifi/python-certifi.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_CERTIFI_VERSION = 2023.7.22
+PYTHON_CERTIFI_VERSION = 2023.11.17
 PYTHON_CERTIFI_DIR = certifi-$(PYTHON_CERTIFI_VERSION)
 PYTHON_CERTIFI_SOURCE = certifi-$(PYTHON_CERTIFI_VERSION).tar.gz
-PYTHON_CERTIFI_SITE = https://files.pythonhosted.org/packages/98/98/c2ff18671db109c9f10ed27f5ef610ae05b73bd876664139cf95bd1429aa
+PYTHON_CERTIFI_SITE = https://files.pythonhosted.org/packages/source/c/certifi
 
 PYTHON_CERTIFI_SETUP_TYPE = setuptools
 
