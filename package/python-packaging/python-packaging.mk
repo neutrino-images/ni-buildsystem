116bcd82fb04b24b103c781b465bb77d75aba89e
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:51 +0100

    - python-packaging: bump to version 23.2; update url

--- package/python-packaging/python-packaging.mk
+++ package/python-packaging/python-packaging.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PACKAGING_VERSION = 23.1
+PYTHON_PACKAGING_VERSION = 23.2
 PYTHON_PACKAGING_DIR = packaging-$(PYTHON_PACKAGING_VERSION)
 PYTHON_PACKAGING_SOURCE = packaging-$(PYTHON_PACKAGING_VERSION).tar.gz
-PYTHON_PACKAGING_SITE = https://files.pythonhosted.org/packages/b9/6c/7c6658d258d7971c5eb0d9b69fa9265879ec9a9158031206d47800ae2213
+PYTHON_PACKAGING_SITE = https://files.pythonhosted.org/packages/source/p/packaging
 
 # -----------------------------------------------------------------------------
 
