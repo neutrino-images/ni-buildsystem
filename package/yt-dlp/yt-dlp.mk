39b1bb213ce274bcc4f00afa228bc1984158c26f
vanhofen <vanhofen@gmx.de>
2024-01-21 20:58:54 +0100

    - yt-dlp: replace brotli with python-brotli

--- package/yt-dlp/yt-dlp.mk
+++ package/yt-dlp/yt-dlp.mk
@@ -10,7 +10,7 @@ YT_DLP_SOURCE = yt-dlp
 YT_DLP_SITE = https://github.com/yt-dlp/yt-dlp/releases/$(YT_DLP_VERSION)/download
 YT_DLP_SITE_METHOD = curl
 
-YT_DLP_DEPENDENCIES = python3 brotli python-certifi python-websockets \
+YT_DLP_DEPENDENCIES = python3 python-brotli python-certifi python-websockets \
 	python-requests python-mutagen python-pycryptodomex
 
 define YT_DLP_INSTALL
