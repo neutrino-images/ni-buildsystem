39470dacf38a3b7131bdc15207f5e602d50076f4
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:52 +0100

    - python-pyproject-hooks: update url

--- package/python-pyproject-hooks/python-pyproject-hooks.mk
+++ package/python-pyproject-hooks/python-pyproject-hooks.mk
@@ -7,7 +7,7 @@
 PYTHON_PYPROJECT_HOOKS_VERSION = 1.0.0
 PYTHON_PYPROJECT_HOOKS_DIR = pyproject_hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION)
 PYTHON_PYPROJECT_HOOKS_SOURCE = pyproject_hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION).tar.gz
-PYTHON_PYPROJECT_HOOKS_SITE = https://files.pythonhosted.org/packages/25/c1/374304b8407d3818f7025457b7366c8e07768377ce12edfe2aa58aa0f64c
+PYTHON_PYPROJECT_HOOKS_SITE = https://files.pythonhosted.org/packages/source/p/pyproject_hooks
 
 # -----------------------------------------------------------------------------
 
