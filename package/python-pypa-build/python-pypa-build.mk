d46f9e6cb4e476f5e0d8f1a3038a1c54dc28d28d
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:52 +0100

    - python-pypa-build: update url

--- package/python-pypa-build/python-pypa-build.mk
+++ package/python-pypa-build/python-pypa-build.mk
@@ -7,7 +7,7 @@
 PYTHON_PYPA_BUILD_VERSION = 1.0.3
 PYTHON_PYPA_BUILD_DIR = build-$(PYTHON_PYPA_BUILD_VERSION)
 PYTHON_PYPA_BUILD_SOURCE = build-$(PYTHON_PYPA_BUILD_VERSION).tar.gz
-PYTHON_PYPA_BUILD_SITE = https://files.pythonhosted.org/packages/98/e3/83a89a9d338317f05a68c86a2bbc9af61235bc55a0c6a749d37598fb2af1
+PYTHON_PYPA_BUILD_SITE = https://files.pythonhosted.org/packages/source/b/build
 
 # -----------------------------------------------------------------------------
 
