0f0876730fc8e95c5a52404e149d1dca9cbdcf96
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:52 +0100

    - python-wheel: bump to version 0.42.0; update url

--- package/python-wheel/python-wheel.mk
+++ package/python-wheel/python-wheel.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_WHEEL_VERSION = 0.40.0
+PYTHON_WHEEL_VERSION = 0.42.0
 PYTHON_WHEEL_DIR = wheel-$(PYTHON_WHEEL_VERSION)
 PYTHON_WHEEL_SOURCE = wheel-$(PYTHON_WHEEL_VERSION).tar.gz
-PYTHON_WHEEL_SITE = https://files.pythonhosted.org/packages/fc/ef/0335f7217dd1e8096a9e8383e1d472aa14717878ffe07c4772e68b6e8735
+PYTHON_WHEEL_SITE = https://files.pythonhosted.org/packages/source/w/wheel
 
 PYTHON_WHEEL_SETUP_TYPE = flit
 
