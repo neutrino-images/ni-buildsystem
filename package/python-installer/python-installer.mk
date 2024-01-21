c0f158949107a93fb364c5a02568fde6040028ef
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:51 +0100

    - python-installer: update url

--- package/python-installer/python-installer.mk
+++ package/python-installer/python-installer.mk
@@ -7,7 +7,7 @@
 PYTHON_INSTALLER_VERSION = 0.7.0
 PYTHON_INSTALLER_DIR = installer-$(PYTHON_INSTALLER_VERSION)
 PYTHON_INSTALLER_SOURCE = installer-$(PYTHON_INSTALLER_VERSION).tar.gz
-PYTHON_INSTALLER_SITE = https://files.pythonhosted.org/packages/05/18/ceeb4e3ab3aa54495775775b38ae42b10a92f42ce42dfa44da684289b8c8
+PYTHON_INSTALLER_SITE = https://files.pythonhosted.org/packages/source/i/installer
 
 # -----------------------------------------------------------------------------
 
