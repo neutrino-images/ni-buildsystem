e79a0d29d993b4da77deb3a285e526d55ea98553
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:51 +0100

    - python-pip: bump to version 23.3.2; update url

--- package/python-pip/python-pip.mk
+++ package/python-pip/python-pip.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PIP_VERSION = 22.3.1
+PYTHON_PIP_VERSION = 23.3.2
 PYTHON_PIP_DIR = pip-$(PYTHON_PIP_VERSION)
 PYTHON_PIP_SOURCE = pip-$(PYTHON_PIP_VERSION).tar.gz
-PYTHON_PIP_SITE = https://files.pythonhosted.org/packages/a3/50/c4d2727b99052780aad92c7297465af5fe6eec2dbae490aa9763273ffdc1
+PYTHON_PIP_SITE = https://files.pythonhosted.org/packages/source/p/pip
 
 PYTHON_PIP_SETUP_TYPE = setuptools
 
