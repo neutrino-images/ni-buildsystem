9be20a17bf5b6e1bd4386558c5f39a6d854bc4d2
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:52 +0100

    - python-pycryptodomex: bump to version 3.20.0; update url

--- package/python-pycryptodomex/python-pycryptodomex.mk
+++ package/python-pycryptodomex/python-pycryptodomex.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PYCRYPTODOMEX_VERSION = 3.18.0
+PYTHON_PYCRYPTODOMEX_VERSION = 3.20.0
 PYTHON_PYCRYPTODOMEX_DIR = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION)
 PYTHON_PYCRYPTODOMEX_SOURCE = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION).tar.gz
-PYTHON_PYCRYPTODOMEX_SITE = https://files.pythonhosted.org/packages/40/92/efd675dba957315d705f792b28d900bddc36f39252f6713961b4221ee9af
+PYTHON_PYCRYPTODOMEX_SITE = https://files.pythonhosted.org/packages/source/p/pycryptodomex
 
 PYTHON_PYCRYPTODOMEX_SETUP_TYPE = setuptools
 
