a780e6f206a9643a2fd57d4b75ca907d6d4ce0dd
vanhofen <vanhofen@gmx.de>
2024-01-21 23:35:52 +0100

    - add python-requests

--- package/python-requests/python-requests.mk
+++ package/python-requests/python-requests.mk
@@ -7,7 +7,7 @@
 PYTHON_REQUESTS_VERSION = 2.31.0
 PYTHON_REQUESTS_DIR = requests-$(PYTHON_REQUESTS_VERSION)
 PYTHON_REQUESTS_SOURCE = requests-$(PYTHON_REQUESTS_VERSION).tar.gz
-PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3
+PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/source/r/requests
 
 PYTHON_REQUESTS_SETUP_TYPE = setuptools
 
