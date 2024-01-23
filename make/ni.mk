d5e90377317ca6e6964c0dec138aa1ef6e09d66c
vanhofen <vanhofen@gmx.de>
2024-01-23 20:25:43 +0100

    - python: remove python3-compile-site-packages; thx to max_10

--- make/ni.mk
+++ make/ni.mk
@@ -75,7 +75,6 @@ ifeq ($(BOXSERIES),$(filter $(BOXSERIES),hd2 hd5x hd6x vusolo4k vuduo4k vuduo4ks
 	$(MAKE) sysstat
 	$(MAKE) python3
 	$(MAKE) python-yt-dlp
-	$(MAKE) python3-compile-site-packages
   endif
   ifeq ($(DEBUG),yes)
 	$(MAKE) strace
