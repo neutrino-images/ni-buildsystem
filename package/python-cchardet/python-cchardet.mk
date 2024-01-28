e37b4137cea6323a1b34572728f4a57c82440d78
vanhofen <vanhofen@gmx.de>
2024-01-28 20:59:31 +0100

    - add python-cchardet

---
+++ package/python-cchardet/python-cchardet.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-cchardet
+#
+################################################################################
+
+PYTHON_CCHARDET_VERSION = 2.1.7
+PYTHON_CCHARDET_DIR = cchardet-$(PYTHON_CCHARDET_VERSION)
+PYTHON_CCHARDET_SOURCE = cchardet-$(PYTHON_CCHARDET_VERSION).tar.gz
+PYTHON_CCHARDET_SITE = https://files.pythonhosted.org/packages/source/c/cchardet
+
+PYTHON_CCHARDET_SETUP_TYPE = setuptools
+
+PYTHON_CCHARDET_DEPENDS = host-python-cython
+
+python-cchardet: | $(TARGET_DIR)
+	$(call python-package)
