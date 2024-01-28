3cc0f864d988e4880b65a29c8d4e8b5f6d238149
vanhofen <vanhofen@gmx.de>
2024-01-28 20:59:31 +0100

    - add python-urllib3

---
+++ package/python-urllib3/python-urllib3.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-urllib3
+#
+################################################################################
+
+PYTHON_URLLIB3_VERSION = 2.1.0
+PYTHON_URLLIB3_DIR = urllib3-$(PYTHON_URLLIB3_VERSION)
+PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/source/u/urllib3
+
+PYTHON_URLLIB3_SETUP_TYPE = pep517
+
+PYTHON_URLLIB3_DEPENDENCIES = host-python-hatchling
+
+python-urllib3: | $(TARGET_DIR)
+	$(call python-package)
