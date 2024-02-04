6fed1b54f2bae04d8d47c81174bd24af2e1866d6
vanhofen <vanhofen@gmx.de>
2024-02-04 21:11:09 +0100

    - libpng: bump to version 1.6.41; update build

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -4,14 +4,19 @@
 #
 ################################################################################
 
-LIBPNG_VERSION = 1.6.40
+LIBPNG_VERSION = 1.6.41
+LIBPNG_SERIES = 16
 LIBPNG_DIR = libpng-$(LIBPNG_VERSION)
 LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
-LIBPNG_SITE = https://sourceforge.net/projects/libpng/files/libpng16/$(LIBPNG_VERSION)
+LIBPNG_SITE = https://sourceforge.net/projects/libpng/files/libpng$(LIBPNG_SERIES)/$(LIBPNG_VERSION)
 
 LIBPNG_DEPENDENCIES = zlib
 
-LIBPNG_CONFIG_SCRIPTS = libpng16-config libpng-config
+LIBPNG_CONFIG_SCRIPTS = libpng$(LIBPNG_SERIES)-config libpng-config
+
+LIBPNG_CONF_OPTS = \
+	--disable-tools \
+	--disable-powerpc-vsx
 
 libpng: | $(TARGET_DIR)
 	$(call autotools-package)
