1b431943dc6e69245367cbafa9b29b3dda36f849
vanhofen <vanhofen@gmx.de>
2024-03-08 17:43:35 +0100

    - libdvbcsa: update build

--- package/libdvbcsa/libdvbcsa.mk
+++ package/libdvbcsa/libdvbcsa.mk
@@ -7,10 +7,15 @@
 LIBDVBCSA_VERSION = master
 LIBDVBCSA_DIR = libdvbcsa.git
 LIBDVBCSA_SOURCE = libdvbcsa.git
-LIBDVBCSA_SITE = https://code.videolan.org/videolan
+LIBDVBCSA_SITE = $(GITHUB)/oe-mirrors
 LIBDVBCSA_SITE_METHOD = git
 
 LIBDVBCSA_AUTORECONF = YES
+
+ifeq ($(TARGET_ARCH),arm)
+LIBDVBCSA_CONF_OPTS = \
+	$(if $(findstring neon,$(TARGET_ABI)),--enable-neon,--disable-neon)
+endif
 
 libdvbcsa: | $(TARGET_DIR)
 	$(call autotools-package)
