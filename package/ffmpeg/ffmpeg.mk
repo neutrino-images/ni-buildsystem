97b7af58c8616d5bb04271150044e2ffb15466cf
vanhofen <vanhofen@gmx.de>
2024-03-30 00:11:18 +0100

    - ffmpeg-wrapper: switch to virtual-package

--- package/ffmpeg/ffmpeg.mk
+++ package/ffmpeg/ffmpeg.mk
@@ -4,5 +4,7 @@
 #
 ################################################################################
 
-ffmpeg: $(if $(filter $(BOXTYPE),coolstream),ffmpeg2,ffmpeg6)
-	@$(call TOUCH)
+FFMPEG_DEPENDENCIES = $(if $(filter $(BOXTYPE),coolstream),ffmpeg2,ffmpeg6)
+
+ffmpeg: | $(TARGET_DIR)
+	$(call virtual-package)
