6db6ee34df6e12d25ef0c42ade977bc0ae21e198
vanhofen <vanhofen@gmx.de>
2024-04-03 20:23:05 +0200

    - harfbuzz: bump version version 8.3.1; change build to meson-package

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -4,22 +4,32 @@
 #
 ################################################################################
 
-HARFBUZZ_VERSION = 1.8.8
+HARFBUZZ_VERSION = 8.3.1
 HARFBUZZ_DIR = harfbuzz-$(HARFBUZZ_VERSION)
-HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.bz2
-HARFBUZZ_SITE = https://www.freedesktop.org/software/harfbuzz/release
+HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.xz
+HARFBUZZ_SITE = https://github.com/harfbuzz/harfbuzz/releases/download/$(HARFBUZZ_VERSION)
 
 HARFBUZZ_DEPENDENCIES = freetype glib2
 
 HARFBUZZ_AUTORECONF = YES
 
 HARFBUZZ_CONF_OPTS = \
-	--with-freetype \
-	--with-glib \
-	--without-cairo \
-	--without-fontconfig \
-	--without-graphite2 \
-	--without-icu
+	-Dgdi=disabled \
+	-Ddirectwrite=disabled \
+	-Dcoretext=disabled \
+	-Dtests=disabled \
+	-Ddocs=disabled \
+	-Dbenchmark=disabled \
+	-Dicu_builtin=false \
+	-Dexperimental_api=false \
+	-Dfuzzer_ldflags="" \
+	-Dgobject=disabled \
+	-Dintrospection=disabled \
+	-Dgraphite=disabled \
+	-Dcairo=disabled \
+	-Dicu=disabled \
+	-Dfreetype=enabled \
+	-Dglib=enabled
 
 harfbuzz: | $(TARGET_DIR)
-	$(call autotools-package)
+	$(call meson-package)
