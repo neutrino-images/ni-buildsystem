824ab2e0ff73178e02be59a05e6149a8c10758dc
vanhofen <vanhofen@gmx.de>
2025-12-04 23:28:32 +0100

    - python-urllib3: bump to version 2.5.0

--- package/python-urllib3/python-urllib3.mk
+++ package/python-urllib3/python-urllib3.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 2.1.0
+PYTHON_URLLIB3_VERSION = 2.5.0
 PYTHON_URLLIB3_DIR = urllib3-$(PYTHON_URLLIB3_VERSION)
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
 PYTHON_URLLIB3_SITE = $(PYPI_MIRROR)/u/urllib3
 
-PYTHON_URLLIB3_SETUP_TYPE = pep517
+PYTHON_URLLIB3_SETUP_TYPE = hatch
 
-PYTHON_URLLIB3_DEPENDENCIES = host-python-hatchling
+PYTHON_URLLIB3_DEPENDENCIES = \
+	host-python-hatch-vcs \
+	host-python-setuptools-scm
 
 python-urllib3: | $(TARGET_DIR)
 	$(call python-package)
