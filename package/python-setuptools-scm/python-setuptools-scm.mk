c4dd8703eae8ab8ab89ccd8f322ef0828bfc4f41
vanhofen <vanhofen@gmx.de>
2025-12-04 23:24:43 +0100

    - python-setuptools-scm: bump to version 8.3.1

--- package/python-setuptools-scm/python-setuptools-scm.mk
+++ package/python-setuptools-scm/python-setuptools-scm.mk
@@ -4,13 +4,9 @@
 #
 ################################################################################
 
-#PYTHON_SETUPTOOLS_SCM_VERSION = 7.1.0
-#PYTHON_SETUPTOOLS_SCM_DIR = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
-#PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
-#PYTHON_SETUPTOOLS_SCM_SITE = $(PYPI_MIRROR)/s/setuptools_scm
-PYTHON_SETUPTOOLS_SCM_VERSION = 8.0.4
-PYTHON_SETUPTOOLS_SCM_DIR = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
-PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
+PYTHON_SETUPTOOLS_SCM_VERSION = 8.3.1
+PYTHON_SETUPTOOLS_SCM_DIR = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
+PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
 PYTHON_SETUPTOOLS_SCM_SITE = $(PYPI_MIRROR)/s/setuptools-scm
 
 # -----------------------------------------------------------------------------
