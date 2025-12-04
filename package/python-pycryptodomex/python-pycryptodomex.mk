bf280fe90387d0a7eede35ee844134848939db52
vanhofen <vanhofen@gmx.de>
2025-12-04 23:22:06 +0100

    - python-pycryptodomex: bump to version 3.23.0

--- package/python-pycryptodomex/python-pycryptodomex.mk
+++ package/python-pycryptodomex/python-pycryptodomex.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYCRYPTODOMEX_VERSION = 3.20.0
+PYTHON_PYCRYPTODOMEX_VERSION = 3.23.0
 PYTHON_PYCRYPTODOMEX_DIR = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION)
 PYTHON_PYCRYPTODOMEX_SOURCE = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION).tar.gz
 PYTHON_PYCRYPTODOMEX_SITE = $(PYPI_MIRROR)/p/pycryptodomex
