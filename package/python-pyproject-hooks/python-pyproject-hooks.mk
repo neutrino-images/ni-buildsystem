0cff63cea19a4d0d598f2ece4cd99b836970116b
vanhofen <vanhofen@gmx.de>
2025-12-04 23:23:25 +0100

    - python-pyproject-hooks: bump to version 1.2.0

--- package/python-pyproject-hooks/python-pyproject-hooks.mk
+++ package/python-pyproject-hooks/python-pyproject-hooks.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYPROJECT_HOOKS_VERSION = 1.0.0
+PYTHON_PYPROJECT_HOOKS_VERSION = 1.2.0
 PYTHON_PYPROJECT_HOOKS_DIR = pyproject_hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION)
 PYTHON_PYPROJECT_HOOKS_SOURCE = pyproject_hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION).tar.gz
 PYTHON_PYPROJECT_HOOKS_SITE = $(PYPI_MIRROR)/p/pyproject_hooks
