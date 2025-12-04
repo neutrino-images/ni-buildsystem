0cbfa90d930d49b9301035bcef93876ce2759c12
vanhofen <vanhofen@gmx.de>
2025-12-04 23:21:24 +0100

    - python-pluggy: bump to version 1.6.0

--- package/python-pluggy/python-pluggy.mk
+++ package/python-pluggy/python-pluggy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PLUGGY_VERSION = 1.4.0
+PYTHON_PLUGGY_VERSION = 1.6.0
 PYTHON_PLUGGY_DIR = pluggy-$(PYTHON_PLUGGY_VERSION)
 PYTHON_PLUGGY_SOURCE = pluggy-$(PYTHON_PLUGGY_VERSION).tar.gz
 PYTHON_PLUGGY_SITE = $(PYPI_MIRROR)/p/pluggy
