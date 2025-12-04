1aabfb4e32b08fa918c2097a537588dce74956bd
vanhofen <vanhofen@gmx.de>
2025-12-04 23:08:25 +0100

    - add python-hatch-vcs

---
+++ package/python-hatch-vcs/python-hatch-vcs.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-hatch-vcs
+#
+################################################################################
+
+PYTHON_HATCH_VCS_VERSION = 0.5.0
+PYTHON_HATCH_VCS_DIR = hatch_vcs-$(PYTHON_HATCH_VCS_VERSION)
+PYTHON_HATCH_VCS_SOURCE = hatch_vcs-$(PYTHON_HATCH_VCS_VERSION).tar.gz
+PYTHON_HATCH_VCS_SITE = $(PYPI_MIRROR)/h/hatch-vcs
+
+PYTHON_HATCH_VCS_SETUP_TYPE = hatch
+
+HOST_PYTHON_HATCH_VCS_DEPENDENCIES = host-python-setuptools-scm
+
+host-python-hatch-vcs: | $(HOST_DIR)
+	$(call host-python-package)
