80298a96589887454185386fe1494596569bd25f
vanhofen <vanhofen@gmx.de>
2025-12-04 23:12:39 +0100

    - python-calver: bump to version 2025.10.20

--- package/python-calver/python-calver.mk
+++ package/python-calver/python-calver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_CALVER_VERSION = 2022.6.26
+PYTHON_CALVER_VERSION = 2025.10.20
 PYTHON_CALVER_DIR = calver-$(PYTHON_CALVER_VERSION)
 PYTHON_CALVER_SOURCE = calver-$(PYTHON_CALVER_VERSION).tar.gz
 PYTHON_CALVER_SITE = $(PYPI_MIRROR)/c/calver
