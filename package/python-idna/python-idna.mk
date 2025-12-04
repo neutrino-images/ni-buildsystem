3ab895401aaaf66c4631ab90de4ac8db0f0ab839
vanhofen <vanhofen@gmx.de>
2025-12-04 23:17:34 +0100

    - python-idna: bump to version 3.11

--- package/python-idna/python-idna.mk
+++ package/python-idna/python-idna.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_IDNA_VERSION = 3.6
+PYTHON_IDNA_VERSION = 3.11
 PYTHON_IDNA_DIR = idna-$(PYTHON_IDNA_VERSION)
 PYTHON_IDNA_SOURCE = idna-$(PYTHON_IDNA_VERSION).tar.gz
 PYTHON_IDNA_SITE = $(PYPI_MIRROR)/i/idna
