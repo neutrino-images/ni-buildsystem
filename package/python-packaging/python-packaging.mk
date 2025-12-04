d61c46b61a6db5ef46fd019f1dfd56500b29b474
vanhofen <vanhofen@gmx.de>
2025-12-04 23:19:29 +0100

    - python-packaging: bump to version 25.0

--- package/python-packaging/python-packaging.mk
+++ package/python-packaging/python-packaging.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PACKAGING_VERSION = 23.2
+PYTHON_PACKAGING_VERSION = 25.0
 PYTHON_PACKAGING_DIR = packaging-$(PYTHON_PACKAGING_VERSION)
 PYTHON_PACKAGING_SOURCE = packaging-$(PYTHON_PACKAGING_VERSION).tar.gz
 PYTHON_PACKAGING_SITE = $(PYPI_MIRROR)/p/packaging
