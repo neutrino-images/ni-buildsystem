9e4ec23d7bab3aa53baf1fedbe20c3f73c4971f2
vanhofen <vanhofen@gmx.de>
2025-12-04 23:11:52 +0100

    - python-brotli: bump to version 1.2.0

--- package/python-brotli/python-brotli.mk
+++ package/python-brotli/python-brotli.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BROTLI_VERSION = 1.1.0
-PYTHON_BROTLI_DIR = Brotli-$(PYTHON_BROTLI_VERSION)
-PYTHON_BROTLI_SOURCE = Brotli-$(PYTHON_BROTLI_VERSION).tar.gz
+PYTHON_BROTLI_VERSION = 1.2.0
+PYTHON_BROTLI_DIR = brotli-$(PYTHON_BROTLI_VERSION)
+PYTHON_BROTLI_SOURCE = brotli-$(PYTHON_BROTLI_VERSION).tar.gz
 PYTHON_BROTLI_SITE = $(PYPI_MIRROR)/b/brotli
 
 PYTHON_BROTLI_SETUP_TYPE = setuptools
