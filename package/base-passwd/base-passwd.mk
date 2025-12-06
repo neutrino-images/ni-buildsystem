46ebc0aecd578d2dc4cb629d099f7eb24896df2a
vanhofen <vanhofen@gmx.de>
2025-12-07 00:15:54 +0100

    - base-passwd: bump version to 3.6.8

--- package/base-passwd/base-passwd.mk
+++ package/base-passwd/base-passwd.mk
@@ -4,10 +4,18 @@
 #
 ################################################################################
 
-BASE_PASSWD_VERSION = 3.5.29
-BASE_PASSWD_DIR = base-passwd-$(BASE_PASSWD_VERSION)
-BASE_PASSWD_SOURCE = base-passwd_$(BASE_PASSWD_VERSION).tar.gz
-BASE_PASSWD_SITE = https://launchpad.net/debian/+archive/primary/+files
+BASE_PASSWD_VERSION = 3.6.8
+BASE_PASSWD_DIR = work
+BASE_PASSWD_SOURCE = base-passwd_$(BASE_PASSWD_VERSION).tar.xz
+BASE_PASSWD_SITE = https://launchpad.net/debian/+archive/primary/+sourcefiles/base-passwd/$(BASE_PASSWD_VERSION)
+
+BASE_PASSWD_AUTORECONF = YES
+
+BASE_PASSWD_CONF_OPTS = \
+	--disable-docs \
+	--disable-selinux \
+	--disable-debconf \
+	--docdir=$(REMOVE_docdir)
 
 define BASE_PASSWD_INSTALL_MASTER_FILES
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/group.master $(TARGET_datadir)/base-passwd/group.master
