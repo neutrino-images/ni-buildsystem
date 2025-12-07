889bf3b1b84878d700521885041d27123c5b87d4
vanhofen <vanhofen@gmx.de>
2025-12-07 20:41:41 +0100

    - libfuse3: bump version to 3.17.4

--- package/libfuse3/libfuse3.mk
+++ package/libfuse3/libfuse3.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-LIBFUSE3_VERSION = 3.14.1
-LIBFUSE3_DIR     = fuse-$(LIBFUSE3_VERSION)
-LIBFUSE3_SOURCE  = fuse-$(LIBFUSE3_VERSION).tar.xz
-LIBFUSE3_SITE    = https://github.com/libfuse/libfuse/releases/download/fuse-$(LIBFUSE3_VERSION)
+LIBFUSE3_VERSION = 3.17.4
+LIBFUSE3_DIR = fuse-$(LIBFUSE3_VERSION)
+LIBFUSE3_SOURCE = fuse-$(LIBFUSE3_VERSION).tar.gz
+LIBFUSE3_SITE = https://github.com/libfuse/libfuse/releases/download/fuse-$(LIBFUSE3_VERSION)
 
 LIBFUSE3_CONF_OPTS = \
-	-Ddisable-mtab=true \
-	-Dudevrulesdir=/dev/null \
-	-Dutils=false \
 	-Dexamples=false \
-	-Duseroot=false
+	-Dudevrulesdir=/dev/null \
+	-Duseroot=false \
+	-Dtests=false \
+	-Dutils=false
 
 libfuse3: | $(TARGET_DIR)
 	$(call meson-package)
