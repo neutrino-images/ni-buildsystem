4744d9ade495b5824ee1c599b7b8337496189f65
vanhofen <vanhofen@gmx.de>
2025-12-07 18:52:29 +0100

    - ethtool: bump version to 6.15

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-ETHTOOL_VERSION = 6.0
+ETHTOOL_VERSION = 6.15
 ETHTOOL_DIR = ethtool-$(ETHTOOL_VERSION)
 ETHTOOL_SOURCE = ethtool-$(ETHTOOL_VERSION).tar.xz
 ETHTOOL_SITE = $(KERNEL_MIRROR)/software/network/ethtool
 
 ETHTOOL_CONF_OPTS = \
+	--datarootdir=$(REMOVE_datarootdir) \
 	--disable-pretty-dump \
-	--disable-netlink
+	--disable-netlink \
+	--with-bash-completion-dir=no
 
 ethtool: | $(TARGET_DIR)
 	$(call autotools-package)
