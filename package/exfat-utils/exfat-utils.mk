7dfc1d5a8bf4fca8584595a08df5c4775c471d10
vanhofen <vanhofen@gmx.de>
2025-12-07 20:41:41 +0100

    - exfat-utils: update build

--- package/exfat-utils/exfat-utils.mk
+++ package/exfat-utils/exfat-utils.mk
@@ -9,10 +9,6 @@ EXFAT_UTILS_DIR = exfat-utils-$(EXFAT_UTILS_VERSION)
 EXFAT_UTILS_SOURCE = exfat-utils-$(EXFAT_UTILS_VERSION).tar.gz
 EXFAT_UTILS_SITE = https://github.com/relan/exfat/releases/download/v$(EXFAT_UTILS_VERSION)
 
-EXFAT_UTILS_DEPENDENCIES = fuse-exfat
-
-EXFAT_UTILS_AUTORECONF = YES
-
 EXFAT_UTILS_CONF_OPTS = \
 	--prefix=$(base_prefix) \
 	--exec_prefix=$(base_prefix) \
