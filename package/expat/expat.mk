5bb6d070aa52afab18647064a220ab4318051267
vanhofen <vanhofen@gmx.de>
2025-12-08 18:54:30 +0100

    - expat: bump version to 2.7.3

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-EXPAT_VERSION = 2.5.0
+EXPAT_VERSION = 2.7.3
 EXPAT_DIR = expat-$(EXPAT_VERSION)
-EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.bz2
+EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.xz
 EXPAT_SITE = https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$(EXPAT_VERSION))
-
-EXPAT_AUTORECONF = YES
 
 EXPAT_CONF_OPTS = \
 	--docdir=$(REMOVE_docdir) \
@@ -29,7 +27,9 @@ expat: | $(TARGET_DIR)
 # -----------------------------------------------------------------------------
 
 HOST_EXPAT_CONF_OPTS = \
-	--without-docbook
+	--without-docbook \
+	--without-examples \
+	--without-tests
 
 host-expat: | $(HOST_DIR)
 	$(call host-autotools-package)
