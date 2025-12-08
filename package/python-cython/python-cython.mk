8f6b33a3b08886722cf2e6c039d15f71e5c9336f
vanhofen <vanhofen@gmx.de>
2025-12-08 19:37:36 +0100

    - python-cython: fix filename

--- package/python-cython/python-cython.mk
+++ package/python-cython/python-cython.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 PYTHON_CYTHON_VERSION = 3.2.2
-PYTHON_CYTHON_DIR = Cython-$(PYTHON_CYTHON_VERSION)
-PYTHON_CYTHON_SOURCE = Cython-$(PYTHON_CYTHON_VERSION).tar.gz
+PYTHON_CYTHON_DIR = cython-$(PYTHON_CYTHON_VERSION)
+PYTHON_CYTHON_SOURCE = cython-$(PYTHON_CYTHON_VERSION).tar.gz
 PYTHON_CYTHON_SITE = $(PYPI_MIRROR)/c/cython
 
 PYTHON_CYTHON_SETUP_TYPE = setuptools
