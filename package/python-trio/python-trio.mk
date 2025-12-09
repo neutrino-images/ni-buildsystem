33b28235c19559ff74a2ea1426f697891006a72e
vanhofen <vanhofen@gmx.de>
2025-12-09 20:24:47 +0100

    - add python-trio

---
+++ package/python-trio/python-trio.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-trio
+#
+################################################################################
+
+PYTHON_TRIO_VERSION = 0.31.0
+PYTHON_TRIO_DIR = trio-$(PYTHON_TRIO_VERSION)
+PYTHON_TRIO_SOURCE = trio-$(PYTHON_TRIO_VERSION).tar.gz
+PYTHON_TRIO_SITE = $(PYPI_MIRROR)/t/trio
+
+PYTHON_TRIO_SETUP_TYPE = setuptools
+
+python-trio: | $(TARGET_DIR)
+	$(call python-package)
