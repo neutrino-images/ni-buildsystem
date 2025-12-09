32edcd31c30827cc12ef5e5f77dffafe3c651dc3
vanhofen <vanhofen@gmx.de>
2025-12-09 20:08:01 +0100

    - add python-poetry-core

---
+++ package/python-poetry-core/python-poetry-core.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-poetry-core
+#
+################################################################################
+
+PYTHON_POETRY_CORE_VERSION = 2.2.0
+PYTHON_POETRY_CORE_DIR = poetry_core-$(PYTHON_POETRY_CORE_VERSION)
+PYTHON_POETRY_CORE_SOURCE = poetry_core-$(PYTHON_POETRY_CORE_VERSION).tar.gz
+PYTHON_POETRY_CORE_SITE = $(PYPI_MIRROR)/p/poetry-core
+
+# -----------------------------------------------------------------------------
+
+HOST_PYTHON_POETRY_CORE_SETUP_TYPE = pep517
+
+host-python-poetry-core: | $(HOST_DIR)
+	$(call host-python-package)
