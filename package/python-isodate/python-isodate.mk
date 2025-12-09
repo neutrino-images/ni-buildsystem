25449fc4b7b9e40f853d7d09d063a26fbe115287
vanhofen <vanhofen@gmx.de>
2025-12-09 20:08:02 +0100

    - add python-isodate

---
+++ package/python-isodate/python-isodate.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-isodate
+#
+################################################################################
+
+PYTHON_ISODATE_VERSION = 0.7.2
+PYTHON_ISODATE_DIR = isodate-$(PYTHON_ISODATE_VERSION)
+PYTHON_ISODATE_SOURCE = isodate-$(PYTHON_ISODATE_VERSION).tar.gz
+PYTHON_ISODATE_SITE = $(PYPI_MIRROR)/i/isodate
+
+PYTHON_ISODATE_SETUP_TYPE = flit
+
+python-isodate: | $(TARGET_DIR)
+	$(call python-package)
