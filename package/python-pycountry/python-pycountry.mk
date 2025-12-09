ec6f629eff0c2c44ed2b9c8e5760711504353287
vanhofen <vanhofen@gmx.de>
2025-12-09 20:08:02 +0100

    - add python-pycountry

---
+++ package/python-pycountry/python-pycountry.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-pycountry
+#
+################################################################################
+
+PYTHON_PYCOUNTRY_VERSION = 24.6.1
+PYTHON_PYCOUNTRY_DIR = pycountry-$(PYTHON_PYCOUNTRY_VERSION)
+PYTHON_PYCOUNTRY_SOURCE = pycountry-$(PYTHON_PYCOUNTRY_VERSION).tar.gz
+PYTHON_PYCOUNTRY_SITE = $(PYPI_MIRROR)/p/pycountry
+
+PYTHON_PYCOUNTRY_SETUP_TYPE = flit
+
+PYTHON_PYCOUNTRY_DEPENDENCIES = host-python-poetry-core
+
+python-pycountry: | $(TARGET_DIR)
+	$(call python-package)
