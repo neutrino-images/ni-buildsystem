d286abd2ab5b854c9a7cd08d5cff1382aa67b645
vanhofen <vanhofen@gmx.de>
2025-12-13 13:10:43 +0100

    - python-pip: add pip.conf to supress:
    WARNING: Running pip as the 'root' user can result in broken permissions and conflicting behaviour with the system package manager, possibly rendering your system unusable. It is recommended to use a virtual environment instead: https://pip.pypa.io/warnings/venv. Use the --root-user-action option if you know what you are doing and want to suppress this warning.

--- package/python-pip/python-pip.mk
+++ package/python-pip/python-pip.mk
@@ -11,6 +11,11 @@ PYTHON_PIP_SITE = $(PYPI_MIRROR)/p/pip
 
 PYTHON_PIP_SETUP_TYPE = flit
 
+define PYTHON_PIP_INSTALL_PIP_CONF
+	$(INSTALL_DATA) $(PKG_FILES_DIR)/pip.conf $(TARGET_sysconfdir)/pip.conf
+endef
+PYTHON_PIP_TARGET_FINALIZE_HOOKS += PYTHON_PIP_INSTALL_PIP_CONF
+
 python-pip: | $(TARGET_DIR)
 	$(call python-package)
 
