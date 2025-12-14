a5c6025d3bcb74eb32ac14e31cb086d89d62bbf3
vanhofen <vanhofen@gmx.de>
2025-12-14 10:35:09 +0100

    - gmp: bump version to 6.3.0

--- package/gmp/gmp.mk
+++ package/gmp/gmp.mk
@@ -4,10 +4,20 @@
 #
 ################################################################################
 
-GMP_VERSION = 6.2.1
+GMP_VERSION = 6.3.0
 GMP_DIR = gmp-$(GMP_VERSION)
 GMP_SOURCE = gmp-$(GMP_VERSION).tar.xz
 GMP_SITE = $(GNU_MIRROR)/gmp
+
+# 0001-Complete-function-prototype-in-acinclude.m4-for-C23-.patch
+GMP_AUTORECONF = YES
+
+GMP_CONF_ENV = \
+	CC_FOR_BUILD="$(HOSTCC) -std=c99"
+
+GMP_CONF_OPTS = \
+	--disable-assembly \
+	--enable-cxx
 
 gmp: | $(TARGET_DIR)
 	$(call autotools-package)
