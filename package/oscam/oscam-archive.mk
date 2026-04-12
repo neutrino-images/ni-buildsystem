a410b308f3320259be6743259c824ae9603b44d5
vanhofen <vanhofen@gmx.de>
2026-04-12 20:58:50 +0200

    - add oscam-archive

---
+++ package/oscam/oscam-archive.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# oscam-archive
+#
+################################################################################
+
+OSCAM_ARCHIVE_DEPENDENCIES = oscam # oscam requires OSCAM_KEEP_BUILD_DIR = YES
+
+define OSCAM_ARCHIVE_COMPRESS
+	v=$$($(BUILD_DIR)/$(OSCAM_DIR)/config.sh --oscam-version); \
+	$(INSTALL_EXEC) $(OSCAM_OSCAM_BIN) $(BUILD_DIR)/oscam-$${v}; \
+	$(CD) $(BUILD_DIR); \
+		rm -f oscam-$${v}.zip; \
+		zip -m oscam-$${v}.zip oscam-$${v}
+endef
+OSCAM_ARCHIVE_INDIVIDUAL_HOOKS += OSCAM_ARCHIVE_COMPRESS
+
+oscam-archive: | $(TARGET_DIR)
+	$(call virtual-package)
