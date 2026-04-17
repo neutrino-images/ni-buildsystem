939f43d44c33dd2499a44df7b335545e19abc0aa
vanhofen <vanhofen@gmx.de>
2026-04-17 20:58:17 +0200

    - ninjy: bump to version 1.13.2

--- package/ninja/ninja.mk
+++ package/ninja/ninja.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-NINJA_VERSION_MAJOR = 1.11.1
-NINJA_VERSION = $(NINJA_VERSION_MAJOR).g95dee.kitware.jobserver-1
+NINJA_VERSION = 1.13.2
 NINJA_DIR = ninja-$(NINJA_VERSION)
 NINJA_SOURCE = ninja-$(NINJA_VERSION).tar.gz
-NINJA_SITE = $(call github,Kitware,ninja,v$(NINJA_VERSION))
+NINJA_SITE = $(call github,ninja-build,ninja,v$(NINJA_VERSION))
 
 # -----------------------------------------------------------------------------
 
