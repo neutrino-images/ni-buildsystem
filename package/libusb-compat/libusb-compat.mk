0e307e03c7e7c343c35966b7a590bb32ee7aa6a6
vanhofen <vanhofen@gmx.de>
2026-04-18 17:37:46 +0200

    - libusb-compat: bump to version 0.1.8

--- package/libusb-compat/libusb-compat.mk
+++ package/libusb-compat/libusb-compat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUSB_COMPAT_VERSION = 0.1.7
+LIBUSB_COMPAT_VERSION = 0.1.8
 LIBUSB_COMPAT_DIR = libusb-compat-$(LIBUSB_COMPAT_VERSION)
 LIBUSB_COMPAT_SOURCE = libusb-compat-$(LIBUSB_COMPAT_VERSION).tar.bz2
 LIBUSB_COMPAT_SITE = https://github.com/libusb/libusb-compat-0.1/releases/download/v$(LIBUSB_COMPAT_VERSION)
