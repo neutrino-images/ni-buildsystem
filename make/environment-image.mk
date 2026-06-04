e21de2a4f37dcaaa79faf83522b03c12de44efe1
vanhofen <vanhofen@gmx.de>
2026-06-04 22:02:37 +0200

    - change to calendar version

--- make/environment-image.mk
+++ make/environment-image.mk
@@ -7,13 +7,13 @@
 DEBUG ?= no
 
 # Versioning
-GITTAG = $(shell git tag -l "NI-*" | tail -n1)
+GITTAG = $(shell git tag -l --sort=version:refname "NI-*" | tail -n1)
 GITREV = $(shell git rev-list $(GITTAG)..HEAD --count)
 
-# *Must* be a one-digit number
-IMAGE_VERSION_MAJOR = 4
-# *Must* be a two-digit number
-IMAGE_VERSION_MINOR = 30
+# *Must* be a four-digit number, the year (YYYY)
+IMAGE_VERSION_MAJOR = 2026
+# *Must* be a two-digit number, the month (MM)
+IMAGE_VERSION_MINOR = 05
 IMAGE_VERSION_MICRO = $(GITREV)
 IMAGE_VERSION_DOY = $(shell date +%j)
 
@@ -28,11 +28,11 @@ IMAGE_SUFFIX = $(BOXTYPE_SC)-$(BOXMODEL)
 IMAGE_NAME   = $(IMAGE_PREFIX)-$(IMAGE_SUFFIX)
 
 # Image type
-IMAGE_TYPE  ?= 9
 # Release    = 0
 # Beta       = 1
 # Nightly    = 2
 # Selfmade   = 9
+IMAGE_TYPE  ?= 9
 
 IMAGE_VERSION_STRING = $(IMAGE_TYPE)$(IMAGE_VERSION_MAJOR)$(IMAGE_VERSION_MINOR)$(IMAGE_DATE)
 
