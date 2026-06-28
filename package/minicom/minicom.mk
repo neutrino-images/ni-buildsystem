39df54ca078115e7b4b686548398232accc86e70
vanhofen <vanhofen@gmx.de>
2026-06-28 18:58:52 +0200

    - minicom: bump to version 2.11.1

--- package/minicom/minicom.mk
+++ package/minicom/minicom.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MINICOM_VERSION = 2.8
+MINICOM_VERSION = 2.11.1
 MINICOM_DIR = minicom-$(MINICOM_VERSION)
 MINICOM_SOURCE = minicom-$(MINICOM_VERSION).tar.gz
 MINICOM_SITE = https://salsa.debian.org/minicom-team/minicom/-/archive/$(MINICOM_VERSION)
