b3a80204210231f2aeacf027a7e448a0a79f0ce5
vanhofen <vanhofen@gmx.de>
2026-07-03 12:26:54 +0200

    - aio-grab: back to commit 2cadf8c

--- package/aio-grab/aio-grab.mk
+++ package/aio-grab/aio-grab.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AIO_GRAB_VERSION = master
+AIO_GRAB_VERSION = 2cadf8c
 AIO_GRAB_DIR = aio-grab.git
 AIO_GRAB_SOURCE = aio-grab.git
 AIO_GRAB_SITE = https://github.com/oe-alliance
